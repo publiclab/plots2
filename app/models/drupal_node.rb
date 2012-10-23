693258f7a5c39f8b2377dd9330e87fd7f4ae34eb
Jeffrey Warren <jeff@unterbahn.com>
2012-10-23 00:34:21 -0400

    fixed column collision

--- app/models/drupal_node.rb
+++ app/models/drupal_node.rb
@@ -4,6 +4,13 @@ class DrupalNode < ActiveRecord::Base
   has_one :drupal_node_revision, :foreign_key => 'nid'
 
   self.primary_key = 'nid'
+  class << self
+    def instance_method_already_implemented?(method_name)
+      return true if method_name == 'changed'
+      return true if method_name == 'changed?'
+      super
+    end
+  end
 
   def self.inheritance_column
     "rails_type"
