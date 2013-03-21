6cd61953b1f66130f441d96d15a785fa90cf5145
jywarren <jeff@unterbahn.com>
2013-03-21 11:09:23 -0400

    community tag fix for dashboard

--- app/models/drupal_tag.rb
+++ app/models/drupal_tag.rb
@@ -10,6 +10,7 @@ class DrupalTag < ActiveRecord::Base
   end
 
   has_many :drupal_node_community_tag, :foreign_key => 'tid'
+  # this probably never gets used; tag.drupal_node will use the above definition.
   has_many :drupal_node, :through => :drupal_node_community_tag do
     def filter_by_type(type)
       find(:all, :conditions => {:status => 1, :type => type})
@@ -25,10 +26,15 @@ class DrupalTag < ActiveRecord::Base
   end
 
   # results in time-unordered list... rework!
+  # also clean up params to be a hash with defaults
   def self.find_nodes_by_type(tags,type,limit)
     node_ids = []
     tags.each do |tag|
       tag.drupal_node.filter_by_type(type).each do |node|
+        node_ids << node.nid
+      end
+      # work in community tags; see how has_many at top is failing
+      DrupalNodeCommunityTag.find_all_by_tid(tag.tid).each do |node|
         node_ids << node.nid
       end
     end
