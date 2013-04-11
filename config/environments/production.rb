1bc4ae34c1ba6254db1d46f9a0c88d6e5cdb9c80
Jeffrey Warren <jeff@unterbahn.com>
2013-04-11 22:50:03 +0000

    sendmail config for email notifications

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ Plots2::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
 
   # Enable threaded mode
   # config.threadsafe!
