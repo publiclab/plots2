f16dded3633840b04720e25d9b9fdc05fa820e25
jywarren <jeff@unterbahn.com>
2013-04-18 19:28:55 -0400

    subscriptions on dashboard and updated alpha site switcher

--- app/models/tag_selection.rb
+++ app/models/tag_selection.rb
@@ -3,6 +3,9 @@ class TagSelection < ActiveRecord::Base
   self.primary_keys = :user_id, :tid
   belongs_to :drupal_tag, :foreign_key => :tid
 
+  validates :user_id, :presence => :true
+  validates :tid, :presence => :true
+
   def user
     DrupalUsers.find_by_uid self.user_id
   end
