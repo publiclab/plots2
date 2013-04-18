e1e50392ce1eeb9db6130ae35d35fc795a219a28
jywarren <jeff@unterbahn.com>
2013-04-17 23:03:41 -0400

    working tag subscriptions

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -33,7 +33,7 @@ class NotesController < ApplicationController
         @node.add_tag(tagname,current_user)
       end
       # trigger subscription notifications:
-      SubscriptionMailer.notify_node_creation(@node)
+      SubscriptionMailer.notify_node_creation(@node).deliver
       # opportunity for moderation
       flash[:notice] = "Research note published."
       redirect_to @node.path
