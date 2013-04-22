413a98d24f01ea5aebba28f3c5b298557d6e4fea
jywarren <jeff@unterbahn.com>
2013-04-22 17:24:45 -0400

    fix!

--- app/models/drupal_profile_value.rb
+++ app/models/drupal_profile_value.rb
@@ -1,7 +1,7 @@
 class DrupalProfileValue < ActiveRecord::Base
   attr_accessible :bio
   self.table_name = 'profile_values'
-  self.primary_key = 'fid'
+  self.primary_key = 'uid'
 
   belongs_to :drupal_users, :foreign_key => 'uid'
   belongs_to :drupal_profile_field, :foreign_key => 'fid'
