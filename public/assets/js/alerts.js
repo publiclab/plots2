2951cd58bdc0c03133a852c5b3be4f4b411e241c
Bryan Bonvallet <btbonval@gmail.com>
2013-07-07 16:52:56 -0400

    Moving alert javascript into its own static file rather than inline include.

---
+++ public/assets/js/alerts.js
@@ -0,0 +1,20 @@
+function alert_clear() { alert_set("hidden",""); }
+function alert_notice(msg) { alert_set("alert alert-success", msg); }
+function alert_error(msg) { alert_set("alert alert-error", msg); }
+function alert_warning(msg) { alert_set("alert alert", msg); }
+
+function alert_set(aclass, msg) {
+  // Adds an alert DIV using the alert class aclass and content from msg.
+  var new_alert = $(document.createElement("DIV"));
+  new_alert.addClass(aclass);
+  new_alert.append('<a class="close" data-dismiss="alert">×</a>');
+  if (aclass == "alert alert") {
+    new_alert.append('<i class="icon-exclamation-sign"></i>');
+  }
+  new_alert.append(msg);
+  $('#alert-placeholder').append(new_alert);
+
+  // Scroll page to the alerts
+  location.href = '#'; // some kind of Chrome bug fix
+  location.href = '#alert-placeholder';
+}
