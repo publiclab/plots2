540132fdf260cff00e9c62e606aa21abd25c7e06
Jeffrey Warren <jeff@unterbahn.com>
2013-07-09 22:57:01 +0000

    reintegrating banner.js code via non-precompiled script include until stable

--- public/assets/js/alerts.js
+++ public/assets/js/alerts.js
@@ -1,17 +1,17 @@
 function alert_clear() { alert_set("hidden",""); }
-function alert_notice(msg) { alert_set("alert alert-success", msg); }
-function alert_error(msg) { alert_set("alert alert-error", msg); }
-function alert_warning(msg) { alert_set("alert alert", msg); }
+function alert_notice(msg) { alert_set("success", msg); }
+function alert_error(msg) { alert_set("error", msg); }
+function alert_warning(msg) { alert_set("warning", msg); }
 
 /*
-aclass = "error", "warning", or "notice"
+aclass = "error", "warning", or "success"
 */
 function alert_set(aclass, msg, scroll) {
   // Adds an alert DIV using the alert class aclass and content from msg.
   var new_alert = $(document.createElement("DIV"));
   new_alert.addClass('alert alert-'+aclass);
   new_alert.append('<a class="close" data-dismiss="alert">×</a>');
-  if (aclass == "alert alert") {
+  if (aclass == "warning") {
     new_alert.append('<i class="icon-exclamation-sign"></i> ');
   }
   new_alert.append(msg);
