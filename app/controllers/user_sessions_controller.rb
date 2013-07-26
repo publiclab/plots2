48a075ef12d988eedb7aba0dcbc1547a1289be13
Jeffrey Warren <jeff@unterbahn.com>
2013-07-26 17:42:05 +0000

    user migration fix

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -9,7 +9,7 @@ class UserSessionsController < ApplicationController
     username = params[:user_session][:username] if params[:user_session]
 
     @user = User.find_by_username(username)
-    if params[:user_session].nil? || @user && @user.drupal_user.status == 1
+    if params[:user_session].nil? || @user && @user.drupal_user.status == 1 || @user.nil?
     # an existing native user
       if params[:user_session].nil? || @user
         if @user && @user.crypted_password.nil? # the user has not created a pwd in the new site
