c624eb4a2b3e69b8e5848889d26fb36fa3b90d3e
jywarren <jeff@unterbahn.com>
2013-09-02 16:33:38 -0400

    mailing list subscription tweaks close #168

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,11 +1,11 @@
-class PasswordResetMailer < ActionMailer::Base
+class WelcomeMailer < ActionMailer::Base
   #default from: "do-not-reply@publiclab.org"
 
   # PasswordResetMailer.reset_notify(user).deliver 
   def add_to_list(user,list)
     subject = "subscribe"
     @list = list
-    mail(:to => list+'+subscribe@googlegroups.com', :subject => subject, :from => user.mail).deliver
+    mail(:to => list+'+subscribe@googlegroups.com', :subject => subject, :from => user.email).deliver
   end
 
 end
