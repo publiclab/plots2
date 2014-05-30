00dc61633fd7491a87297f7458149db1cadeafaa
jywarren <jeff@unterbahn.com>
2014-05-30 10:10:54 -0400

    some chapter subdomains; publiclab/plots#91

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Plots2::Application.routes.draw do
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
+  #match '', to: 'blogs#show', constraints: {subdomain: /.+/}
+  # or to skip www:
+  match "*all", to: 'wiki#subdomain', constraints: lambda { |r| r.subdomain.present? && r.subdomain != 'www' && r.subdomain != "i" }
+
   match 'ioby' => "legacy#ioby"
 
   match 'login' => "user_sessions#new",      :as => :login
@@ -211,5 +215,6 @@ Plots2::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
 
   match ':controller(/:action(/:id))(.:format)'
+
 
 end
