7c1a95413b9b81cfaee72012b57b5c12c3ce5c9a
Jeffrey Warren <jeff@unterbahn.com>
2014-06-25 16:36:11 +0000

    merged in SSL changes and other

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,6 @@ module Plots2
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.force_ssl = true
-
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
