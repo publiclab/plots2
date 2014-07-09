992e74b631e6134e656b45d94a558e0ccdcab7db
Jeffrey Warren <jeff@unterbahn.com>
2014-07-09 21:18:01 +0200

    newsletter helper basics publiclab/plots2#68

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -4,6 +4,15 @@ class EditorController < ApplicationController
 
   # main image via URL passed as GET param
   def post
+    if params[:newsletter]
+      # nids already used
+      already = []
+      # filter just events
+      @events = DrupalNode.find :all, :conditions => ['type = "note" AND status = 1 AND created > '+(Time.now.to_i-1.weeks.to_i).to_s]
+      already += @events.collect(&:nid)
+      # filter just events
+      @notes = DrupalNode.find :all, :conditions => ['type = "note" AND status = 1 AND nid NOT IN (?) AND created > '+(Time.now.to_i-1.weeks.to_i).to_s,already]
+    end
     # /post/?i=http://myurl.com/image.jpg
     if params[:i]
       @image = Image.new({
