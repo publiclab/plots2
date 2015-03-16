916fc51359fb3c4b4e23576e5aa08b63e5e9cf3b
jywarren <jeff@unterbahn.com>
2015-03-16 10:34:46 -0400

    fixed book icons and moved leaflet assets into bower

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,6 +24,7 @@
 //= require marked/marked.min.js
 //= require ics/ics.deps.min.js
 //= require ics/ics.min.js
+//= require leaflet/dist/leaflet
 //  APPLICATION SCRIPTS:
 //= require search.js
 //= require setup.js
