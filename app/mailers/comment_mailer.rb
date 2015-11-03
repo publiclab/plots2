1b17c84a25d2ca2d6f1e18142a0d97243154e78a
jywarren <jeff@unterbahn.com>
2015-11-03 14:44:17 -0500

    fix for barnstar award email routing

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -18,7 +18,7 @@ class CommentMailer < ActionMailer::Base
   def notify_barnstar(user,note)
     @giver = user.drupal_user
     @note = note
-    mail(:to => user.email, :subject => "You were awarded a Barnstar!").deliver
+    mail(:to => note.author.email, :subject => "You were awarded a Barnstar!").deliver
   end
 
   def notify_callout(comment,user)
