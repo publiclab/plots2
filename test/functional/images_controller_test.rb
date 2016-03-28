bc1658609b7001cebfdacc8baf4d20b82b2675ed
jywarren <jeff@unterbahn.com>
2016-03-28 11:46:20 -0400

    commented-out tests for image and revision deletion features in #470

--- test/functional/images_controller_test.rb
+++ test/functional/images_controller_test.rb
@@ -1,13 +1,26 @@
 require 'test_helper'
 
 class ImagesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
 
 # def create
 # def new
 # def update
-# def delete
+
+#  test "normal user should not delete image" do
+#    UserSession.new(users(:bob))
+#    post :delete, id: Image.last.id
+#    assert_equal flash[:error], "Only admins can delete wiki pages."
+#    assert_redirected_to "/wiki/" + title.parameterize # use node_path?
+#  end
+
+#  test "admin user should delete image" do
+#    UserSession.new(users(:admin))
+#    post :delete, id: Image.last.id
+#    assert_equal flash[:notice], "Wiki revision deleted."
+#    assert_redirected_to "/wiki/" + title.parameterize # use node_path?
+
+## also ensure any revisions/wiki pages/notes using this image have been re-assigned a most-recent, or no image
+
+#  end
 
 end
