22dab75b21429dea4734d9c0506a3577c9195e23
jywarren <jeff@unterbahn.com>
2016-04-24 13:08:08 -0400

    publishes new dashboard as default

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -32,12 +32,4 @@ class HomeControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get dashboard2" do
-    UserSession.create(rusers(:bob))
-
-    get :dashboard2
-
-    assert_response :success
-  end
-
 end
