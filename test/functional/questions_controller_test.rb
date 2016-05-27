74d6b8dc1f03d1bb0e84328e7bf6ea816600376e
Ananya Maiti <ananyoevo@gmail.com>
2016-05-27 22:35:50 +0530

    Add functional tests for questions controller
    Add tests for redirection in create and update actions in notes controller
    
    Add functional test for user_sessions controller
    
    Add integration test for login flow

---
+++ test/functional/questions_controller_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+
+class QuestionsControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:notes)
+    assert assigns(:notes).first.has_power_tag('question')
+  end
+
+  test "should get show" do
+    note = node(:question)
+    get :show,
+        author: note.author.name,
+        date: Time.at(note.created).strftime("%m-%d-%Y"),
+        id: note.title.parameterize
+
+    assert_response :success
+  end
+
+  test "show note by id" do
+    note = node(:question)
+    get :show, id: note.id
+    assert_response :success
+  end
+
+  test "should not show notes other than question" do
+    note = node(:one)
+    get :show,
+        author: note.author.name,
+        date: Time.at(note.created).strftime("%m-%d-%Y"),
+        id: note.title.parameterize
+    assert_redirected_to '/'
+    assert_equal flash[:error], "Not a question"
+  end
+
+  test "redirect question to short url" do
+    note = node(:question)
+    get :shortlink, id: note.id
+    assert_redirected_to "/questions/"+note.author.name+"/"+Time.now.strftime("%m-%d-%Y")+"/"+note.title.parameterize
+  end
+
+  test "should get popular" do
+    get :popular
+    assert_response :success
+  end
+
+  test "should get liked" do
+    get :liked
+    assert_response :success
+  end
+
+end
