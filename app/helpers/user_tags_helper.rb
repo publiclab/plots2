f5488f3ed6f722bca1ce380406fb309313207027
Lalith <lalithr95@gmail.com>
2016-06-01 02:31:15 +0530

    display saved tags in additional info

--- app/helpers/user_tags_helper.rb
+++ app/helpers/user_tags_helper.rb
@@ -1,2 +1,10 @@
 module UserTagsHelper
+  def fetch_tags type
+    tag_types = ['skill', 'gear', 'role', 'tool']
+    tags = []
+    if tag_types.include? type
+      tags = UserTag.where("value LIKE ?", type + ":" + "%")
+    end
+    return tags
+  end
 end
