961c63f02133f05d3b2a4e1fa011ee0900cbc77c
jywarren <jeff@unterbahn.com>
2016-06-03 16:58:54 -0400

    tab/space indent changes

--- test/functional/user_sessions_controller_test.rb
+++ test/functional/user_sessions_controller_test.rb
@@ -1,20 +1,20 @@
 require 'test_helper'
 
 class UserSessionsControllerTest < ActionController::TestCase
-	test "should login an user" do
-		post :create, user_session: {
-						username: rusers(:jeff).username,
-						password: 'secret'
-		}
-		assert_redirected_to '/dashboard'
-	end
+  test "should login an user" do
+    post :create, user_session: {
+      username: rusers(:jeff).username,
+      password: 'secret'
+    }
+    assert_redirected_to '/dashboard'
+  end
 
-	test "should login and redirect to corresct url" do
-		session[:return_to] = '/post?tags=question:question&template=question'
-		post :create, user_session: {
-						username: rusers(:jeff).username,
-						password: 'secret'
-		}
-		assert_redirected_to '/post?tags=question:question&template=question'
-	end
+  test "should login and redirect to corresct url" do
+    session[:return_to] = '/post?tags=question:question&template=question'
+    post :create, user_session: {
+      username: rusers(:jeff).username,
+      password: 'secret'
+    }
+    assert_redirected_to '/post?tags=question:question&template=question'
+  end
 end
--- test/integration/login_flow_test.rb
+++ test/integration/login_flow_test.rb
@@ -1,17 +1,17 @@
 require 'test_helper'
 
 class LoginFlowTest < ActionDispatch::IntegrationTest
-  test "redirect to login page if user is not logged in and then redirect back to desired page after login" do  	
-  	get '/post?tags=question:question&template=question'
-  	follow_redirect!
-  	assert_equal '/login', path
+  test "redirect to login page if user is not logged in and then redirect back to desired page after login" do    
+    get '/post?tags=question:question&template=question'
+    follow_redirect!
+    assert_equal '/login', path
 
-  	post '/user_sessions', user_session: {
-  										username: rusers(:jeff).username,
-  										password: 'secret'
-  							   	}
-  	follow_redirect!
-  	assert_equal '/post?tags=question:question&template=question', request.fullpath
+    post '/user_sessions', user_session: {
+      username: rusers(:jeff).username,
+      password: 'secret'
+    }
+    follow_redirect!
+    assert_equal '/post?tags=question:question&template=question', request.fullpath
   end
 
   test "search a question and go through post mechanism if question not found when user is not logged in" do
@@ -23,9 +23,9 @@ class LoginFlowTest < ActionDispatch::IntegrationTest
     assert_equal '/login', path
 
     post '/user_sessions', user_session: {
-                      username: rusers(:jeff).username,
-                      password: 'secret'
-                    }
+      username: rusers(:jeff).username,
+      password: 'secret'
+    }
     follow_redirect!
     assert_equal '/post?tags=question:question&template=question&title=What&redirect=question', request.fullpath
     assert_select "input#title" do
