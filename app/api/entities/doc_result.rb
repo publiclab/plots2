572505000a5fb61d08e1a32a4d713ce3cca942b4
david.c.days@gmail.com <david.c.days@gmail.com>
2016-06-05 19:50:50 -0400

    Continuing to flesh out RESTful api

--- app/api/entities/doc_result.rb
+++ app/api/entities/doc_result.rb
@@ -2,12 +2,12 @@ module API
   module Entities
     # A DocResult is an individual return item for a document (web page) search
     class DocResult < Grape::Entity
-      expose :docType
-      expose :docUrl
-      expose :docTitle
-      expose :docSummary
-      expose :docTags
-      expose :docScore
+      expose :docType, documentation: { type: "String", desc: "Type of web document being returned." }
+      expose :docUrl, documentation: { type: "String", desc: "URL to the resource document." }
+      expose :docTitle, documentation: { type: "String", desc: "Title or primary descriptor of the linked result." }
+      expose :docSummary, documentation: { type: "String", desc: "If available, first paragraph or descriptor of the linked document." }
+      expose :docTags, using API::Entities::TagResult, as: :responses, documentation: { type: "API::Entities::TagResult", desc:  "An array of the associated tag values for the document, if they exist." }
+      expose :docScore, documentation: { type: "Float", desc: "If calculated, the relevance of the document result to the search request; i.e. the 'matching score'" }
     end
   end
 end
--- app/api/entities/tag_result.rb
+++ app/api/entities/tag_result.rb
@@ -2,9 +2,10 @@ module API
   module Entities
     # Tags are text values assigned to various site documents.
     class TagResult < Grape::Entity
-      expose :tagId
-      expose :tagVal
-      expose :tagType
+      expose :tagId, documentation: { type:  "Integer", desc: "Not required, but the primary key identifer for this tag" }
+      expose :tagVal, documentation: { type: "String", desc: "The text value of the tag" }
+      expose :tagType, documentation: { type: "String", desc:  "The type of tag this represents" }
+      expose :tagSource, documentation: { type: "String", desc: "The original source of the tag value; this varies for different document types" }
     end
   end
 end
