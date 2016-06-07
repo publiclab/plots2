1ba2a240684f59dd89876b61d23e3bc4b4444f4d
Ujitha Perera <ujithaperera1@gmail.com>
2016-06-07 11:06:31 +0530

    refactor for DRY

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,13 +1,5 @@
 class Search < ActiveRecord::Base
 
-  def users(params)
-    @users ||= find_users(params)
-  end
 
-  def find_users(input)
-    DrupalUsers.where('name LIKE ? AND access != 0', '%' +input+ '%')
-        .order("uid")
-        .limit(5)
-  end
 
 end
