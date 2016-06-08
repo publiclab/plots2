df364afd5a15e6cacccb5cbf7a4451772460a052
Ujitha Perera <ujithaperera1@gmail.com>
2016-06-08 15:37:00 +0530

    fix broken links and styles issues

--- app/models/search.rb
+++ app/models/search.rb
@@ -5,12 +5,12 @@ class Search < ActiveRecord::Base
   ## current advanced search
   def advanced_search(input, params)
     all = !params[:notes] && !params[:wikis] && !params[:maps] && !params[:comments]
-    nodes = []
+    @nodes = []
     unless input.blank?
-      nodes += SearchService.new.find_notes(input, 25) if params[:notes] || all
-      nodes += SearchService.new.find_maps(input, 25) if params[:maps] || all
-      nodes += SearchService.new.find_comments(input, 25) if params[:comments] || all
-      nodes += DrupalNode.limit(25)
+      @nodes += SearchService.new.find_notes(input, 25) if params[:notes] || all
+      @nodes += SearchService.new.find_maps(input, 25) if params[:maps] || all
+      @nodes += SearchService.new.find_comments(input, 25) if params[:comments] || all
+      @nodes += DrupalNode.limit(25)
                     .order("nid DESC")
                     .where('(type = "page" OR type = "place" OR type = "tool") AND node.status = 1 AND title LIKE ?', "%" + input + "%") if params[:wikis] || all
     end
