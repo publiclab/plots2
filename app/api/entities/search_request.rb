ce80fd6e07abcd2717dae3f23317170ab89865fe
david.c.days@gmail.com <david.c.days@gmail.com>
2016-06-11 00:52:36 -0400

    Refactoring based on example of gitlabhq project

--- app/api/entities/search_request.rb
+++
@@ -1,8 +0,0 @@
-module API
-  module Entities
-    class SearchRequest < Grape::Entity
-      expose :srchString, documentation: { type: "String", desc: "Search Query text."}
-      expose :seq, documentation: { type: "Integer", desc: "Sequence value passed from client through to the SearchResult.  For client sequencing usage" }      
-    end
-  end
-end
