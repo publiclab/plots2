8a76cd650e03a89fdd6b7eff807be82f87727457
david.c.days@gmail.com <david.c.days@gmail.com>
2016-06-11 01:50:38 -0400

    generic typeahead implementation

--- lib/api/typeahead.rb
+++ lib/api/typeahead.rb
@@ -1,14 +1,25 @@
 module API
   class Typeahead < Grape::API
     
-    desc 'RESTful method for typeahead for tag searches' do
-      detail 'This method conducts searches for tags that match the given value.'
-      params API::Entities::SearchRequest
-      success API::Entities::SearchResult
-    end
-    get :tags do
+    before_filter :set_search_service
 
+    resource :typeahead do
+
+      # Request URL should be /api/typeahead/general/:id
+      # Basic implementation from classic plots2 SearchController
+      get :general do
+        match = @search_service.type_ahead(params[:id])
+        { sresult: match }
+      end
     end
 
+  private
+    def set_search_service
+      @search_service = SearchService.new
+    end
+
+    def search_params
+      params.require(:search).permit(:comments, :maps, :wikis, :@notes)
+    end
   end
 end
