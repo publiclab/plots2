265dd0fad1882a536123fdb3cd1fce25c5765f8c
david.c.days@gmail.com <david.c.days@gmail.com>
2016-06-12 22:45:17 -0400

    Structure adjustment to accord with Rails

--- config/application.rb
+++ config/application.rb
@@ -96,6 +96,7 @@ module Plots2
       #{config.root}/app/controllers/concerns
       #{config.root}/app/models/concerns
     )
+
     # Search API configuration
     config.paths.add File.join('app','api'), glob: File.join('**', '*.rb')
     config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
--- config/routes.rb
+++ config/routes.rb
@@ -302,7 +302,7 @@ Plots2::Application.routes.draw do
   # match ':controller(/:action(/:id))(.:format)'
 
 
-  mount API::Base => '/api'
+  mount Search::API => '/'
   mount GrapeSwaggerRails::Engine => '/apidoc'
 
 end
