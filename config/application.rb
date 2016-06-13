4f2523238491140e5a51a3bd0ab85d42708edcb7
david.c.days@gmail.com <david.c.days@gmail.com>
2016-06-12 22:45:17 -0400

    Structure adjustment to accord with Rails

--- config/application.rb
+++ config/application.rb
@@ -90,5 +90,9 @@ module Plots2
       #{config.root}/app/controllers/concerns
       #{config.root}/app/models/concerns
     )
+    # Search API configuration
+    config.paths.add File.join('app','api'), glob: File.join('**', '*.rb')
+
+    config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
   end
 end
