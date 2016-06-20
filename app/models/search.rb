5d8dc648816d0ed6697a9424c698f908f3ad57f3
Ujitha Perera <ujithaperera1@gmail.com>
2016-06-20 14:16:40 +0530

    add date range filter

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,5 +1,5 @@
 class Search < ActiveRecord::Base
-
+  require 'date'
   include ActiveModel::ForbiddenAttributesProtection
 
   attr_accessible :key_words, :title, :main_type, :note_type, :min_date, :created_by,
@@ -22,6 +22,10 @@ class Search < ActiveRecord::Base
     @users ||= find_users(params)
   end
 
+  def date_i(date_param)
+    Date.strptime(date_param, '%d-%m-%Y').to_time.to_i
+  end
+
   def nodes
     @nodes ||= find_nodes
   end
@@ -37,11 +41,11 @@ class Search < ActiveRecord::Base
   end
 
   def minimum_date_conditions
-    ['node.created >= ?', min_date] unless min_date.blank?
+    ['node.created >= ?', date_i(min_date)] unless min_date.blank?
   end
 
   def maximum_date_conditions
-    ['node.created <= ?', max_date] unless max_date.blank?
+    ['node.created <= ?', date_i(max_date)] unless max_date.blank?
   end
 
   def type_conditions
