19471b3332b5c6b48420078907f8f00d10da15fe
david.c.days@gmail.com <david.c.days@gmail.com>
2016-07-06 21:25:01 -0400

    Basic dynamic search page and display of RESTful results.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -57,6 +57,16 @@ class SearchesController < ApplicationController
                  .includes(:drupal_node_revision)
   end
 
+  # DEPRECATED 
+  # utility response to fill out search autocomplete
+  # needs *dramatic* optimization
+
+  def typeahead
+    warn "[DEPRECATED] SearchesController.typeahead is deprecated.  Use the RESTful API for typeahead instead."
+    @match = @search_service.type_ahead(params[:id])
+    render json: @match
+  end
+
   def map
     @users = DrupalUsers.where("lat != 0.0 AND lon != 0.0")
   end
