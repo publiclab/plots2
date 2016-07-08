a16913ef6c9b534a57c38e67f90fce180d83224b
david.c.days@gmail.com <david.c.days@gmail.com>
2016-07-08 16:46:44 -0400

    Reworked advanced search javascript to work across all pages and implementations.

--- app/assets/javascripts/searchform.js
+++ app/assets/javascripts/searchform.js
@@ -1,3 +1,6 @@
+/**
+  This functionality operates on the header search form that is part of the primary layout.  It operates the submission of the search form to the general search page.  Any site-wide scripting that should affect the header searches should go here.
+**/
 jQuery(document).ready(function() {
   //Set up the _header search form submission
   $('#searchform').submit(function(e){ 
