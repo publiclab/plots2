366a5f24d5ebaa75b96f85944dcb02f56d5f3c00
Ananya Maiti <ananyoevo@gmail.com>
2016-07-18 04:24:36 +0530

    Add unit tests for answer, comment and subscription mailer

---
+++ test/unit/answer_mailer_test.rb
@@ -0,0 +1,78 @@
+require 'test_helper'
+
+class AnswerMailerTest < ActionMailer::TestCase
+  test "notify question author" do
+    user = rusers(:jeff)
+    answer = answers(:one)
+    assert_difference 'ActionMailer::Base.deliveries.size', 1 do
+      AnswerMailer.notify_question_author(user, answer).deliver
+    end
+    assert !ActionMailer::Base.deliveries.empty?
+    
+    email = ActionMailer::Base.deliveries.last
+    assert_equal ['do-not-reply@publiclab.org'], email.from
+    assert_equal [user.email], email.to
+    assert_equal "[PublicLab] New answer to Question: " + answer.node.title, email.subject
+    assert email.body.include?("Hi! A new answer has been posted for your question '<a href='https://publiclab.org#{answer.node.path(:question)}'>#{answer.node.title}</a>'")
+  end
+
+  test "notify other answer authors" do
+    user = rusers(:bob)
+    answer = answers(:two)
+    assert_difference 'ActionMailer::Base.deliveries.size', 1 do
+      AnswerMailer.notify_answer_likers_author(user, answer).deliver
+    end
+    assert !ActionMailer::Base.deliveries.empty?
+    
+    email = ActionMailer::Base.deliveries.last
+    assert_equal ['do-not-reply@publiclab.org'], email.from
+    assert_equal [user.email], email.to
+    assert_equal "[PublicLab] New answer to Question: " + answer.node.title, email.subject
+    assert email.body.include?("Hi! There's been a new answer posted for the question '<a href='https://publiclab.org#{answer.node.path(:question)}'>#{answer.node.title}</a>' that you also answered")
+  end
+
+  test "notify user who liked the question" do
+    user = rusers(:admin)
+    answer = answers(:two)
+    assert_difference 'ActionMailer::Base.deliveries.size', 1 do
+      AnswerMailer.notify_answer_likers_author(user, answer).deliver
+    end
+    assert !ActionMailer::Base.deliveries.empty?
+    
+    email = ActionMailer::Base.deliveries.last
+    assert_equal ['do-not-reply@publiclab.org'], email.from
+    assert_equal [user.email], email.to
+    assert_equal "[PublicLab] New answer to Question: " + answer.node.title, email.subject
+    assert email.body.include?("Hi! There's been a new answer posted for the question '<a href='https://publiclab.org#{answer.node.path(:question)}'>#{answer.node.title}</a>' that you liked")
+  end
+
+  test "notify answer author when answer is accepted" do
+    user = rusers(:bob)
+    answer = answers(:one)
+    assert_difference 'ActionMailer::Base.deliveries.size', 1 do
+      AnswerMailer.notify_answer_accept(user, answer).deliver
+    end
+    assert !ActionMailer::Base.deliveries.empty?
+    
+    email = ActionMailer::Base.deliveries.last
+    assert_equal ['do-not-reply@publiclab.org'], email.from
+    assert_equal [user.email], email.to
+    assert_equal "[PublicLab] Your answer has been accepted", email.subject
+    assert email.body.include?("Your answer for the question <a href='https://publiclab.org#{answer.node.path(:question)}'>#{answer.node.title}</a> has been accepted")
+  end
+
+  test "notify answer author when answer is liked" do
+    user = rusers(:bob)
+    answer = answers(:one)
+    assert_difference 'ActionMailer::Base.deliveries.size', 1 do
+      AnswerMailer.notify_answer_like(user, answer).deliver
+    end
+    assert !ActionMailer::Base.deliveries.empty?
+    
+    email = ActionMailer::Base.deliveries.last
+    assert_equal ['do-not-reply@publiclab.org'], email.from
+    assert_equal [answer.author.email], email.to
+    assert_equal "[PublicLab] #{user.username} liked your answer to: " + answer.node.title, email.subject
+    assert email.body.include?("Public Lab contributor <a href='https://publiclab.org/profile/#{user.username}'>#{user.username}</a> just liked your answer to the question <a href='https://publiclab.org#{answer.node.path(:question)}'>#{answer.node.title}</a>")
+  end
+end
