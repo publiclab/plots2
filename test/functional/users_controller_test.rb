1e3c99a40e9ef38c0dbd8a8f3deb16de689f8e86
Ananya Maiti <ananyoevo@gmail.com>
2016-07-21 06:29:43 +0530

    Stub Geocoder lookup in users contoller tests

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -15,6 +15,22 @@ class UsersControllerTest < ActionController::TestCase
 
   def setup
     activate_authlogic
+
+    Geocoder.configure(:lookup => :test)
+
+    Geocoder::Lookup::Test.set_default_stub(
+      [
+         {
+            'latitude'     => 40.7143528,
+            'longitude'    => -74.0059731,
+            'address'      => 'New York, NY, USA',
+            'state'        => 'New York',
+            'state_code'   => 'NY',
+            'country'      => 'United States',
+            'country_code' => 'US'
+         }
+      ]
+    )
   end
 
   test "new user page" do
