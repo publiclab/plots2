f5c4c91507141304299a1f65e5f82dcf897d5f1b
Jitesh Kumar Jha <jiteshjha96@gmail.com>
2016-07-23 08:49:53 +0000

    Added timestamp to the redirected url
    Add test
    
    Added requested changes

--- test/integration/I18n_test.rb
+++ test/integration/I18n_test.rb
@@ -7,7 +7,6 @@ class I18nTest < ActionDispatch::IntegrationTest
             get '/home'
             get_via_redirect "/change_locale/"+lang.to_s
             assert_select "a[href=/wiki/issues]", I18n.t("layout._footer.getting_help.report_bug")
-            assert true
         end
     end
     
@@ -23,7 +22,6 @@ class I18nTest < ActionDispatch::IntegrationTest
             follow_redirect!
             get_via_redirect "/dashboard", :locale => lang
             assert_select "a[href=/dashboard]", I18n.t('layout._header.dashboard')
-            assert true
         end
     end
     
@@ -34,7 +32,6 @@ class I18nTest < ActionDispatch::IntegrationTest
             assert_equal "#{lang}", "#{I18n.locale}"
             get_via_redirect "/dashboard"
             assert_equal "#{lang}", "#{I18n.locale}"
-            assert true
         end
     end
     
@@ -42,7 +39,6 @@ class I18nTest < ActionDispatch::IntegrationTest
         get "/change_locale/"+"unavail_locale".to_s
         follow_redirect!
         assert_equal "#{I18n.locale}", "#{I18n.default_locale}"
-        assert true
     end
     
     test "should choose i18n for subscriptions" do
@@ -55,7 +51,6 @@ class I18nTest < ActionDispatch::IntegrationTest
         follow_redirect!
         get '/subscriptions'
         assert_select 'b', I18n.t('home.subscriptions.title')
-        assert true
       end
     end
     
@@ -68,7 +63,6 @@ class I18nTest < ActionDispatch::IntegrationTest
         }
         follow_redirect!
         assert_select 'h3', I18n.t('dashboard._activity.activity')
-        assert true
       end
     end
     
@@ -81,12 +75,13 @@ class I18nTest < ActionDispatch::IntegrationTest
         }
         follow_redirect!
         assert_select 'h1', I18n.t('dashboard._header.dashboard')
-        assert true
       end
     end
     
     test "should choose i18n for dashboard/_node_comment" do
       available_testing_locales.each do |lang|
+        get "/change_locale/"+lang.to_s
+        follow_redirect!
         post '/user_sessions', user_session: {
           username: rusers(:jeff).username,
           password: 'secret'
@@ -94,12 +89,13 @@ class I18nTest < ActionDispatch::IntegrationTest
         follow_redirect!
         get '/dashboard'
         assert_select 'span', I18n.t('dashboard._node_comment.commented_on')
-        assert true
       end
     end
     
     test "should choose i18n for dashboard/_node_moderate" do
       available_testing_locales.each do |lang|
+        get "/change_locale/"+lang.to_s
+        follow_redirect!
         post '/user_sessions', user_session: {
           username: rusers(:jeff).username,
           password: 'secret'
@@ -112,12 +108,13 @@ class I18nTest < ActionDispatch::IntegrationTest
          status: 4
         get '/dashboard'
         assert_select 'a[class=btn btn-default btn-xs]', I18n.t('dashboard._node_moderate.approve')
-        assert true
       end
     end
     
     test "should choose i18n for dashboard/_node_question" do
       available_testing_locales.each do |lang|
+        get "/change_locale/"+lang.to_s
+        follow_redirect!
         post '/user_sessions', user_session: {
           username: rusers(:jeff).username,
           password: 'secret'
@@ -129,12 +126,13 @@ class I18nTest < ActionDispatch::IntegrationTest
          status: 1
         get '/dashboard'
         assert_select 'a[class=btn btn-default btn-xs pull-right respond answer]', I18n.t('dashboard._node_question.post_answer')
-        assert true
       end
     end
     
     test "should choose i18n for dashboard/_node_wiki" do
       available_testing_locales.each do |lang|
+        get "/change_locale/"+lang.to_s
+        follow_redirect!
         post '/user_sessions', user_session: {
           username: rusers(:jeff).username,
           password: 'secret'
@@ -147,12 +145,13 @@ class I18nTest < ActionDispatch::IntegrationTest
          status: 1
         get '/dashboard'
         assert_select 'span', I18n.t('dashboard._node_wiki.new_page_by')
-        assert true
       end
     end
     
     test "should choose i18n for dashboard/_sidebar" do
       available_testing_locales.each do |lang|
+        get "/change_locale/"+lang.to_s
+        follow_redirect!
         post '/user_sessions', user_session: {
           username: rusers(:jeff).username,
           password: 'secret'
@@ -160,12 +159,13 @@ class I18nTest < ActionDispatch::IntegrationTest
         follow_redirect!
         get '/dashboard'
         assert_select 'span', I18n.t('dashboard._sidebar.wiki')
-        assert true
       end
     end
     
     test "should choose i18n for dashboard/_wiki" do
       available_testing_locales.each do |lang|
+        get "/change_locale/"+lang.to_s
+        follow_redirect!
         post '/user_sessions', user_session: {
           username: rusers(:jeff).username,
           password: 'secret'
@@ -173,7 +173,25 @@ class I18nTest < ActionDispatch::IntegrationTest
         follow_redirect!
         get '/dashboard'
         assert_select 'a', I18n.t('dashboard._wiki.more') + " &raquo;"
-        assert true
       end
+    end
+    
+    test "should choose i18n-de and move back to i18n-en" do
+      post '/user_sessions', user_session: {
+        username: rusers(:jeff).username,
+        password: 'secret'
+      }
+      follow_redirect!
+      get '/dashboard'
+      
+      # Set the language as Deutsch
+      lang=:de
+      get_via_redirect "/change_locale/"+lang.to_s
+      assert_equal "#{lang}", "#{I18n.locale}"
+      
+      # Move back to English(Default locale)
+      lang=:en
+      get_via_redirect "/change_locale/"+lang.to_s
+      assert_equal "#{lang}", "#{I18n.locale}"
     end
 end
