215d37ae8bb44ac679e0b2c46009242cf54730f6
Lalith <lalithr95@gmail.com>
2016-08-06 16:13:54 -0400

    add location tag migration columns

--- app/models/location_tag.rb
+++ app/models/location_tag.rb
@@ -1,4 +1,5 @@
 class LocationTag < ActiveRecord::Base
   attr_accessible :lat, :location, :lon, :uid, :country, :state, :city
   belongs_to :drupal_users, :foreign_key => :uid
+  belongs_to :drupal_node, :foreign_key => :nid
 end
