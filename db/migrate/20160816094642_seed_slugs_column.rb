c21294e1f8b2a903ba68b98df3f08d18cf7e5194
Jeffrey Warren <jeff@unterbahn.com>
2016-08-18 15:35:01 -0400

    removing problematic migration for now (#694)

--- db/migrate/20160816094642_seed_slugs_column.rb
+++ db/migrate/20160816094642_seed_slugs_column.rb
@@ -1,5 +1,6 @@
 class SeedSlugsColumn < ActiveRecord::Migration
   def change
-  	DrupalNode.find_each(&:save)
+    # used to seed `friendly_id` slugs, but removed due to issues discussed in https://github.com/publiclab/plots2/issues/691
+    # DrupalNode.find_each(&:save)
   end
 end
