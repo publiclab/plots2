231e7f66adb3820a5dcefa1e0eb2a108c5f772e7
jywarren <jeff@unterbahn.com>
2016-08-19 11:58:02 -0400

    test for insert_extras() helper

---
+++ test/integration/node_insert_extras_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class NodeInsertExtrasTest < ActionDispatch::IntegrationTest
+
+  test "note with inline tagged notes table via insert_extras() helper" do
+    post '/user_sessions', user_session: {
+      username: rusers(:jeff).username,
+      password: 'secret'
+    }
+
+    title = "One more post about balloon mapping"
+
+    post '/notes/create', 
+         title: title,
+         body:  "This is a fascinating post about a balloon mapping event. [notes:test]",
+         tags:  "test"
+
+    follow_redirect!
+    get DrupalNode.last.path
+    
+    assert_select "h1", title
+    assert_select "table.insert-extras"
+  end
+
+end
