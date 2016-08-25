0c385af974bed79a65800f1b5ff162aba0042a62
Jeffrey Warren <jeff@unterbahn.com>
2016-08-25 10:23:37 -0400

    upgrade grid test (#732)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -73,13 +73,43 @@ module ApplicationHelper
       nodes.each do |node|
         output += '    <tr>'
         output += '      <td><a href="' + node.path + '">' + node.title + '</a></td>'
-        output += '      <td>' + (node.has_power_tag('category') ? node.power_tag('status') : '-') + '</td>'
         output += '      <td>' + (node.has_power_tag('status') ? node.power_tag('status') : '-') + '</td>'
         output += '      <td><a href="/profile/' + node.author.username + '">@' + node.author.username + '</a></td>'
         output += '      <td>' + (node.has_power_tag('time') ? node.power_tag('time') : '-') + '</td>'
         output += '      <td>' + (node.has_power_tag('difficulty') ? node.power_tag('difficulty') : '-') + '</td>'
         output += '      <td>' + node.response_count.to_s + ' replications</td>'
         output += '      <td>' + (node.has_power_tag('goal') ? node.power_tag('goal') + ' replications' : '-') + '</td>'
+        output += '    </tr>'
+      end
+      output += '</table>'
+      output
+    end
+    body = body.gsub(/\[upgrades\:(.+)\]/) do |tagname|
+      output  =  '<table class="table insert-extras upgrades-grid">'
+      output += '  <tr>'
+      output += '    <th>Title</th>'
+      output += '    <th>Status</th>'
+      output += '    <th>Author</th>'
+      output += '    <th>Time</th>'
+      output += '    <th>Difficulty</th>'
+      output += '    <th>Builds</th>'
+      output += '    <th>Goal</th>'
+      output += '  </tr>'
+      nodes = DrupalNode.where(status: 1, type: 'note')
+                        .includes(:drupal_node_revision, :drupal_tag)
+                        .where('term_data.name LIKE ?', "upgrade:#{$1}")
+                        .page(params[:page])
+                        .order("node_revisions.timestamp DESC")
+      nodes.each do |node|
+        output += '    <tr>'
+        output += '      <td><a href="' + node.path + '">' + node.title + '</a></td>'
+        output += '      <td>' + (node.has_power_tag('category') ? node.power_tag('status') : '-') + '</td>'
+        output += '      <td>' + (node.has_power_tag('status') ? node.power_tag('status') : '-') + '</td>'
+        output += '      <td><a href="/profile/' + node.author.username + '">@' + node.author.username + '</a></td>'
+        output += '      <td>' + (node.has_power_tag('time') ? node.power_tag('time') : '-') + '</td>'
+        output += '      <td>' + (node.has_power_tag('difficulty') ? node.power_tag('difficulty') : '-') + '</td>'
+        output += '      <td>' + node.response_count.to_s + ' builds</td>'
+        output += '      <td>' + (node.has_power_tag('goal') ? node.power_tag('goal') + ' builds' : '-') + '</td>'
         output += '    </tr>'
       end
       output += '</table>'
