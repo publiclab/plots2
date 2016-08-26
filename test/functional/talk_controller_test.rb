f8428218c96525e76c176deecf1209d60852d83f
Jeffrey Warren <jeff@unterbahn.com>
2016-08-26 13:30:17 -0400

    various style & talk page fixes, new style:minimal power tag (#740)

--- test/functional/talk_controller_test.rb
+++ test/functional/talk_controller_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class TalkControllerTest < ActionController::TestCase
   test "should get show" do
-  	node = node(:about)
-    get :show, id: node.slug[0,50]
+    node = node(:about)
+    get :show, id: node.slug
     assert_response :success
   end
 end
