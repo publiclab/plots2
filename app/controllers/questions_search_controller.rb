60c9a62eea9eb0b9c5033196e2a7c5334d7e0ef9
Sandy <SVern99@aol.com>
2016-09-26 21:07:30 +0000

    replaced pipes in the typeahead method with 2 spaces (&nbsp;)

--- app/controllers/questions_search_controller.rb
+++ app/controllers/questions_search_controller.rb
@@ -40,8 +40,8 @@ class QuestionsSearchController < ApplicationController
       link = "<i data-url='" + match.path(:question) + 
              "' class='fa fa-question-circle'></i> " + match.title
       author = "<span class='pull-right'><i class='fa fa-user'></i> " + match.author.name
-      likes = " | <i class='fa fa-star'></i> " + match.cached_likes.to_s
-      answers = " | <i class='fa fa-comments'></i> " + match.answers.length.to_s + "</span>"
+      likes = " &nbsp; &nbsp; <i class='fa fa-star'></i> " + match.cached_likes.to_s
+      answers = " &nbsp; &nbsp; <i class='fa fa-comments'></i> " + match.answers.length.to_s + "</span>"
       matches << link + author + likes + answers
     end
     render :json => matches
