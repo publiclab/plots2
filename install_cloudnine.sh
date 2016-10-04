557cb4170a618f20d9b1ceac0e75a75f744c3bc7
Ananya Maiti <ananyoevo@gmail.com>
2016-10-04 10:02:19 +0530

    Add solr changes to cloud9 installation script

--- install_cloudnine.sh
+++ install_cloudnine.sh
@@ -2,11 +2,13 @@ read -p "Enter your cloud9 username: " un
 rvm install ruby-2.1.2
 source $(rvm 2.1.2 do rvm env --path)
 sudo apt-get update
-sudo apt-get -y install imagemagick ruby-rmagick
+sudo apt-get -y install imagemagick ruby-rmagick openjdk-7-jre openjdk-7-jdk
 npm install -g bower
 bower install
 bundle install --without production
 cp db/schema.rb.example db/schema.rb
 rake db:setup
+rake sunspot:solr:start
+rake sunspot:reindex
 rake cloud9 username=$un
 echo "Done! Run the application with 'rails s -b \$IP -p \$PORT'"
