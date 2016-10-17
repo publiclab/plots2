4d39b31422eecc0e59d95d7386329f8db8d2bedd
Sebastian Silva <sebastian@fuentelibre.org>
2016-10-17 22:13:28 +0000

    Fix for reindexing utf-8 in Solr

--- app/models/drupal_node_revision.rb
+++ app/models/drupal_node_revision.rb
@@ -2,7 +2,10 @@ class DrupalNodeRevision < ActiveRecord::Base
 
   searchable do
     text :title, boost: 5
-    text :body, :teaser
+    text :body do
+      body.to_s.gsub!(/[[:cntrl:]]/,'')
+    end
+    text :teaser
   end
 
   attr_accessible :title, :body, :nid, :uid, :teaser, :log, :timestamp, :format
