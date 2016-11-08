03f6b4691b2eab1543a65eaf3813fe11cf624e8c
Wenyi Chen <wenyichen@outlook.com>
2016-11-08 10:31:10 +0000

    updated  to  in README.md and install_cloudnine.sh

--- install_cloudnine.sh
+++ install_cloudnine.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get -y install imagemagick ruby-rmagick
 npm install -g bower
 bower install
-bundle install --without production
+bundle install --without production mysql
 cp db/schema.rb.example db/schema.rb
 cp config/database.yml.sqlite.example config/database.yml
 rake db:setup
