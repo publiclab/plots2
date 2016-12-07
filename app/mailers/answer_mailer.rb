8b2ad72e1c495bc3e16bc8a7bdc2db680d4624df
ykl7 <ykl7@users.noreply.github.com>
2016-12-07 22:57:03 +0530

    added note in footer in all mailers (#1075)
    * added note in footer in all mailers
    
    * template fix for footers

--- app/mailers/answer_mailer.rb
+++ app/mailers/answer_mailer.rb
@@ -1,10 +1,13 @@
 class AnswerMailer < ActionMailer::Base
+  helper :application
+  include ApplicationHelper
   default from: "do-not-reply@#{ActionMailer::Base.default_url_options[:host]}"
 
   def notify_question_author(user, answer)
     subject = "[PublicLab] New answer to Question: " + answer.node.title
     @user = user
     @answer = answer
+    @footer = feature('email-footer')
     mail(:to => @user.email, :subject => subject)
   end
 
@@ -12,12 +15,14 @@ class AnswerMailer < ActionMailer::Base
     subject = "[PublicLab] New answer to Question: " + answer.node.title
     @user = user
     @answer = answer
+    @footer = feature('email-footer')
     mail(:to => @user.email, :subject => subject)
   end
 
   def notify_answer_accept(user, answer)
     @user = user
     @answer = answer
+    @footer = feature('email-footer')
     mail(:to => @user.email, :subject => "[PublicLab] Your answer has been accepted")
   end
 
@@ -25,6 +30,7 @@ class AnswerMailer < ActionMailer::Base
     subject = "[PublicLab] #{user.username} liked your answer to: " + answer.node.title
     @user = user
     @answer = answer
+    @footer = feature('email-footer')
     mail(:to => @answer.author.email, :subject => subject)
   end
 end
--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,10 +1,13 @@
 class WelcomeMailer < ActionMailer::Base
+	helper :application
+  include ApplicationHelper
   #default from: "do-not-reply@#{ActionMailer::Base.default_url_options[:host]}"
 
   # PasswordResetMailer.reset_notify(user).deliver 
   def add_to_list(user,list)
     subject = "subscribe"
     @list = list
+    @footer = feature('email-footer')
     mail(:to => list+'+subscribe@googlegroups.com', :subject => subject, :from => user.email).deliver
   end
 
