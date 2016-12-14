fb37c10cf7758b9e9b21fb00e854d91f7c23692f
Mridul Nagpal <mridulnagpal07@gmail.com>
2016-12-15 00:13:39 +0530

    Update login_flow_test.rb

--- test/integration/login_flow_test.rb
+++ test/integration/login_flow_test.rb
@@ -32,9 +32,9 @@ class LoginFlowTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_response :redirect
     assert_redirected_to  '/questions/new?tags=question%3Aquestion&template=question&title=What&redirect=question'
-    # assert_select "input#title" do
-    # assert_select "[value=?]", "What"
-    #end
+    assert_select "input#title" do
+    assert_select "[value=?]", "What"
+    end
   end
 
   test "should redirect to current page when logging in through the header login" do
