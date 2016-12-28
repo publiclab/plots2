961812c2f43801ec26cb4b305d1e9f1617f0d116
Swapnil Gupta <500swapnil@github.com>
2016-12-28 21:10:27 +0530

    Removed assert_select

--- test/functional/wiki_controller_test.rb
+++ test/functional/wiki_controller_test.rb
@@ -140,7 +140,6 @@ class WikiControllerTest < ActionController::TestCase
           id: 'organizers'
     end
     assert_equal flash[:warning] , "This page is <a href='/wiki/power-tags#Locking'>locked</a>, and only <a href='/wiki/moderators'>moderators</a> can edit it."
-    assert_select ".alert", "This page is <a href='/wiki/power-tags#Locking'>locked</a>, and only <a href='/wiki/moderators'>moderators</a> can edit it."
     assert_redirected_to node.path
   end
 
