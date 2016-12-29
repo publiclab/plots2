52d6eb2375f197c48155f74faa0aac214cc124a2
Swapnil Gupta <500swapnil@gmail.com>
2016-12-29 05:49:29 +0530

    Back to 961812c state

--- test/functional/wiki_controller_test.rb
+++ test/functional/wiki_controller_test.rb
@@ -139,7 +139,7 @@ class WikiControllerTest < ActionController::TestCase
       post :edit,
           id: 'organizers'
     end
-    assert_template "wiki/show"
+    assert_equal flash[:warning] , "This page is <a href='/wiki/power-tags#Locking'>locked</a>, and only <a href='/wiki/moderators'>moderators</a> can edit it."
     assert_redirected_to node.path
   end
 
