c6bd9644d49f50a092a753518adf0a44a9fe92a1
pk13055 <pkrockstar7@gmail.com>
2017-01-01 13:26:36 +0530

    Updated change. Change reflected in answer(s) partial(s)

--- app/assets/javascripts/comment_expand.js
+++ app/assets/javascripts/comment_expand.js
@@ -6,7 +6,7 @@ function comment_select(question_id){
 
 function expand_comments(question_id){
   if (comment_select(question_id).length > 0){
-    comment_select(question_id).slice(0,3).show();
+    comment_select(question_id).slice(-3).show();
     $('#answer-' + question_id + '-expand').text('View ' + comment_select(question_id).length + ' more comments');
     if (comment_select(question_id).length == 0){
       $('#answer-' + question_id + '-expand').hide();
