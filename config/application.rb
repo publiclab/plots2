00bfb81ea542b46294dc16901fe0f40cceec260c
sjones88 <steven.g.jones88@gmail.com>
2017-01-03 14:10:16 -0600

    Change to Rails 4.0. Remove assets group in gemfile. Update versions of rails-i18n, sass-rails, coffee-rails for Rails 4.0 compatibility.

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rack/cors'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  Bundler.require(*Rails.groups)
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
