d623e80cdab373dcb451b75bd1070df8fe88d7e7
jywarren <jeff@unterbahn.com>
2017-01-06 13:19:22 -0500

    followup changes to .gitignore and token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Plots2::Application.config.secret_token = 'd2db59377fa49d9c8fa08fa5bb771dd326c0e84b0f1f67d324a0cc7f91ce4e99fb448097c1c9ba030d80ed331833674b5c3f85a08b109ca1684a477ee5934ddb'
+Plots2::Application.config.secret_token = 'CHANGE-THIS-TO-A-SECRET-KEY-AND-DONT-VERSION-TRACK-IT-IN-GIT'
