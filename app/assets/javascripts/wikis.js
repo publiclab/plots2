98adeaeccc618ed705c9860f0bf5605e69ea8a4d
jywarren <jeff@unterbahn.com>
2017-02-03 09:51:27 -0500

    seemingly functional client-side rendering of markdown

--- app/assets/javascripts/wikis.js
+++ app/assets/javascripts/wikis.js
@@ -13,14 +13,20 @@ function setupWiki(node_id, raw) {
             "h1", "h2", "h3", "h4", "h5", "h6", "hr", "i", "img", "ins", "kbd", "li", "main", "ol",
             "p", "pre", "section", "span", "strike", "strong", "sub", "summary", "sup", "table",
             "tbody", "td", "th", "thead", "tr", "u", "ul", 
-            "div", "script", "iframe", "button" // but all classnames are scrubbed
+            "form", "input", "textarea", "div", "script", "iframe", "button"
           ],
           allowedAttributes: {
-            a: ['class', 'id'],
-            div: ['class', 'id']
+            a: ['class', 'id', 'href'],
+            button: ['class', 'id'],
+            div: ['class', 'id'],
+            form: ['class', 'id'],
+            input: ['class', 'id', 'name', 'placeholder'],
+            textarea: ['class', 'id', 'name', 'placeholder'],
+            iframe: ['class', 'id', 'src']
           },
           allowedClasses: {
             button: ['class'],
+            input: ['class'],
             a: ['class'] ,
             div: ['class']
           }
