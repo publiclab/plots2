9cf61a363250fd22168670528f970ad9b4c3da91
Jeffrey Warren <jeff@unterbahn.com>
2017-02-06 17:03:51 -0500

    Update tag.rb (#1244)
    Add `.where(status: 1)` to Tag model method for optimization
    
    basic working subsection replacement
    
    working basic replacement interface!

---
+++ app/assets/javascripts/wikis/replaceWithMarkdown.js
@@ -0,0 +1,34 @@
+// Runs megamark with default whitelist
+function replaceWithMarkdown(element) {
+  var markdown = megamark(
+    element,
+    { 
+      sanitizer: {  
+        allowedTags: [
+          "a", "article", "b", "blockquote", "br", "caption", "code", "del", "details", "div", "em",
+          "h1", "h2", "h3", "h4", "h5", "h6", "hr", "i", "img", "ins", "kbd", "li", "main", "ol",
+          "p", "pre", "section", "span", "strike", "strong", "sub", "summary", "sup", "table",
+          "tbody", "td", "th", "thead", "tr", "u", "ul", 
+          "form", "input", "textarea", "div", "script", "iframe", "button"
+        ],
+        allowedAttributes: {
+          a: ['class', 'id', 'href'],
+          button: ['class', 'id'],
+          div: ['class', 'id'],
+          form: ['class', 'id'],
+          input: ['class', 'id', 'name', 'placeholder'],
+          textarea: ['class', 'id', 'name', 'placeholder'],
+          iframe: ['class', 'id', 'src']
+        },
+        allowedClasses: {
+          button: ['class'],
+          input: ['class'],
+          a: ['class'] ,
+          div: ['class']
+        }
+        //"allowedClasses": "class"
+      }
+    }
+  );
+  return markdown;
+}
