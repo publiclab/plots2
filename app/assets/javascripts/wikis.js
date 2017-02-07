b88d5ef855001b1c79c07d2869ee7d4337586959
jywarren <jeff@unterbahn.com>
2017-02-06 19:19:02 -0500

    basic working subsection replacement

--- app/assets/javascripts/wikis.js
+++ app/assets/javascripts/wikis.js
@@ -1,17 +1,10 @@
 function setupWiki(node_id, raw) {
-
   // insert inline forms
   if (raw) {
     $('#content-raw-markdown').html(shortCodePrompt($('#content-raw-markdown')[0], { submitUrl: '/wiki/replace/' + node_id }));
-
     $('#content').html('');
-
-// THIS DOESN"T WORK BECAUSE IT"S NOT HTML YET
-
-
-    var sections = $('#content-raw-markdown').find('h1,h2,h3,h4,p,ul');
-    sections.each(forEachSection);
-
+    var sections = $('#content-raw-markdown').html().split('\n\n');
+    sections.forEach(forEachSection);
   } else {
     $('#content').html(shortCodePrompt($('#content')[0], { submitUrl: '/wiki/replace/' + node_id }));
   }
@@ -20,53 +13,78 @@ function setupWiki(node_id, raw) {
   $("[rel=tooltip]").tooltip()
   $("[rel=popover]").popover({container: 'body'})
   $('table').addClass('table')
-  
-  $('iframe').css('border','none')
-  
+
   /* add "link" icon to headers */
   $("#content h1, #content h2, #content h3, #content h4").append(function(i,html) {
     return " <small><a href='#" + this.innerHTML.replace(/ /g,'+') + "'><i class='icon fa fa-link'></i></a></small>";
   });
 
-}
+  function forEachSection(markdown, index) {
+    var html     = replaceWithMarkdown(markdown),
+        uniqueId = "section-form-" + index;
+ 
+    $('#content').append(html);
+    var el = $('#content > *:last');
+ 
+    var editLink = "";
+    editLink += "<a class='inline-edit-link inline-edit-link-" + uniqueId + "'><i class='fa fa-pencil'></i></a>";
+ 
+    var formHtml = "<form style='display:none;' class='well' id='" + uniqueId + "'>";
+    formHtml += "<p><b>Edit this section:</b></p>";
+    formHtml += "<p><textarea rows='6' class='form-control'>" 
+    formHtml += markdown + "</textarea></p>";
+    formHtml += "<p><button type='submit' class='btn btn-primary'>Save</button> ";
+    formHtml += " &nbsp; <a class='cancel'>cancel</a>";
+    formHtml += "<small style='color:#aaa;'><i> | In-line editing only works with basic content. For more, edit the whole page.</i></small>";
+    formHtml += "</form>";
+ 
+    // filter? Only p,h1-5,ul?
+    var isMarkdown = markdown.match(/</) === null; // has tags
+ 
+    if (isMarkdown) {
+      el.after(formHtml);
+      var form = $('#' + uniqueId);
+      el.append(editLink);
+      $('.inline-edit-link-' + uniqueId).click(function inlineEditLinkClick(e) {
+        e.preventDefault();
+        form.toggle();
+      });
+      form.find('.cancel').click(function inlineEditCancelClick(e) {
+        e.preventDefault();
+        form.hide();
+      });
+      form.find('button').click(submitSectionForm);
+    }
 
-function forEachSection(index, section) {
-  var markdown = $(section).html(),
-      html     = replaceWithMarkdown(html),
-      uniqueId = "section-form-" + index;
-
-  $('#content').append(markdown);
-  var el = $('#content > *:last');
-console.log(section, el);
-
-  el.append("<div class='well' id='" + uniqueId + "'><p>Markdown:</p><textarea class='form-control'>" + markdown + "</textarea></div>");
-
-  // build a form to attach:
-
-  function onComplete(response) {
-    var message = $('#' + uniqueId + ' .prompt-message');
-    if (response === 'true' || response === true) {
-      message.html('<i class="fa fa-check" style="color:green;"></i>');
-      var input = $('#' + uniqueId + ' .form-control').val();
-      var form = $('#' + uniqueId).before('<p>' + input + '</p>');
-      $('#' + uniqueId + ' .form-control').val('');
-    } else {
+/*
+    function onComplete(response) {
+      var message = $('#' + uniqueId + ' .prompt-message');
+      if (response === 'true' || response === true) {
+        message.html('<i class="fa fa-check" style="color:green;"></i>');
+        var input = $('#' + uniqueId + ' .form-control').val();
+        var form = $('#' + uniqueId).before('<p>' + input + '</p>');
+        $('#' + uniqueId + ' .form-control').val('');
+      } else {
+        message.html('There was an error. Do you need to <a href="/login">log in</a>?');
+      }
+    }
+ 
+    function onFail(response) {
+      var message = $('#' + uniqueId + ' .prompt-message');
       message.html('There was an error. Do you need to <a href="/login">log in</a>?');
     }
-  }
+*/
 
-  function onFail(response) {
-    var message = $('#' + uniqueId + ' .prompt-message');
-    message.html('There was an error. Do you need to <a href="/login">log in</a>?');
-  }
-
-  function submitSectionForm(e) {
-    $.post(options.submitUrl, {
-      before: markdown,
-      after: after
-    })
-     .done(onComplete)
-     .fail(onFail);
+    function submitSectionForm(e) {
+      e.preventDefault();
+console.log('node_id', node_id)
+      $.post("/wiki/replace/" + node_id, {
+        before: markdown,
+        after: form.find('textarea').val()
+      })
+//     .done(onComplete)
+//     .fail(onFail);
+    }
   }
 }
 
