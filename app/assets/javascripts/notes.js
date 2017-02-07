af9bc66bda37d0cb13ee86e804c9d15f651d3c3d
Jeffrey Warren <jeff@unterbahn.com>
2017-02-07 14:44:05 -0500

    Update notes.js

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
 
-  $('#content').hide();
-  $('#content-raw-markdown').show();
+  //$('#content').hide();
+  //$('#content-raw-markdown').show();
 
   // insert inline forms
   /*
@@ -10,7 +10,7 @@ jQuery(document).ready(function($) {
     $('#content-raw-markdown').html(marked($('#content-raw-markdown').html()));
   } else {
   */
-    $('#content').html(shortCodePrompt($('#content')[0], { submitUrl: '/wiki/replace/' + node_id }));
+    //$('#content').html(shortCodePrompt($('#content')[0], { submitUrl: '/wiki/replace/' + node_id }));
   //}
 
   toggleRaw = function() {
