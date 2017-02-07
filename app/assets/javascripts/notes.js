b0d5abac67b15061717966ff82a7fd26a125778b
jywarren <jeff@unterbahn.com>
2017-02-07 14:33:19 -0500

    fix for notes

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -4,12 +4,14 @@ jQuery(document).ready(function($) {
   $('#content-raw-markdown').show();
 
   // insert inline forms
+  /*
   if (raw) {
     $('#content-raw-markdown').html(shortCodePrompt($('#content-raw-markdown')[0], { submitUrl: '/wiki/replace/' + node_id }));
     $('#content-raw-markdown').html(marked($('#content-raw-markdown').html()));
   } else {
+  */
     $('#content').html(shortCodePrompt($('#content')[0], { submitUrl: '/wiki/replace/' + node_id }));
-  }
+  //}
 
   toggleRaw = function() {
     $("#content-raw-markdown").toggle();$("#content").toggle();
