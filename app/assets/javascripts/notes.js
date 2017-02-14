59249258414abf46fcc997b85f10aa82ee49c285
Jeffrey Warren <jeff@unterbahn.com>
2017-02-14 17:34:52 -0500

    fix for tables in questions (#1261)

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -14,17 +14,18 @@ jQuery(document).ready(function($) {
   //}
 
   toggleRaw = function() {
-    $("#content-raw-markdown").toggle();$("#content").toggle();
+    $("#content-raw-markdown").toggle();
+    $("#content").toggle();
   }
 
   /* setup bootstrap behaviors */
-  $("[rel=tooltip]").tooltip()
-  $("[rel=popover]").popover({container: 'body'})
-  $('table').addClass('table')
+  $("[rel=tooltip]").tooltip();
+  $("[rel=popover]").popover({container: 'body'});
+  $('table').addClass('table');
   
   $('iframe').css('border','none')
   
   /* add "link" icon to headers */
-  $("#content-raw-markdown h1, #content-raw-markdown h2, #content-raw-markdown h3, #content-raw-markdown h4, #content h1, #content h2, #content h3, #content h4").append(function(i,html) {return " <small><a href='#" + this.innerHTML.replace(/ /g,'+') + "'><i class='icon fa fa-link'></i></a></small>"})
+  $("#content").find('h1,h2,h3,h4').append(function(i,html) {return " <small><a href='#" + this.innerHTML.replace(/ /g,'+') + "'><i class='icon fa fa-link'></i></a></small>"})
 
 });
