6ae20bd8d475971ca071e5206ebe3c96e8bbb1fc
jywarren <jeff@unterbahn.com>
2017-02-18 15:54:07 -0500

    button dropdown

--- app/assets/javascripts/wikis/buildSectionForm.js
+++ app/assets/javascripts/wikis/buildSectionForm.js
@@ -4,6 +4,14 @@ function buildSectionForm(uniqueId, contents) {
   formHtml += "<p><textarea rows='6' class='form-control'>" 
   formHtml += contents + "</textarea></p>";
   formHtml += "<p><button type='submit' class='btn btn-primary'>Save</button> ";
+
+  formHtml += "<span class='btn-group'>";
+  formHtml += "<button type='submit' class='btn btn-default dropdown-toggle' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>Insert</button> ";
+  formHtml += "  <ul class='dropdown-menu'>";
+  formHtml += "    <li><a href="#">Action</a></li>
+  formHtml += "  </ul>
+  formHtml += "</span>
+
   formHtml += " &nbsp; <a class='cancel'>cancel</a>";
   formHtml += "<small style='color:#aaa;'><i> | <span class='section-message'>In-line editing only works with basic content. For more, edit the whole page.</span></i></small>";
   formHtml += "</form>";
