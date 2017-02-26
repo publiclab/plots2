c5df3b92c1e6658872ca1baeaf844d085a6a237a
Jeffrey Warren <jeff@unterbahn.com>
2017-02-26 15:23:20 -0500

    various cleanup (#1295)
    * various cleanup
    
    * jasmine test fixes
    
    * quick fix of jQuery issue

--- spec/javascripts/wikis_spec.js
+++ spec/javascripts/wikis_spec.js
@@ -26,31 +26,6 @@ describe("Wikis", function() {
 
   });
 
-
-  it("adds callout links", function() {
-
-    //loadFixtures('index.html');
-
-    var html = "@warren";
-
-    expect(addCallouts(html)).toEqual('<a href="/profile/warren">@warren</a>');
-
-    html = "@warren and @liz";
-
-    expect(addCallouts(html)).toEqual('<a href="/profile/warren">@warren</a> and <a href="/profile/liz">@liz</a>');
-
-  });
-
-  it("adds hashtag links", function() {
-
-    //loadFixtures('index.html');
-
-    var html = "#timelapse and #balloon-mapping";
-
-    expect(addHashtags(html)).toEqual('<a href="/tag/timelapse">#timelapse</a> and <a href="/tag/balloon-mapping">#balloon-mapping</a>');
-
-  });
-
   it("adds deep links like example.com#Sub+section", function() {
 
     loadFixtures('content.html');
@@ -64,19 +39,6 @@ describe("Wikis", function() {
     loadFixtures('content.html');
     postProcessContent($('#content'));
     expect($('#content table.table').length).not.toBe(0);
-
-  });
-
-  xit("adds edit links", function() {
-
-    loadFixtures('content.html');
-
-    // insert edit links!!
-    processSection(markdown, selector, node_id);
-
-    $('#content .inline-edit-link:first').click()
-
-    expect($('#content form.inline-edit-form:visible').length).toBe(1);
 
   });
 
