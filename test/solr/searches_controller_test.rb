a70b6cf774a52909f08241a7108c13d8433f73bd
jywarren <jeff@unterbahn.com>
2017-03-03 13:06:14 -0500

    solr staging tests in json, fix for circular reference in solr test

--- test/solr/searches_controller_test.rb
+++ test/solr/searches_controller_test.rb
@@ -15,6 +15,7 @@ class SearchesControllerTest < ActionController::TestCase
     get :test
     assert_not_nil :search
     assert_response :success
+    assert_not_nil JSON.parse(@response.body)
   end
 
   test 'should get index' do
