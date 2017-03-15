4602e1499554b37e0d159a6c727f2ef88729995a
Aayush Gupta <aayushgupta1@users.noreply.github.com>
2017-03-15 12:08:22 +0530

    Update wiki_controller.rb

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -353,6 +353,19 @@ class WikiController < ApplicationController
                          .includes(:drupal_node_revision, :tag)
                          .order("node_revisions.timestamp DESC")
     end
+    if params[:topic]
+      nids = @nodes.collect(&:nid) || []
+      @nodes = DrupalNode.where(status: 1, type: ['page'])
+                         .where('node.nid IN (?)', nids)
+                         .where('(type = "note" OR type = "page" OR type = "map") AND node.status = 1 AND (node.title LIKE ? OR node_revisions.title LIKE ? OR node_revisions.body LIKE ? OR term_data.name = ?)', 
+                           "%"+params[:topic]+"%",
+                           "%"+params[:topic]+"%",
+                           "%"+params[:topic]+"%",
+                           params[:topic])
+                         .includes(:drupal_node_revision, :tag)
+                         .order("node_revisions.timestamp DESC")
+    end
+
     @unpaginated = true
     @topics = [
       "agriculture",
