ff6b2fb3db04658d21f47350eb90b4135360d285
jywarren <jeff@unterbahn.com>
2017-03-29 16:40:23 -0400

    trying not loading fixture

--- spec/javascripts/question_spec.js
+++ spec/javascripts/question_spec.js
@@ -7,7 +7,7 @@ describe("Question", function() {
 
   it("loads question.js", function() {
 
-    loadFixtures('content.html');
+    //loadFixtures('content.html');
     expect(true).toBe(true); // just confirm that things were loaded at all
 
   });
