3695b9364d36cc49885aa51c0ca234dc6ae4a738
jywarren <jeff@unterbahn.com>
2017-03-29 17:21:04 -0400

    tweak test

--- spec/javascripts/question_spec.js
+++ spec/javascripts/question_spec.js
@@ -5,6 +5,17 @@ var editor;
 
 describe("Question", function() {
 
+  beforeEach(function() {
+
+    // for phantomjs running
+    jasmine.getFixtures().fixturesPath="../../spec/javascripts/fixtures";
+
+    // for in-browser running... still doesn't work
+    //jasmine.getFixtures().fixturesPath = 'assets/fixtures';
+    preloadFixtures('content.html');
+
+  });
+
   it("loads question.js", function() {
 
     //loadFixtures('content.html');
