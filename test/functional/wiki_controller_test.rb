efa806eb0b14583e1e3535beb6a79f5c66516b68
jywarren <jeff@unterbahn.com>
2017-04-04 19:40:38 -0400

    two separate fixes

--- test/functional/wiki_controller_test.rb
+++ test/functional/wiki_controller_test.rb
@@ -504,6 +504,17 @@ class WikiControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "abtest: redirects to another page" do
+    wiki = node(:wiki_page)
+    slug = wiki.path.gsub('/wiki/', '')
+    blog = node(:blog)
+    wiki.add_tag("abtest:#{blog.nid}", rusers(:bob))
+    assert_equal wiki.power_tag("abtest"), blog.nid.to_s
+
+    get :show, id: slug
+    assert_response :success
+  end
+
   test "redirect to non-existent page fails gracefully; no redirect" do
     wiki = node(:wiki_page)
     slug = wiki.path.gsub('/wiki/', '')
