8af8a909605f0d4cf1149bcd73f1b88116a6dfc2
Jeffrey Warren <jeff@unterbahn.com>
2017-04-05 11:57:55 -0400

    Update solr_toggle.rb

--- lib/solr_toggle/solr_toggle.rb
+++ lib/solr_toggle/solr_toggle.rb
@@ -1,17 +1,21 @@
 module SolrToggle
 
   def shouldIndexSolr
-    begin
-      if !Sunspot::Rails.configuration.disabled?
-        Node.search do
-          fulltext 'test' # provisional, shouldn't matter?
+    if ENV["DISABLE_SOLR"]
+      false
+    else
+      begin
+        if !Sunspot::Rails.configuration.disabled?
+          Node.search do
+            fulltext 'test' # provisional, shouldn't matter?
+          end
+          true
+        else
+          false
         end
-        true
-      else
+      rescue
         false
       end
-    rescue
-      false
     end
   end
 
