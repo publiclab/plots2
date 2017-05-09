publiclab/plots2

commit 6719063903344193a65c9eea7d5830a9a1897397

Fix all controller files to match style guide (#1355)
* Improve code to match Ruby Style Guide
This make simple code improvements for a more readable code
Closes #1348 (part of)

* Improve code to match Ruby Style Guide

This make simple code improvements for a more readable code of controller files

* Enforce Ruby style guide for model files

Corrects code style to match Ruby Style Guide. All tests passed

* Fixes for migration issue on earlier versions of MySQL

* Update wiki_controller.rb

config/initializers/abstract_mysql2_adapter.rb
@@ -1,0 +1,5 @@
+require 'active_record/connection_adapters/mysql2_adapter'
+
+class ActiveRecord::ConnectionAdapters::Mysql2Adapter
+  NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
+end

app/controllers/answer_like_controller.rb
@@ -1,8 +1,8 @@
 class AnswerLikeController < ApplicationController
-  before_filter :require_user, :only => :likes
+  before_filter :require_user, only: :likes
 
   def show
-    render :json => Answer.find(params[:id]).cached_likes
+    render json: Answer.find(params[:id]).cached_likes
   end
 
   def likes
@@ -16,7 +16,7 @@ class AnswerLikeController < ApplicationController
     end
     @answer.reload
     respond_to do |format|
-      format.js{}
+      format.js {}
     end
   end
 end

app/controllers/answers_controller.rb
@@ -11,8 +11,8 @@ class AnswersController < ApplicationController
     respond_to do |format|
       if current_user && @answer.save
         @answer.answer_notify(current_user)
-        format.html{ redirect_to @node.path(:question), notice: "Answer successfully posted" }
-        format.js{}
+        format.html { redirect_to @node.path(:question), notice: 'Answer successfully posted' }
+        format.js {}
       end
     end
   end
@@ -22,14 +22,14 @@ class AnswersController < ApplicationController
     if @answer.uid == current_user.uid
       @answer.content = params[:body]
       if @answer.save
-        flash[:notice] = "Answer updated"
+        flash[:notice] = 'Answer updated'
         redirect_to @answer.node.path(:question)
       else
         flash[:error] = "Answer couldn't be updated"
         redirect_to @answer.node.path(:question)
       end
     else
-      flash[:error] = "Only the author of the answer can edit it."
+      flash[:error] = 'Only the author of the answer can edit it.'
       redirect_to @answer.node.path(:question)
     end
   end
@@ -37,12 +37,12 @@ class AnswersController < ApplicationController
   def delete
     @answer = Answer.find(params[:id])
     if current_user.uid == @answer.node.uid ||
-      @answer.uid == current_user.uid ||
-      current_user.role == "admin" ||
-      current_user.role == "moderator"
+       @answer.uid == current_user.uid ||
+       current_user.role == 'admin' ||
+       current_user.role == 'moderator'
       respond_to do |format|
         if @answer.delete
-          format.html{ redirect_to @answer.node.path(:question), notice: "Answer deleted" }
+          format.html { redirect_to @answer.node.path(:question), notice: 'Answer deleted' }
           format.js
         else
           flash[:error] = "The answer couldn't be deleted"
@@ -50,7 +50,7 @@ class AnswersController < ApplicationController
         end
       end
     else
-      prompt_login "Only the answer or question author can delete this answer"
+      prompt_login 'Only the answer or question author can delete this answer'
     end
   end
 

app/controllers/application_controller.rb
@@ -9,182 +9,181 @@ class ApplicationController < ActionController::Base
 
   private
 
-    # eventually videos could be a power tag
-    def set_sidebar(type = :generic, data = :all, args = {})
-      args[:note_count] ||= 8
-      if type == :tags # accepts data of array of tag names as strings
-        if params[:controller] == 'questions'
-          @notes = @notes || Tag.find_nodes_by_type(data, 'note', args[:note_count])
-        else
-          @notes = @notes || Tag.find_research_notes(data, args[:note_count])
-        end
-
-        @notes = @notes.where('node.nid != (?)', @node.nid) if @node
-        @wikis = Tag.find_pages(data,10)
-        @videos = Tag.find_nodes_by_type_with_all_tags(['video']+data,'note',8) if args[:videos] && data.length > 1
-        @maps = Tag.find_nodes_by_type(data,'map',20)
-      else # type is generic
-        # remove "classroom" postings; also switch to an EXCEPT operator in sql, see https://github.com/publiclab/plots2/issues/375
-        hidden_nids = Node.joins(:drupal_node_community_tag)
-                                .joins("LEFT OUTER JOIN term_data ON term_data.tid = community_tags.tid")
-                                .select('node.*, term_data.*, community_tags.*')
-                                .where(type: 'note', status: 1)
-                                .where('term_data.name = (?)', 'hidden:response')
-                                .collect(&:nid)
-        if params[:controller] == 'questions'
-          @notes = Node.questions
-                             .joins(:drupal_node_revision)
-        else
-          @notes = Node.research_notes
-                           .joins(:drupal_node_revision)
-                           .order('node.nid DESC')
-                           .paginate(page: params[:page])
-        end
-        @notes = @notes.where('node.nid != (?)', @node.nid) if @node
-        @notes = @notes.where('node_revisions.status = 1 AND node.nid NOT IN (?)', hidden_nids) if hidden_nids.length > 0
-
-        if current_user && (current_user.role == "moderator" || current_user.role == "admin")
-          @notes = @notes.where('(node.status = 1 OR node.status = 4)')
-        elsif current_user
-          @notes = @notes.where('(node.status = 1 OR (node.status = 4 AND node.uid = ?))', current_user.uid)
-        else
-          @notes = @notes.where('node.status = 1')
-        end
-
-        @wikis = Node.order("changed DESC")
-                           .joins(:drupal_node_revision)
-                           .where('node_revisions.status = 1 AND node.status = 1 AND type = "page"')
-                           .limit(10)
-                           .group('node_revisions.nid')
-                           .order('node_revisions.timestamp DESC')
+  # eventually videos could be a power tag
+  def set_sidebar(type = :generic, data = :all, args = {})
+    args[:note_count] ||= 8
+    if type == :tags # accepts data of array of tag names as strings
+      if params[:controller] == 'questions'
+        @notes ||= Tag.find_nodes_by_type(data, 'note', args[:note_count])
+      else
+        @notes ||= Tag.find_research_notes(data, args[:note_count])
       end
-    end
 
-    # non-Authlogic... homebrew
-    def prompt_login(message = I18n.t('application_controller.must_be_logged_in'))
-      flash[:warning] = message
-      redirect_to "/login"
-    end
+      @notes = @notes.where('node.nid != (?)', @node.nid) if @node
+      @wikis = Tag.find_pages(data, 10)
+      @videos = Tag.find_nodes_by_type_with_all_tags(['video'] + data, 'note', 8) if args[:videos] && data.length > 1
+      @maps = Tag.find_nodes_by_type(data, 'map', 20)
+    else # type is generic
+      # remove "classroom" postings; also switch to an EXCEPT operator in sql, see https://github.com/publiclab/plots2/issues/375
+      hidden_nids = Node.joins(:drupal_node_community_tag)
+                        .joins('LEFT OUTER JOIN term_data ON term_data.tid = community_tags.tid')
+                        .select('node.*, term_data.*, community_tags.*')
+                        .where(type: 'note', status: 1)
+                        .where('term_data.name = (?)', 'hidden:response')
+                        .collect(&:nid)
+      @notes = if params[:controller] == 'questions'
+                 Node.questions
+                     .joins(:drupal_node_revision)
+               else
+                 Node.research_notes
+                     .joins(:drupal_node_revision)
+                     .order('node.nid DESC')
+                     .paginate(page: params[:page])
+               end
+      @notes = @notes.where('node.nid != (?)', @node.nid) if @node
+      @notes = @notes.where('node_revisions.status = 1 AND node.nid NOT IN (?)', hidden_nids) unless hidden_nids.empty?
 
-    def current_user_session
-      return @current_user_session if defined?(@current_user_session)
-      @current_user_session = UserSession.find
-    end
-
-    def current_user
-      if not defined?(@current_user)
-        @current_user = current_user_session && current_user_session.record
+      if current_user && (current_user.role == 'moderator' || current_user.role == 'admin')
+        @notes = @notes.where('(node.status = 1 OR node.status = 4)')
+      elsif current_user
+        @notes = @notes.where('(node.status = 1 OR (node.status = 4 AND node.uid = ?))', current_user.uid)
+      else
+        @notes = @notes.where('node.status = 1')
       end
-      # if banned or moderated:
-      if @current_user.try(:drupal_user).try(:status) == 0
-        # Same effect as if the user clicked logout:
-        current_user_session.destroy
-        # Ensures no code will use old @current_user info. Treat the user
-        # as anonymous (until the login process sets @current_user again):
-        @current_user = nil
-      elsif @current_user.try(:drupal_user).try(:status) == 5
-        # Tell the user they are banned. Fails b/c redirect to require below.
-        flash[:warning] = "The user '#{@current_user.username}' has been placed in moderation; please see <a href='https://#{request.host}/wiki/moderators'>our moderation policy</a> and contact <a href='mailto:moderators@#{request.host}'>moderators@#{request.host}</a> if you believe this is in error."
-        # Same effect as if the user clicked logout:
-        current_user_session.destroy
-        # Ensures no code will use old @current_user info. Treat the user
-        # as anonymous (until the login process sets @current_user again):
-        @current_user = nil
-      end
-      @current_user
-    end
 
-    def require_user
-      unless current_user
-        store_location
-        flash[:warning] ||= I18n.t('application_controller.must_be_logged_in_to_access')
-        redirect_to login_url
-        return false
-      end
+      @wikis = Node.order('changed DESC')
+                   .joins(:drupal_node_revision)
+                   .where('node_revisions.status = 1 AND node.status = 1 AND type = "page"')
+                   .limit(10)
+                   .group('node_revisions.nid')
+                   .order('node_revisions.timestamp DESC')
     end
+  end
 
-    def require_no_user
-      if current_user
-        store_location
-        flash[:notice] = I18n.t('application_controller.must_be_logged_out_to_access')
-        redirect_to home_url+'?return_to=' + URI.encode(request.env['PATH_INFO'])
-        return false
-      end
+  # non-Authlogic... homebrew
+  def prompt_login(message = I18n.t('application_controller.must_be_logged_in'))
+    flash[:warning] = message
+    redirect_to '/login'
+  end
+
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+
+  def current_user
+    unless defined?(@current_user)
+      @current_user = current_user_session && current_user_session.record
     end
-
-    def store_location
-      session[:return_to] = request.fullpath
+    # if banned or moderated:
+    if @current_user.try(:drupal_user).try(:status) == 0
+      # Same effect as if the user clicked logout:
+      current_user_session.destroy
+      # Ensures no code will use old @current_user info. Treat the user
+      # as anonymous (until the login process sets @current_user again):
+      @current_user = nil
+    elsif @current_user.try(:drupal_user).try(:status) == 5
+      # Tell the user they are banned. Fails b/c redirect to require below.
+      flash[:warning] = "The user '#{@current_user.username}' has been placed in moderation; please see <a href='https://#{request.host}/wiki/moderators'>our moderation policy</a> and contact <a href='mailto:moderators@#{request.host}'>moderators@#{request.host}</a> if you believe this is in error."
+      # Same effect as if the user clicked logout:
+      current_user_session.destroy
+      # Ensures no code will use old @current_user info. Treat the user
+      # as anonymous (until the login process sets @current_user again):
+      @current_user = nil
     end
+    @current_user
+  end
 
-    def redirect_back_or_default(default)
-      redirect_to(session[:return_to] || default)
-      session[:return_to] = nil
-    end
-
-    def check_and_redirect_node(node)
-      if !node.nil? && node.type[/^redirect\|/]
-        node = Node.find(node.type[/\|\d+/][1..-1])
-        redirect_to node.path, status: 301
-        return true
-      end
+  def require_user
+    unless current_user
+      store_location
+      flash[:warning] ||= I18n.t('application_controller.must_be_logged_in_to_access')
+      redirect_to login_url
       false
     end
+  end
 
-    def alert_and_redirect_moderated
-      if @node.author.status == 0 && !(current_user && (current_user.role == "admin" || current_user.role == "moderator"))
-        flash[:error] = I18n.t('application_controller.author_has_been_banned')
-        redirect_to "/"
-      elsif @node.status == 4 && (current_user && (current_user.role == "admin" || current_user.role == "moderator"))
-        flash[:warning] = "First-time poster <a href='#{@node.author.name}'>#{@node.author.name}</a> submitted this #{time_ago_in_words(@node.created_at)} ago and it has not yet been approved by a moderator. <a class='btn btn-default btn-sm' href='/moderate/publish/#{@node.id}'>Approve</a> <a class='btn btn-default btn-sm' href='/moderate/spam/#{@node.id}'>Spam</a>"
-      elsif @node.status == 4 && (current_user && current_user.id == @node.author.id) && !flash[:first_time_post]
-        flash[:warning] = "Thank you for contributing open research, and thanks for your patience while your post is approved by <a href='/wiki/moderation'>community moderators</a> and we'll email you when it is published. In the meantime, if you have more to contribute, feel free to do so."
-      elsif @node.status != 1 && !(current_user && (current_user.role == "admin" || current_user.role == "moderator"))
-        # if it's spam or a draft
-        # no notification; don't let people easily fish for existing draft titles; we should try to 404 it
-        redirect_to "/"
-      elsif @node.author.status == 5
-        flash.now[:warning] = "The user '#{@node.author.username}' has been placed <a href='https://#{request.host}/wiki/moderators'>in moderation</a> and will not be able to respond to comments."
-      end
+  def require_no_user
+    if current_user
+      store_location
+      flash[:notice] = I18n.t('application_controller.must_be_logged_out_to_access')
+      redirect_to home_url + '?return_to=' + URI.encode(request.env['PATH_INFO'])
+      false
     end
+  end
 
-    # Check the locale set and adjust the locale accordingly
-    def set_locale
-      if cookies[:plots2_locale] && I18n.available_locales.include?(cookies[:plots2_locale].to_sym)
-        lang = cookies[:plots2_locale].to_sym
-      else
-        lang = http_accept_language.compatible_language_from(I18n.available_locales) || I18n.default_locale
-        cookies.permanent[:plots2_locale] = lang
-      end
-      I18n.locale = lang
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+
+  def redirect_back_or_default(default)
+    redirect_to(session[:return_to] || default)
+    session[:return_to] = nil
+  end
+
+  def check_and_redirect_node(node)
+    if !node.nil? && node.type[/^redirect\|/]
+      node = Node.find(node.type[/\|\d+/][1..-1])
+      redirect_to node.path, status: 301
+      return true
     end
+    false
+  end
 
-    def comments_node_and_path
-      if @comment.aid == 0
-        # finding node for node comments
-        @node = @comment.node
-      else
-        # finding node for answer comments
-        @node = @comment.answer.node
-      end
-
-      if params[:type] && params[:type] == 'question'
-        # questions path
-        @path = @node.path(:question)
-      else
-        # notes path
-        @path = @node.path
-      end
+  def alert_and_redirect_moderated
+    if @node.author.status == 0 && !(current_user && (current_user.role == 'admin' || current_user.role == 'moderator'))
+      flash[:error] = I18n.t('application_controller.author_has_been_banned')
+      redirect_to '/'
+    elsif @node.status == 4 && (current_user && (current_user.role == 'admin' || current_user.role == 'moderator'))
+      flash[:warning] = "First-time poster <a href='#{@node.author.name}'>#{@node.author.name}</a> submitted this #{time_ago_in_words(@node.created_at)} ago and it has not yet been approved by a moderator. <a class='btn btn-default btn-sm' href='/moderate/publish/#{@node.id}'>Approve</a> <a class='btn btn-default btn-sm' href='/moderate/spam/#{@node.id}'>Spam</a>"
+    elsif @node.status == 4 && (current_user && current_user.id == @node.author.id) && !flash[:first_time_post]
+      flash[:warning] = "Thank you for contributing open research, and thanks for your patience while your post is approved by <a href='/wiki/moderation'>community moderators</a> and we'll email you when it is published. In the meantime, if you have more to contribute, feel free to do so."
+    elsif @node.status != 1 && !(current_user && (current_user.role == 'admin' || current_user.role == 'moderator'))
+      # if it's spam or a draft
+      # no notification; don't let people easily fish for existing draft titles; we should try to 404 it
+      redirect_to '/'
+    elsif @node.author.status == 5
+      flash.now[:warning] = "The user '#{@node.author.username}' has been placed <a href='https://#{request.host}/wiki/moderators'>in moderation</a> and will not be able to respond to comments."
     end
+  end
 
-    # used for url redirects for friendly_id
-    # currently unused for issues discussed in https://github.com/publiclab/plots2/issues/691
-    def redirect_old_urls
-      # If an old id or a numeric id was used to find the record, then
-      # the request path will not match the notes path, and we should do
-      # a 301 redirect that uses the current friendly id.
-      if request.path != @node.path
-        return redirect_to @node.path, :status => :moved_permanently
-      end
+  # Check the locale set and adjust the locale accordingly
+  def set_locale
+    if cookies[:plots2_locale] && I18n.available_locales.include?(cookies[:plots2_locale].to_sym)
+      lang = cookies[:plots2_locale].to_sym
+    else
+      lang = http_accept_language.compatible_language_from(I18n.available_locales) || I18n.default_locale
+      cookies.permanent[:plots2_locale] = lang
     end
+    I18n.locale = lang
+  end
 
+  def comments_node_and_path
+    @node = if @comment.aid == 0
+              # finding node for node comments
+              @comment.node
+            else
+              # finding node for answer comments
+              @comment.answer.node
+            end
+
+    @path = if params[:type] && params[:type] == 'question'
+              # questions path
+              @node.path(:question)
+            else
+              # notes path
+              @node.path
+            end
+  end
+
+  # used for url redirects for friendly_id
+  # currently unused for issues discussed in https://github.com/publiclab/plots2/issues/691
+  def redirect_old_urls
+    # If an old id or a numeric id was used to find the record, then
+    # the request path will not match the notes path, and we should do
+    # a 301 redirect that uses the current friendly id.
+    if request.path != @node.path
+      redirect_to @node.path, status: :moved_permanently
+    end
+  end
 end

app/controllers/comment_controller.rb
@@ -1,11 +1,10 @@
 class CommentController < ApplicationController
-
   respond_to :html, :xml, :json
-  before_filter :require_user, :only => [:create, :update, :delete]
+  before_filter :require_user, only: [:create, :update, :delete]
 
   def index
     @comments = Comment.paginate(page: params[:page], per_page: 30)
-                             .order('timestamp DESC')
+                       .order('timestamp DESC')
     render template: 'comments/index'
   end
 
@@ -13,7 +12,7 @@ class CommentController < ApplicationController
   # create node comments
   def create
     @node = Node.find params[:id]
-    @comment = @node.add_comment({:uid => current_user.uid,:body => params[:body]})
+    @comment = @node.add_comment(uid: current_user.uid, body: params[:body])
     if current_user && @comment.save
       @comment.notify(current_user)
       respond_with do |format|
@@ -23,17 +22,17 @@ class CommentController < ApplicationController
         else
           format.html do
             if request.xhr?
-              render :partial => "notes/comment", :locals => {:comment => @comment}
+              render partial: 'notes/comment', locals: { comment: @comment }
             else
-              flash[:notice] = "Comment posted."
-              redirect_to @node.path+"#last" # to last comment
+              flash[:notice] = 'Comment posted.'
+              redirect_to @node.path + '#last' # to last comment
             end
           end
         end
       end
     else
-      flash[:error] = "The comment could not be saved."
-      render :text => "failure"
+      flash[:error] = 'The comment could not be saved.'
+      render text: 'failure'
     end
   end
 
@@ -49,11 +48,11 @@ class CommentController < ApplicationController
     if @comment.save
       @comment.answer_comment_notify(current_user)
       respond_to do |format|
-        format.js { render template: "comment/create" }
+        format.js { render template: 'comment/create' }
       end
     else
-      flash[:error] = "The comment could not be saved."
-      render :text => "failure"
+      flash[:error] = 'The comment could not be saved.'
+      render text: 'failure'
     end
   end
 
@@ -66,14 +65,14 @@ class CommentController < ApplicationController
       # should abstract ".comment" to ".body" for future migration to native db
       @comment.comment = params[:body]
       if @comment.save
-        flash[:notice] = "Comment updated."
-        redirect_to @path + "?_=" + Time.now.to_i.to_s
+        flash[:notice] = 'Comment updated.'
+        redirect_to @path + '?_=' + Time.now.to_i.to_s
       else
-        flash[:error] = "The comment could not be updated."
+        flash[:error] = 'The comment could not be updated.'
         redirect_to @path
       end
     else
-      flash[:error] = "Only the author of the comment can edit it."
+      flash[:error] = 'Only the author of the comment can edit it.'
       redirect_to @path
     end
   end
@@ -84,9 +83,9 @@ class CommentController < ApplicationController
     comments_node_and_path
 
     if current_user.uid == @node.uid ||
-      @comment.uid == current_user.uid ||
-      current_user.role == "admin" ||
-      current_user.role == "moderator"
+       @comment.uid == current_user.uid ||
+       current_user.role == 'admin' ||
+       current_user.role == 'moderator'
 
       if @comment.delete
         respond_with do |format|
@@ -96,21 +95,20 @@ class CommentController < ApplicationController
           else
             format.html do
               if request.xhr?
-                render :text => "success"
+                render text: 'success'
               else
-                flash[:notice] = "Comment deleted."
-                redirect_to "/"+@node.path
+                flash[:notice] = 'Comment deleted.'
+                redirect_to '/' + @node.path
               end
             end
           end
         end
       else
-        flash[:error] = "The comment could not be deleted."
-        render :text => "failure"
+        flash[:error] = 'The comment could not be deleted.'
+        render text: 'failure'
       end
     else
-      prompt_login "Only the comment or post author can delete this comment"
+      prompt_login 'Only the comment or post author can delete this comment'
     end
   end
-
 end

app/controllers/editor_controller.rb
@@ -1,23 +1,20 @@
 class EditorController < ApplicationController
-
-  before_filter :require_user, :only => [:post, :rich, :legacy, :editor]
+  before_filter :require_user, only: [:post, :rich, :legacy, :editor]
 
   # main image via URL passed as GET param
   def legacy
     # /post/?i=http://myurl.com/image.jpg
     flash.now[:notice] = "This is the legacy editor. For the new rich editor, <a href='/editor'>click here</a>."
     if params[:i]
-      @image = Image.new({
-        :remote_url => params[:i],
-        :uid => current_user.uid
-      })
-      flash[:error] = "The image could not be saved." unless @image.save!
+      @image = Image.new(remote_url: params[:i],
+                         uid: current_user.uid)
+      flash[:error] = 'The image could not be saved.' unless @image.save!
     end
     if params[:n] && !params[:body] # use another node body as a template
       node = Node.find(params[:n])
       params[:body] = node.body if node
     end
-    if params[:tags] && params[:tags].include?("question:")
+    if params[:tags] && params[:tags].include?('question:')
       redirect_to "/questions/new?#{request.env['QUERY_STRING']}"
     else
       render template: 'editor/post'
@@ -29,13 +26,13 @@ class EditorController < ApplicationController
   end
 
   def post
-    if params[:tags] && params[:tags].include?("question:")
+    if params[:tags] && params[:tags].include?('question:')
       redirect_to "/questions/new?#{request.env['QUERY_STRING']}"
-    elsif params[:legacy] || params[:template] == "event"
+    elsif params[:legacy] || params[:template] == 'event'
       legacy
     else
       rich
-      render "/editor/rich"
+      render '/editor/rich'
     end
   end
 
@@ -49,5 +46,4 @@ class EditorController < ApplicationController
       params[:body] = node.body if node
     end
   end
-
 end

commit a4eaa9d33269f0057a6ac4668ec3a0c5e4e0ae7c

Fixes #1392 - `rake db:setup` fails for new installs
The culprit was an initializer introduced in #1355 to support MySQL 5.7

config/initializers/abstract_mysql2_adapter.rb
@@ -1,5 +1,9 @@
-require 'active_record/connection_adapters/mysql2_adapter'
+begin
+    require 'active_record/connection_adapters/mysql2_adapter'
 
-class ActiveRecord::ConnectionAdapters::Mysql2Adapter
-  NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
+    class ActiveRecord::ConnectionAdapters::Mysql2Adapter
+      NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
+    end
+rescue Exception=>e
+    puts 'Not running mysql today.'
 end