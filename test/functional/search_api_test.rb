687379b1a81d10d725f1320bdbee2a56c9ce9de1
vagrant <vagrant@precise64.(none)>
2017-05-09 21:44:00 +0000

    Added test/functional/search_api_test.rb

---
+++ test/functional/search_api_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class SearchApiTest < ActiveSupport::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Rails.application
+  end
+
+  test 'search notes functionality' do
+    get '/api/srch/notes?srchString=Blog'
+    assert last_response.ok?
+
+    # Expected search pattern
+    pattern = {
+      srchParams: {
+        srchString: 'Blog',
+        seq: false,
+      }.ignore_extra_keys!
+    }.ignore_extra_keys!
+
+    matcher = JsonExpressions::Matcher.new(pattern)
+
+    assert matcher =~ JSON.parse(last_response.body)
+
+    # check for actual Blog note, too
+    # assert_equal ___, node(:blog)
+
+  end
+
+end
