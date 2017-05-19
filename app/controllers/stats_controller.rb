12d56f0db5a18df258462d2f4efe59ea09e7ac1b
Jeffrey Warren <jeff@unterbahn.com>
2017-05-19 12:18:35 -0400

    Update stats_controller.rb

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -18,7 +18,7 @@ class StatsController < ApplicationController
                                .where(timestamp: @start.to_i..@end.to_i)
                                .count - @notes # because notes each have one revision
     @people = User.where(created_at: @start..@end)
-                  .joins(:drupal_users)
+                  .joins('INNER JOIN users ON users.uid = rusers.id')
                   .where(status: 1)
                   .count
   end
@@ -37,7 +37,7 @@ class StatsController < ApplicationController
                                       .where(timestamp: @time.to_i - 1.weeks.to_i..@time.to_i)
                                       .count
     @weekly_members = User.where(created_at: @time - 1.weeks..@time)
-                          .joins(:drupal_users)
+                          .joins('INNER JOIN users ON users.uid = rusers.id')
                           .where(status: 1)
                           .count
     @monthly_notes = Node.select(%i[created type status])
@@ -47,7 +47,7 @@ class StatsController < ApplicationController
                                        .where(timestamp: @time.to_i - 1.months.to_i..@time.to_i)
                                        .count
     @monthly_members = User.where(created_at: @time - 1.months..@time)
-                           .joins(:drupal_users)
+                           .joins('INNER JOIN users ON users.uid = rusers.id')
                            .where(status: 1)
                            .count
 
