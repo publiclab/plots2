43d86285c6d52d29eb9528489b5b814041ad20b1
David Days <david.c.days@gmail.com>
2017-05-24 02:21:33 -0400

    Web Api and initial testing

---
+++ test/functional/web_api_controller_test.rb
@@ -0,0 +1,16 @@
+# def index
+
+
+require 'test_helper'
+
+class WebApiControllerTest < ActionController:: TestCase
+
+  def setup
+    activate_authlogic
+  end
+
+  test 'web_api should return index.html' do
+    get :index
+    assert_response :success
+  end
+end
