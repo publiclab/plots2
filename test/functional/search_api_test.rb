5b9bf9de9176531881d20ddffeb363e86864ab20
silenda <ml.endale@gmail.com>
2017-05-26 18:54:27 +0200

    testssearch api methods responses

--- test/functional/search_api_test.rb
+++ test/functional/search_api_test.rb
@@ -7,6 +7,30 @@ class SearchApiTest < ActiveSupport::TestCase
     Rails.application
   end
 
+  test 'search all functionality' do
+    get '/api/srch/all?srchString=Blog'
+    assert last_response.ok?
+
+    # Expected search pattern
+    pattern = {
+      srchParams: {
+        srchString: 'Blog',
+        seq: nil,
+      }.ignore_extra_keys!
+    }.ignore_extra_keys!
+
+    matcher = JsonExpressions::Matcher.new(pattern)
+
+    json = JSON.parse(last_response.body)
+
+    assert_equal node(:blog).path, json['items'][0]['docUrl']
+    assert_equal "Blog post",       json['items'][0]['docTitle']
+    assert_equal 13,                json['items'][0]['docId']
+
+    assert matcher =~ json
+
+  end
+
   test 'search notes functionality' do
     get '/api/srch/notes?srchString=Blog'
     assert last_response.ok?
@@ -26,7 +50,67 @@ class SearchApiTest < ActiveSupport::TestCase
     assert_equal node(:blog).path, json['items'][0]['docUrl']
     assert_equal "Blog post",       json['items'][0]['docTitle']
     assert_equal 13,                json['items'][0]['docId']
-    
+
+    assert matcher =~ json
+
+  end
+
+  test 'search profiles functionality' do
+    get '/api/srch/profiles?srchString=Blog'
+    assert last_response.ok?
+
+    # Expected search pattern
+    pattern = {
+      srchParams: {
+        srchString: 'Blog',
+        seq: nil,
+      }.ignore_extra_keys!
+    }.ignore_extra_keys!
+
+    matcher = JsonExpressions::Matcher.new(pattern)
+
+    json = JSON.parse(last_response.body)
+
+    assert matcher =~ json
+
+  end
+
+  test 'search questions functionality' do
+    get '/api/srch/questions?srchString=Blog'
+    assert last_response.ok?
+
+    # Expected search pattern
+    pattern = {
+      srchParams: {
+        srchString: 'Blog',
+        seq: nil,
+      }.ignore_extra_keys!
+    }.ignore_extra_keys!
+
+    matcher = JsonExpressions::Matcher.new(pattern)
+
+    json = JSON.parse(last_response.body)
+
+    assert matcher =~ json
+
+  end
+
+  test 'search tags functionality' do
+    get '/api/srch/tags?srchString=Blog'
+    assert last_response.ok?
+
+    # Expected search pattern
+    pattern = {
+      srchParams: {
+        srchString: 'Blog',
+        seq: nil,
+      }.ignore_extra_keys!
+    }.ignore_extra_keys!
+
+    matcher = JsonExpressions::Matcher.new(pattern)
+
+    json = JSON.parse(last_response.body)
+
     assert matcher =~ json
 
   end
