8e5cb45893357186918c1388fd1d5bcaf8b11cac
StlMaris123 <stellamarisnjage@gmail.com>
2017-06-08 14:52:17 +0300

    configure gem mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,4 +44,7 @@ Plots2::Application.configure do
     host: 'www.example.com'
   }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025  }
+
 end
