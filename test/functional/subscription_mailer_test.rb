806445d96773ec3744cde291bb232d7b2e3f1c05
StlMaris123 <stellamarisnjage@gmail.com>
2017-06-10 16:31:38 +0300

    subscription mail tests

--- test/functional/subscription_mailer_test.rb
+++ test/functional/subscription_mailer_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class SubscriptionMailerTest < ActionMailer::TestCase
   test "notify_tag_added" do
-    mail = SubscriptionMailer.notify_tag_added
-    assert_equal "Notify tag added", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = SubscriptionMailer.notify_tag_added(user = User.first, node = Node.first)
+    assert_equal "New Tag Added", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["do-not-reply@www.example.com"], mail.from
+    assert_match user.username, mail.body.encoded
   end
 
 end
