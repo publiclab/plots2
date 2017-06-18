71d3ebb8ba111e1f870f22757f438a79c0394735
StlMaris123 <stellamarisnjage@gmail.com>
2017-06-18 22:10:53 +0300

    list users following this tag but not given tags tests

---
+++ test/models/tag_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+class TagTest < ActiveSupport::TestCase
+  test "it returns users following this tag but not given tags" do
+    tag_followers = [1,4,5]
+    following_given_tags_ids = [4]
+    assert_equal [1,5], tag_followers.reject { |userid| following_given_tags_ids.include? userid  }
+  end
+end
