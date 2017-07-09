66c65beb6d7e8641f197161e53e2eba2dc2c46a7
Jeffrey Warren <jeff@unterbahn.com>
2017-07-09 18:17:56 -0400

    Update 20170704165711_add_user_bio_and_token.rb (#1516)

--- db/migrate/20170704165711_add_user_bio_and_token.rb
+++ db/migrate/20170704165711_add_user_bio_and_token.rb
@@ -7,7 +7,7 @@ class AddUserBioAndToken < ActiveRecord::Migration
 
     # copy bios into new fields for non-spam users
     DrupalUsers.where('status != 0').each do |u|
-      if u.name == u.name.encode('UTF-8') # exclude non-UTF-8 names
+      unless u.name.multibyte? # exclude non-latin names
         user = u.user
         if user and defined? :u.status then
           user.status = u.status
