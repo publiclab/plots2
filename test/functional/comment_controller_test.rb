40e246c8cb625af8ae075b301a7fcae29ccb3f28
aspriya <ashanpriyadarshana@gmail.com>
2017-07-22 15:54:38 +0530

    check notification mails work on a wiki page comments

--- test/functional/comment_controller_test.rb
+++ test/functional/comment_controller_test.rb
@@ -300,32 +300,11 @@ class CommentControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test 'should send mail to tag followers in the wiki comment' do
-    UserSession.create(rusers(:bob))
-    xhr :post, :create,
-        id: node(:wiki_page).nid,
-        body: 'Wiki page #selection_six',
-        type: 'page'
-    assert ActionMailer::Base.deliveries.collect(&:to).include?([rusers(:newcomer).email])
-    # tag followers can be found in tag_selection.yml
-  end
-
-  test 'should send mail to multiple tag followers in the wiki comment' do
-    UserSession.create(rusers(:bob))
-    xhr :post, :create,
-        id: node(:wiki_page).nid,
-        body: 'Wiki page #everything #selection_six',
-        type: 'page'
-    assert ActionMailer::Base.deliveries.collect(&:to).include?([rusers(:newcomer).email])
-    assert ActionMailer::Base.deliveries.collect(&:to).include?([rusers(:moderator).email])
-    # tag followers can be found in tag_selection.yml
-  end
-
   test 'should send notification email upon a new wiki comment' do
-    UserSession.create(rusers(:bob))
+    UserSession.create(rusers(:jeff))
     xhr :post, :create,
         id: node(:wiki_page).nid,
-        body: 'A new comment',
+        body: 'A comment by Jeff on a wiki page of author bob',
         type: 'page'
     assert ActionMailer::Base.deliveries.collect(&:subject).include?("New comment on 'Wiki page title'")
   end
