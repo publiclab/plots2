d2d05601c76e2ffecab0016b005e64572888554e
Jeffrey Warren <jeff@unterbahn.com>
2017-09-19 11:17:36 -0400

    Update node_insert_extras_test.rb

--- test/integration/node_insert_extras_test.rb
+++ test/integration/node_insert_extras_test.rb
@@ -35,7 +35,6 @@ class NodeInsertExtrasTest < ActionDispatch::IntegrationTest
 
     assert_select 'table.notes-grid-test'
     assert_select 'table.activity-grid-test'
-    assert_select 'table.activity-grid-test tr.title'
     assert_select 'table.upgrades-grid-test'
     assert_select 'table.notes-grid-shouldnt', false
 
