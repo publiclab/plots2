1a58fc1b6c0d5229acbf6ef16fb4f3cbb732daa7
Jeffrey Warren <jeff@unterbahn.com>
2017-09-22 12:25:00 -0400

    Update typeahead_service_test.rb

--- test/solr/typeahead_service_test.rb
+++ test/solr/typeahead_service_test.rb
@@ -5,13 +5,6 @@ class TypeaheadServiceTest < ActiveSupport::TestCase
   
   test 'running TypeaheadService.notes' do
     result = TypeaheadService.new.notes('blog')
-    #assert_equal [], TypeaheadService.new.notes('')
-
-    search = Node.search
-    assert_not_nil search.results
-    assert search.results.length > 0
-    assert_equal [], search.results
-    
     assert_true solrAvailable
     assert_not_nil result
     assert_equal result.length, 2
