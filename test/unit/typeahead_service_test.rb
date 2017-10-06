1f5296f1de41eb92f828abd30882883f78b17dbb
Jeffrey Warren <jeff@unterbahn.com>
2017-10-06 12:24:13 -0400

    Update typeahead_service_test.rb

--- test/unit/typeahead_service_test.rb
+++ test/unit/typeahead_service_test.rb
@@ -3,14 +3,14 @@ require 'test_helper'
 class TypeaheadServiceTest < ActiveSupport::TestCase
   include SolrToggle
   
-  test 'running TypeaheadService.notes with no Solr' do
+  test 'running TypeaheadService.new.notes with no Solr' do
     result = TypeaheadService.new.notes('blog')
     assert_false solrAvailable
     assert_not_nil result
     assert_equal 1, result.length
   end
   
-  test 'running TypeaheadService.notes with no Solr' do
+  test 'running TypeaheadService.new.profiles with no Solr' do
     result = TypeaheadService.new.profiles('adm')
     assert_false solrAvailable
     assert_not_nil result
