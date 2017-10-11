3e4f22c7af883118e7eea66f884fb6e307a058f9
gauravano <sachdeva.gaurav1997@gmail.com>
2017-10-11 21:43:33 +0530

    draft text for welcome updated

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,23 +1,3 @@
-$(document).ready(function() {
-
-    // Get the modal
-    var modal = document.getElementById('myModal');
-
-// Get the <span> element that closes the modal
-    var spang = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-    spang.onclick = function () {
-        modal.style.display = "none";
-    };
-
-// When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-});
 
 
 (function() {
