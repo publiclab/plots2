9d8b29f63fbbdc2eaf1702274e7cf9fe62855747
Sagarpreet Chadha <chadha.sagarpreet97@gmail.com>
2017-10-19 01:51:08 +0530

    Changes done - fixes #1692
    1.) Indentation done .
    2.) Error removed

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -24,16 +24,24 @@ class SubscriptionMailer < ActionMailer::Base
     mail(to: node.author.email, subject: subject).deliver
   end
 
-  def notify_tag_added(node, tag, current_user)
+ def notify_tag_added(node, tag, current_user)
     @tag = tag
     @node = node
     @current_user = current_user
-    given_tags = node.tags.reject { |t| t == tag}
+    given_tags = node.tags.reject { |t| t == tag} 
     users_to_email = tag.followers_who_dont_follow_tags(given_tags)
-    users_to_email.each do |user|
+    users_with_everything_tag = Tag.followers('everything')
+    final_users_ids = nil 
+    if(!users_to_email.nil? && !users_with_everything_tag.nil?)
+      final_users_ids = users_to_email.pluck(:uid) - users_with_everything_tag(:user_id)
+    elsif(!users_to_email.nil?) 
+      final_users_ids = users_to_email.pluck(:uid)
+    end
+    final_users_to_email = User.find(final_users_ids) 
+    final_users_to_email.each do |user|
       @user = user
       unless user.id == current_user.id 
-        mail(to: user.email, subject: "New tag added on #{node.title}").deliver 
+         mail(to: user.email, subject: "New tag added on #{node.title}").deliver 
       end
     end
     @footer = feature('email-footer')
