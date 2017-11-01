761913b8da3a8ab00fbdb0a97835a0b2f6ea1896
Sagarpreet Chadha <chadha.sagarpreet97@gmail.com>
2017-11-02 00:25:41 +0530

    Resolves issue #882 part of #607
    1.)  set_liking(value)  method removed .

--- app/controllers/like_controller.rb
+++ app/controllers/like_controller.rb
@@ -27,51 +27,11 @@ class LikeController < ApplicationController
 
   # for the current user, register as liking the given node
   def create
-    render json: set_liking(true)
+    render json: Node.like(params[:id] , current_user)
   end
 
   # for the current user, remove the like from the given node
   def delete
-    render json: set_liking(false)
-  end
-
-  private
-
-  # this ought to go in a model, such as node_selection, and pass :uid and :nid
-  # then it could also be tested independently in a unit test
-  def set_liking(value)
-    # scope like variable outside the transaction
-    like = nil
-    count = nil
-    # Check like status and update like and cache in an atomic transaction
-    ActiveRecord::Base.transaction do
-      # Create the entry if it isn't already created.
-      like = NodeSelection.where(user_id: current_user.uid,
-                                 nid: params[:id]).first_or_create
-      like.liking = value
-
-      # Check if the value changed.
-      if like.liking_changed?
-        node = Node.find(params[:id])
-        if like.liking
-          # it might be good to pull this out of the transaction to reduce
-          # locking time, but all these vars will have to be rescoped
-          if node.type == 'note'
-            SubscriptionMailer.notify_note_liked(node, like.user)
-          end
-          count = 1
-          node.cached_likes = node.cached_likes + 1
-        else
-          count = -1
-          node.cached_likes = node.cached_likes - 1
-        end
-
-        # Save the changes.
-        node.save!
-        like.save!
-      end
-    end
-
-    count
+    render json: Node.unlike(params[:id] , current_user)
   end
 end
