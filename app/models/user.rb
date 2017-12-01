4ef8068661a5f3e1c28ad1876bfbe37f0367101b
Sagarpreet Chadha <chadha.sagarpreet97@gmail.com>
2017-12-01 15:17:21 +0530

    Social Media feature added

--- app/models/user.rb
+++ app/models/user.rb
@@ -139,6 +139,22 @@ class User < ActiveRecord::Base
     user_tags.collect(&:value).include?(tagname)
   end
 
+   # power tags have "key:value" format, and should be searched with a "key:*" wildcard
+  def self.user_has_power_tag(key , user_id)
+    all_key_tags_ids = Tag.where('name LIKE ?' , key + ':%').collect(&:tid)
+    tids = TagSelection.where('user_id = ? AND tid IN (?)', user_id, all_key_tags_ids)
+    !tids.nil? 
+  end
+
+  def self.get_value_of_power_tag(key , user_id)
+    all_key_tags = Tag.where('name LIKE ?' , key + ':%') 
+    all_key_tags_ids = all_key_tags.collect(&:tid)
+    tids = TagSelection.where('user_id = ? AND tid IN (?)', user_id, all_key_tags_ids).collect(&:tid) 
+    tname = Tag.find(tids.first)
+    tvalue = tname.name.partition(':').last  
+    tvalue
+  end 
+
   def subscriptions(type = :tag)
     if type == :tag
       TagSelection.find_all_by_user_id uid, conditions: { following: true }
