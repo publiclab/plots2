6518b29b84484cb537ca62bda0c72e7197c18af8
Sagarpreet Chadha <chadha.sagarpreet97@gmail.com>
2017-12-06 23:20:07 +0530

    Updated constant.rb

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,6 +3,9 @@ module Callouts
   HASHTAG = /(\s)\#([:a-zA-Z0-9_-]+)/
   PRETTYLINKMD = '\1[@\2](/profile/\2)'
   HASHLINKMD = '\1[#\2](/tag/\2)'
+  HASHTAGNUMBER = /(\s)\#([:0-9]+)/
+  NODELINKMD = '\1[#\2](/n/\2)'
   PRETTYLINKHTML = '\1<a href="/profile/\2">@\2</a>'
   HASHLINKHTML = '\1<a href="/tag/\2">#\2</a>'
+  NODELINKHTML = '\1<a href="/n/\2">#\2</a>'
 end
