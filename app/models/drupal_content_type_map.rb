104d56772ca9c77ab2369795d13c68914bea58cc
jywarren <jeff@unterbahn.com>
2017-12-07 18:52:09 -0500

    truncation in model

--- app/models/drupal_content_type_map.rb
+++ app/models/drupal_content_type_map.rb
@@ -12,6 +12,10 @@ class DrupalContentTypeMap < ActiveRecord::Base
                    lat_column_name: :lat,
                    lng_column_name: :lng
 
+  def before_save
+    self.field_publication_date_value = self.field_publication_date_value.slice(0, 19)
+  end
+
   def tms
     field_tms_url_value
   end
