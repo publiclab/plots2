f4d01792db67b75d2377cc3b05d6d95d74e6e2e8
Sagarpreet Chadha <chadha.sagarpreet97@gmail.com>
2017-12-14 15:18:27 +0530

    admins can delete user tag

--- app/controllers/user_tags_controller.rb
+++ app/controllers/user_tags_controller.rb
@@ -62,7 +62,7 @@ class UserTagsController < ApplicationController
           @user_tag = @user_tag.first 
       end 
       if current_user.role == 'admin' || params[:id] == current_user.id
-        if (!@user_tag.nil? && @user_tag.user == current_user)
+        if (!@user_tag.nil? && @user_tag.user == current_user) || (!@user_tag.nil? && current_user.role == 'admin')
           UserTag.where(uid: params[:id] , value: params[:name]).destroy_all    
           message = I18n.t('user_tags_controller.tag_deleted')
           output[:status] = true
