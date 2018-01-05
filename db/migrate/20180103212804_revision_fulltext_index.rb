c5053d2edffdb5e6bf0821edd188e6c8339b2974
jywarren <jeff@unterbahn.com>
2018-01-05 14:38:04 -0500

    updated schema example

--- db/migrate/20180103212804_revision_fulltext_index.rb
+++ db/migrate/20180103212804_revision_fulltext_index.rb
@@ -1,9 +1,9 @@
 class RevisionFulltextIndex < ActiveRecord::Migration
   def up
-    add_index :node_revisions, [:body, :title], name: 'revision_fulltext', type: :fulltext if ActiveRecord::Base.connection.adapter_name == 'Mysql2'
+    add_index :node_revisions, [:body, :title], type: :fulltext if ActiveRecord::Base.connection.adapter_name == 'Mysql2'
   end
 
   def down
-    remove_index :node_revisions, [:body, :title], name: 'revision_fulltext' if ActiveRecord::Base.connection.adapter_name == 'Mysql2'
+    remove_index :node_revisions, [:body, :title] if ActiveRecord::Base.connection.adapter_name == 'Mysql2'
   end
 end
