0ba020c1055f28ded09c583a0e7b658e793f7e9c
jywarren <jeff@unterbahn.com>
2018-01-05 13:25:13 -0500

    rev title fulltext index

--- db/migrate/20180103212804_revision_fulltext_index.rb
+++ db/migrate/20180103212804_revision_fulltext_index.rb
@@ -1,9 +1,11 @@
 class RevisionFulltextIndex < ActiveRecord::Migration
   def up
     add_index :node_revisions, :body, name: 'revision_body_fulltext', type: :fulltext if ActiveRecord::Base.connection.adapter_name == 'Mysql2'
+    add_index :node_revisions, :title, name: 'revision_title_fulltext', type: :fulltext if ActiveRecord::Base.connection.adapter_name == 'Mysql2'
   end
 
   def down
     remove_index :node_revisions, :body, name: 'revision_body_fulltext' if ActiveRecord::Base.connection.adapter_name == 'Mysql2'
+    remove_index :node_revisions, :title, name: 'revision_title_fulltext' if ActiveRecord::Base.connection.adapter_name == 'Mysql2'
   end
 end
