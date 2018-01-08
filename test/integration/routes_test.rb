965f314cda0212eed18a254512b10dcb95caa909
Sagarpreet Chadha <chadha.sagarpreet97@gmail.com>
2018-01-09 02:55:35 +0530

    Develop integration tests for all routes based on provided sitemap list #1846 (#1916)
    * Created routes_test.rb
    
    * Updated routes_test.rb

---
+++ test/integration/routes_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class RoutesTest < ActionDispatch::IntegrationTest
+
+	def setup
+      activate_authlogic
+   	end
+
+	test "test signup route" do
+	    assert_routing({ path: '/signup', method: :get }, { controller: 'users', action: 'new' })
+	end
+
+	test "test user create route" do
+	    assert_routing({ path: '/register', method: :post }, { controller: 'users', action: 'create' })
+	end
+
+	test "test people list route" do 
+		assert_routing({ path: '/people', method: :get }, { controller: 'users', action: 'list' })
+	end
+
+	test "test profile page route" do 
+		assert_routing({ path: '/profile/jeff', method: :get }, { controller: 'users', action: 'profile' , id: 'jeff' })
+	end
+
+	test "edit profile route when user logged out redirects to profile page" do	
+    	assert_routing({ path: '/profile/jeff/edit', method: :get }, { controller: 'users', action: 'edit' , id: 'jeff' })	
+        get '/profile/jeff/edit'
+        assert_response 302 	#error code 302 is for REDIRECT : https://stackoverflow.com/questions/23788331/http-error-code-302-when-calling-https-webservice
+        follow_redirect!
+        assert_equal '/profile/jeff' , path
+	end
+
+	test "edit profile route when user logged in " do
+		UserSession.create(users(:jeff)) 	
+    	assert_routing({ path: '/profile/jeff/edit', method: :get }, { controller: 'users', action: 'edit' , id: 'jeff' })	
+        get '/profile/jeff/edit'	
+        assert_equal '/profile/jeff/edit' , path
+	end
+
+end
