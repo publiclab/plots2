496ba6f4938d42e0db64f3d3360dd4a1142bc162
Diksha2008 <dikshamanchanda20@gmail.com>
2018-01-11 20:13:38 +0530

    inline grid for wiki pages created

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -36,6 +36,7 @@ module ApplicationHelper
     body = NodeShared.notes_map_by_tag(body)
     body = NodeShared.people_grid(body)
     body = NodeShared.graph_grid(body)
+    body = NodeShared.wikis_grid(body)
     body
   end
 
