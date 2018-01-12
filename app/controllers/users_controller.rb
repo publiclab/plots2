5cdb9a4c8604e4080c9f406dc888210ac4e715be
Sagarpreet Chadha <chadha.sagarpreet97@gmail.com>
2018-01-12 17:24:54 +0530

    Maps added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -101,6 +101,12 @@ class UsersController < ApplicationController
 
   def profile
     @user = DrupalUser.find_by(name: params[:id])
+    if !params[:lat].nil? && !params[:lon].nil?
+      lat = "lat:"+params[:lat].to_s
+      lon = "lon:"+params[:lon].to_s
+      UserTag.new(uid: @user.id , value: lat).save! ;
+      UserTag.new(uid:  @user.id  , value: lon).save! ;
+    end
     @profile_user = User.find_by(username: params[:id])
     @title = @user.name
     @notes = Node.research_notes
@@ -125,9 +131,20 @@ class UsersController < ApplicationController
 
     # User's social links
     @github = @profile_user.social_link("github")
-    @twitter = @profile_user.social_link("twitter")
+    @twitter = @profile_user.social_link("twitter") 
     @facebook = @profile_user.social_link("facebook")
-    @instagram = @profile_user.social_link("instagram")
+    @instagram = @profile_user.social_link("instagram") 
+
+    @map_lat = nil
+    @map_lon = nil 
+    @map_blurred = nil 
+    if(@profile_user.has_power_tag("lat") && @profile_user.has_power_tag("lon"))
+      @map_lat = @profile_user.get_value_of_power_tag("lat").to_f
+      @map_lon = @profile_user.get_value_of_power_tag("lon").to_f
+      if(@profile_user.has_power_tag("blurred"))
+        @map_blurred = @profile_user.get_value_of_power_tag("blurred")
+      end
+    end
 
     if @user.status == 0
       if current_user && (current_user.role == "admin" || current_user.role == "moderator")
