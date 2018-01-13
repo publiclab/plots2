8f2895a49fd9f82fa250c6cb0c3d32b16aad1036
Ankit Bhardwaj <bhardwajankit438@gmail.com>
2018-01-13 10:53:37 +0530

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -160,8 +160,6 @@ class User < ActiveRecord::Base
   def get_value_of_power_tag(key)
     tname = self.user_tags.where('value LIKE ?' , key + ':%') 
     tvalue = tname.first.name.partition(':').last  
-    puts "value"
-    puts tvalue
     tvalue
   end 
   
