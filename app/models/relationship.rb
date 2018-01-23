aa2308646e6c58830ab305920de9fcc2806ad8f9
Sea <30443703+seafr@users.noreply.github.com>
2018-01-23 18:11:00 +0000

    Add validations to Relationship model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,4 +2,6 @@ class Relationship < ActiveRecord::Base
   attr_accessible :followed_id, :follower_id
   belongs_to :follower, class_name: 'User'
   belongs_to :followed, class_name: 'User'
+  validates :followed_id, presence: true
+  validates :follower_id, presence: true
 end
