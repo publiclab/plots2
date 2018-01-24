4f04a408534a03cd37b686ddec4ff52950ba272b
Naman Gupta <01namangupta@gmail.com>
2018-01-24 22:30:13 +0530

    Opened port 25 in container to docker host (for SMTP)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,9 +57,12 @@ Plots2::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   # config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.sendmail_settings = {
-    location: '/usr/sbin/sendmail',
-    arguments: '-i'
+  
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  :address => "smtp",
+  :port => 25,
   }
 
   # Enable threaded mode
