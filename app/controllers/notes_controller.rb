0da5e21878b4e472c143b260225ae66b7c3b4861
ViditChitkara <chitkara.vidit@gmail.com>
2018-02-11 19:55:04 +0530

    minor tweaks

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -294,7 +294,7 @@ class NotesController < ApplicationController
     @wikis = Node.limit(10)
       .where(type: 'page', status: 1)
       .order('nid DESC')
-    @notes = Node.where(type: 'note', status: 1, created: Time.now.to_i - 20.weeks.to_i..Time.now.to_i)
+    @notes = Node.where(type: 'note', status: 1, created: Time.now.to_i - 1.weeks.to_i..Time.now.to_i)
     @unpaginated = true
     render template: 'notes/index'
   end
