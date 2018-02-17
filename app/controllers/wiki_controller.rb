2766cf17e4db5fc38e57b1821e22bcf3e97525ab
milindra <milindrakanwar@gmail.com>
2018-02-17 20:00:41 +0530

    issue 2310 first time commit

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -336,7 +336,7 @@ class WikiController < ApplicationController
     @title = I18n.t('wiki_controller.well_liked_wiki_pages')
     @wikis = Node.limit(40)
       .order('node.cached_likes DESC')
-      .where("status = 1 AND nid != 259 AND (type = 'page' OR type = 'tool' OR type = 'place') AND cached_likes > 0")
+      .where("status = 1 AND nid != 259 AND (type = 'page' OR type = 'tool' OR type = 'place') AND cached_likes >= 0")
     render template: 'wiki/index'
   end
 
