65bd0c7703c521725165848b74f9544b4ad766f3
Naman Jain <33549103+namanjain1812@users.noreply.github.com>
2018-03-16 22:30:29 +0530

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@ class User < ActiveRecord::Base
 
   acts_as_authentic do |c|
     c.openid_required_fields = %i(nickname email)
-    c.validates_format_of_email_field_options = { with: /@/ }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    c.validates_format_of_email_field_options = { with: VALID_EMAIL_REGEX }
     c.crypto_provider = Authlogic::CryptoProviders::Sha512
   end
 
