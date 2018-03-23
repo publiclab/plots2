0af60fcba9e051523241f7ab53cdc991b6bb2a66
Sidharth <bansal.sidharth2996@gmail.com>
2018-03-23 16:02:20 +0530

    Added figaro gem

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '199741036945-04g070p58u6m81bi4gq3l5r6dsfkfvap', 'XEcBrIHNHqTtko0lgBvWD7Cd', skip_jwt: true
+  provider :google_oauth2, ENV["google_app_key"],ENV["google_app_secret"] , skip_jwt: true
 end
