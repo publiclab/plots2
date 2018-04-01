e5c0705265e1345b7743a70bc2b629999b8fe3ca
jywarren <jeff@unterbahn.com>
2018-04-01 16:59:49 -0400

    revised helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,7 +15,7 @@ module ApplicationHelper
   def emojify(content)
     h(content).to_str.gsub(/:([\w+-]+):/) do |match|
       if emoji = Emoji.find_by_alias($1)
-        %(<img alt="#$1" src="#{image_path("emoji/#{emoji.image_filename}")}" style="vertical-align:middle" width="20" height="20" />)
+        %(<img class="emoji" alt="#$1" src="#{image_path("emoji/#{emoji.image_filename}")}" style="vertical-align:middle" width="20" height="20" />)
       else
         match
       end
