10c5a611ab581fb2fb1e8591ae0e762ae227eba8
jywarren <jeff@unterbahn.com>
2018-04-01 19:08:01 -0400

    no escape!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,13 +13,13 @@ module ApplicationHelper
   end
 
   def emojify(content)
-    h(content).to_str.gsub(/:([\w+-]+):/) do |match|
+    content.to_str.gsub(/:([\w+-]+):/) do |match|
       if emoji = Emoji.find_by_alias($1)
         %(<img class="emoji" alt="#$1" src="#{image_path("emoji/#{emoji.image_filename}")}" style="vertical-align:middle" width="20" height="20" />)
       else
         match
       end
-    end.html_safe if content.present?
+    end if content.present?
   end
   
   def feature(title)
