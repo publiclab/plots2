66aff9d67820dc0fb9c4336566fb930dd2326de6
souravirus <souravsahoo66@yahoo.com>
2018-05-17 23:09:12 +0530

    Added and modified few config files according to railsdiff.org

--- config/application.rb
+++ config/application.rb
@@ -50,11 +50,6 @@ module Plots2
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
 
-    # Enforce whitelist mode for mass assignment.
-    # This will create an empty whitelist of attributes available for mass-assignment for all models
-    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
-    # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
@@ -104,8 +99,5 @@ module Plots2
     config.paths.add File.join('app','api'), glob: File.join('**', '*.rb')
     config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
 
-    # Allow mass assignments
-    config.active_record.whitelist_attributes = false
-    config.active_record.raise_in_transactional_callbacks = true
   end
 end
