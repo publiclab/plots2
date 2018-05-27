36b5c533611616e9bc19bb8846fde372e754e403
ViditChitkara <chitkara.vidit@gmail.com>
2018-05-27 23:57:52 +0530

    modified docker-compose.yml for redis

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,9 @@
+sidekiq_config = { url: ENV['JOB_WORKER_URL'] }
+
+Sidekiq.configure_server do |config|
+  config.redis = sidekiq_config
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = sidekiq_config
+end
