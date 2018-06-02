f5507309e6fa5699f0b13a9118439de404bf0fab
souravirus <souravsahoo66@yahoo.com>
2018-06-03 03:13:02 +0530

    Removed the depreciation warning related to dynamic :controller and :action segment in routes

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,7 @@ Plots2::Application.routes.draw do
   get 'login' => "user_sessions#new",      :as => :login
   get 'logout' => "user_sessions#destroy", :as => :logout
   get 'logoutRemotely' => 'user_sessions#logout_remotely'
+  get 'users' => 'users#index'
   post 'register' => 'users#create'
   get 'reset' => 'users#reset'
   post 'reset' => 'users#reset'
@@ -112,9 +113,14 @@ Plots2::Application.routes.draw do
   get 'n/:id' => 'notes#shortlink'
   get 'i/:id' => 'images#shortlink'
   get 'p/:id' => 'users#shortlink'
+  get 'notes' => 'notes#index'
   get 'notes/raw/:id' => 'notes#raw'
   get 'notes/popular' => 'notes#popular'
   get 'notes/liked' => 'notes#liked'
+  get 'notes/image/:id' => 'notes#image'
+  post 'notes/delete/:id' => 'notes#delete'
+  post 'notes/update/:id' => 'notes#update'
+  post 'notes/update/:id' => 'notes#edit'
   post 'notes/create' => 'notes#create'
 
   get 'places' => 'notes#places'
@@ -210,6 +216,7 @@ Plots2::Application.routes.draw do
   delete 'map/delete/:id' => 'map#delete'
   get 'map/:name/:date' => 'map#show'
   get 'archive' => 'map#index'
+  get 'stats/range' => 'stats#range'
   get 'stats' => 'stats#index'
   get 'stats/range/:start/:end' => 'stats#range'
   get 'stats/subscriptions' => 'stats#subscriptions'
@@ -237,10 +244,13 @@ Plots2::Application.routes.draw do
   get 'admin/migrate/:id' => 'admin#migrate'
   get 'admin/moderate/:id' => 'admin#moderate'
   get 'admin/unmoderate/:id' => 'admin#unmoderate'
+  get 'admin/publish_comment/:id' => 'admin#publish_comment'
+  post 'admin/mark_comment_spam/:id' => 'admin#mark_comment_spam'
 
   get 'post' => 'editor#post'
   get 'legacy' => 'editor#legacy'
   get 'editor' => 'editor#editor'
+  get 'editor/rich/(:n)' => 'editor#rich'
   post 'images/create' => 'images#create'
   put 'note/add' => 'legacy#note_add'
   put 'page/add' => 'legacy#page_add'
@@ -268,7 +278,10 @@ Plots2::Application.routes.draw do
 
 
   get 'comment/answer_create/:aid' => 'comment#answer_create'
+  get 'comment/delete/:id' => 'comment#delete'
+  post 'comment/update/:id' => 'comment#update'
   post 'comment/make_answer/:id' => 'comment#make_answer'
+  post 'comment/create/:id' => 'comment#create'
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
@@ -318,5 +331,4 @@ Plots2::Application.routes.draw do
   #handling omniauth callbacks
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 
-  match ':controller(/:action(/:id))(.:format)', via: [:get, :post]
 end
