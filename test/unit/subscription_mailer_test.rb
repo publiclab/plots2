9a7ef78af203e67bbeccc59bbee6624a65d9c5ca
Naman Gupta <01namangupta@gmail.com>
2018-06-03 00:56:06 +0530

    Added migrations

--- test/unit/subscription_mailer_test.rb
+++ test/unit/subscription_mailer_test.rb
@@ -12,13 +12,8 @@ class SubscriptionMailerTest < ActionMailer::TestCase
 
     email = ActionMailer::Base.deliveries.last
     assert_equal ["do-not-reply@#{request_host}"], email.from
-<<<<<<< HEAD
-    assert_equal ["do-not-reply@#{request_host}"], email.to
-    assert_equal '[PublicLab] ' + node.title, email.subject
-=======
     assert_equal [subscribers.values.last[:user].email], email.to
     assert_equal '[PublicLab] ' + node.title + ' (#' + node.id.to_s + ') ', email.subject
->>>>>>> Improved tests
     assert email.body.include?("Public Lab contributor <a href='https://#{request_host}/profile/#{node.author.name}'>#{node.author.name}</a> just posted a new research note")
   end
 
@@ -32,13 +27,9 @@ class SubscriptionMailerTest < ActionMailer::TestCase
 
     email = ActionMailer::Base.deliveries.last
     assert_equal ["do-not-reply@#{request_host}"], email.from
-<<<<<<< HEAD
-    assert_equal ["do-not-reply@#{request_host}"], email.to
-    assert_equal '[PublicLab] Question: ' + node.title, email.subject
-=======
     assert_equal [subscribers.values.last[:user].email], email.to
     assert_equal '[PublicLab] Question: ' + node.title + ' (#' + node.id.to_s + ') ', email.subject
->>>>>>> Improved tests
+
     assert email.body.include?("Public Lab contributor <a href='https://#{request_host}/profile/#{node.author.name}'>#{node.author.name}</a> just asked a question")
   end
 
