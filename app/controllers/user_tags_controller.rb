1a3f83f3144ef6176462ee03460a2082d492334f
Sidharth <bansal.sidharth2996@gmail.com>
2018-06-03 14:07:11 +0530

    CodeClimate Error Fixed

--- app/controllers/user_tags_controller.rb
+++ app/controllers/user_tags_controller.rb
@@ -20,7 +20,7 @@ class UserTagsController < ApplicationController
             @output[:errors] << I18n.t('user_tags_controller.tag_already_exists')
             exist = true
           end
- 
+
           unless exist
             user_tag = user.user_tags.build(value: name)
             if user_tag.save
@@ -45,7 +45,7 @@ class UserTagsController < ApplicationController
       else
         flash[:notice] = I18n.t('user_tags_controller.tag_created', tag_name: @output[:saved][0][0]).html_safe
       end
-      redirect_to '/profile/' + user.username
+      redirect_to URI.parse('/profile/' + user.username).path
     end
   end
 
@@ -56,15 +56,15 @@ class UserTagsController < ApplicationController
     }
     message = ''
 
-    begin  
+    begin
       @user_tag = UserTag.where(uid: params[:id], value: params[:name])
       if(!@user_tag.nil?)
-          @user_tag = @user_tag.first 
-      end 
-  
+          @user_tag = @user_tag.first
+      end
+
       if current_user.role == 'admin' || params[:id].to_i == current_user.id
         if (!@user_tag.nil? && @user_tag.user == current_user) || (!@user_tag.nil? && current_user.role == 'admin')
-          UserTag.where(uid: params[:id] , value: params[:name]).destroy_all    
+          UserTag.where(uid: params[:id] , value: params[:name]).destroy_all
           message = I18n.t('user_tags_controller.tag_deleted')
           output[:status] = true
         else
