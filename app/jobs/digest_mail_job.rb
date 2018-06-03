382a0b7e1405676dba9afab5cc522dc619259494
ViditChitkara <chitkara.vidit@gmail.com>
2018-06-03 21:45:14 +0530

    minor changes

--- app/jobs/digest_mail_job.rb
+++ app/jobs/digest_mail_job.rb
@@ -3,6 +3,6 @@ class DigestMailJob < ActiveJob::Base
   # This is a separate job for sending digest mails.This job could be enqueued for executing task asynchronously.
   def perform
     users = User.includes(:user_tags).references(:user_tags).where('user_tags.value=?', 'digest:weekly').all
-    users.each &:send_digest_email
+    users.each(&:send_digest_email)
   end
 end
