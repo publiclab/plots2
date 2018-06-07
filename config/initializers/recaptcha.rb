c015b30e6094d1400226bc1357db01a1af7b70d0
Gregor Destree <gregordestree@gmail.com>
2018-06-07 14:24:19 -0400

    Edited the if statement

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 # config/initializers/recaptcha.rb
-if Rails.env == "production"
+if Rails.env.production?
   Recaptcha.configure do |config|
     config.site_key  = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
     config.secret_key = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
