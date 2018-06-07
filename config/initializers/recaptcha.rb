2548b428a91ad63be30229d742eda1c6267cae55
Gregor Destree <gregordestree@gmail.com>
2018-06-07 15:17:50 -0400

    Edited ENV variable, and added fallback

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,8 +1,8 @@
 # config/initializers/recaptcha.rb
 if Rails.env.production?
   Recaptcha.configure do |config|
-    config.site_key = ENV.fetch("RECAPTCHA_SITE_KEY")
-    config.secret_key = ENV.fetch("RECAPTCHA_SECRET_KEY")
+    config.site_key = ENV["RECAPTCHA_SITE_KEY"] || '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
+    config.secret_key = ENV["RECAPTCHA_SECRET_KEY"] || '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
     # Uncomment the following line if you are using a proxy server:
     # config.proxy = 'http://myproxy.com.au:8080'
   end
