420f94fbd38716f4a580afedd0e393dd58799fbd
Sidharth <bansal.sidharth2996@gmail.com>
2018-06-09 11:41:46 +0530

    user session controller modified

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -9,36 +9,39 @@ class UserSessionsController < ApplicationController
     auth = request.env['omniauth.auth']
     if auth
       # Find an identity here
-      @user_tag = UserTag.find_with_omniauth(auth)
-
-      if @user_tag.nil?
-        # If no identity was found, create a brand new one here
-        @user_tag = UserTag.create_with_omniauth(auth)
-      end
+      @identity = UserTag.find_with_omniauth(auth)
 
       if signed_in?
-        if @identity.user == current_user
-          # User is signed in so they are trying to link an identity with their
-          # account. But we found the identity and the user associated with it
-          # is the current user. So the identity is already associated with
-          # this user. So let's display an error message.
-          redirect_to root_url, notice: "Already linked that account!"
-        else
+        if @identity.nil?
+          # If no identity was found, create a brand new one here
+          @identity = UserTag.create_with_omniauth(auth, current_user.id)
           # The identity is not associated with the current_user so lets
           # associate the identity
           @identity.user = current_user
           @identity.save
-          redirect_to root_url, notice: "Successfully linked that account!"
+          redirect_to root_url, notice: "Successfully linked to your account!"
+        elsif @identity.user == current_user
+          # User is signed in so they are trying to link an identity with their
+          # account. But we found the identity and the user associated with it
+          # is the current user. So the identity is already associated with
+          # this user. So let's display an error message.
+          redirect_to root_url, notice: "Already linked to your account!"
+        else
+          # User is signed in so they are trying to link an identity with their
+          # account. But we found the identity and a different user associated with it
+          # ,which is not the current user. So the identity is already associated with
+          # that user. So let's display an error message.
+          redirect_to root_url, notice: "Already linked to another account!"
         end
       else
-        if @identity.user.present?
+        if @identity && @identity.user.present?
           # The identity we found had a user associated with it so let's
           # just log them in here
           self.current_user = @identity.user
           redirect_to root_url, notice: "Signed in!"
         else
           # No user associated with the identity so we need to create a new one
-          redirect_to new_user_url, notice: "Please finish registering"
+          redirect_to root_url, notice: "Please finish registering"
         end
       end
 
