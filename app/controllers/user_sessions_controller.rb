5d145b7278ad8a390735d985bdd28b5202dac682
Sidharth <bansal.sidharth2996@gmail.com>
2018-06-09 08:47:28 +0530

    User_sessions_controller modified

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,75 +6,113 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-    params[:user_session][:username] = params[:openid] if params[:openid] # second runthrough must preserve username
-    username = params[:user_session][:username] if params[:user_session]
-    @user = User.find_by(username: username)
+    auth = request.env['omniauth.auth']
+    if auth
+      # Find an identity here
+      @user_tag = UserTag.find_with_omniauth(auth)
 
-    # try finding by email, if that exists
-    if @user.nil? && !User.where(email: username).empty?
-      @user = User.find_by(email: username)
-      params[:user_session][:username] = @user.username
-    end
+      if @user_tag.nil?
+        # If no identity was found, create a brand new one here
+        @user_tag = UserTag.create_with_omniauth(auth)
+      end
 
-    if @user.nil?
-      flash[:warning] = "There is nobody in our system by that name, are you sure you have the right username?"
-      redirect_to '/login'
-    elsif params[:user_session].nil? || @user&.drupal_user&.status == 1
-      # an existing Rails user
-      if params[:user_session].nil? || @user
-        if @user&.crypted_password.nil? # the user has not created a pwd in the new site
-          params[:user_session][:openid_identifier] = 'https://old.publiclab.org/people/' + username + '/identity' if username
-          params[:user_session].delete(:password)
-          params[:user_session].delete(:username)
-          params[:openid] = username # pack up username for second runthrough
+      if signed_in?
+        if @identity.user == current_user
+          # User is signed in so they are trying to link an identity with their
+          # account. But we found the identity and the user associated with it
+          # is the current user. So the identity is already associated with
+          # this user. So let's display an error message.
+          redirect_to root_url, notice: "Already linked that account!"
+        else
+          # The identity is not associated with the current_user so lets
+          # associate the identity
+          @identity.user = current_user
+          @identity.save
+          redirect_to root_url, notice: "Successfully linked that account!"
         end
-        @user_session = UserSession.new(username: params[:user_session][:username], 
-                                        password: params[:user_session][:password], 
-                                        remember_me: params[:user_session][:remember_me])
-        saved = @user_session.save do |result|
-          if result
-            # replace this with temporarily saving pwd in session,
-            # and automatically saving it in the user record after login is completed
-            if current_user.crypted_password.nil? # the user has not created a pwd in the new site
-              flash[:warning] = I18n.t('user_sessions_controller.create_password_for_new_site')
-              redirect_to '/profile/edit'
-            else
-              flash[:notice] = I18n.t('user_sessions_controller.logged_in')
-              if session[:openid_return_to] # for openid login, redirects back to openid auth process
-                return_to = session[:openid_return_to]
-                session[:openid_return_to] = nil
-                redirect_to return_to
-              elsif session[:return_to]
-                return_to = session[:return_to]
-                session[:return_to] = nil
-                redirect_to return_to
-              elsif params[:return_to]
-                redirect_to params[:return_to]
-              else
-                redirect_to '/dashboard'
-              end
-            end
-          else
-            # Login failed; probably bad password. 
-            # Errors will display on login form:
-            render action: 'new'
-          end
-        end
-      else # not a native user
-        if !DrupalUser.find_by(name: username).nil?
-          # this is a user from the old site who hasn't registered on the new site
-          redirect_to controller: :users, action: :create, user: { openid_identifier: username }
-        else # totally new user!
-          flash[:warning] = I18n.t('user_sessions_controller.sign_up_to_join')
-          redirect_to '/signup'
+      else
+        if @identity.user.present?
+          # The identity we found had a user associated with it so let's
+          # just log them in here
+          self.current_user = @identity.user
+          redirect_to root_url, notice: "Signed in!"
+        else
+          # No user associated with the identity so we need to create a new one
+          redirect_to new_user_url, notice: "Please finish registering"
         end
       end
-    elsif params[:user_session].nil? || @user&.drupal_user&.status == 5
-      flash[:error] = I18n.t('user_sessions_controller.user_has_been_moderated', username: @user.username).html_safe
-      redirect_to '/'
+
     else
-      flash[:error] = I18n.t('user_sessions_controller.user_has_been_banned', username: @user.username).html_safe
-      redirect_to '/'
+      params[:user_session][:username] = params[:openid] if params[:openid] # second runthrough must preserve username
+      username = params[:user_session][:username] if params[:user_session]
+      @user = User.find_by(username: username)
+
+      # try finding by email, if that exists
+      if @user.nil? && !User.where(email: username).empty?
+        @user = User.find_by(email: username)
+        params[:user_session][:username] = @user.username
+      end
+
+      if @user.nil?
+        flash[:warning] = "There is nobody in our system by that name, are you sure you have the right username?"
+        redirect_to '/login'
+      elsif params[:user_session].nil? || @user&.drupal_user&.status == 1
+        # an existing Rails user
+        if params[:user_session].nil? || @user
+          if @user&.crypted_password.nil? # the user has not created a pwd in the new site
+            params[:user_session][:openid_identifier] = 'https://old.publiclab.org/people/' + username + '/identity' if username
+            params[:user_session].delete(:password)
+            params[:user_session].delete(:username)
+            params[:openid] = username # pack up username for second runthrough
+          end
+          @user_session = UserSession.new(username: params[:user_session][:username],
+                                          password: params[:user_session][:password],
+                                          remember_me: params[:user_session][:remember_me])
+          saved = @user_session.save do |result|
+            if result
+              # replace this with temporarily saving pwd in session,
+              # and automatically saving it in the user record after login is completed
+              if current_user.crypted_password.nil? # the user has not created a pwd in the new site
+                flash[:warning] = I18n.t('user_sessions_controller.create_password_for_new_site')
+                redirect_to '/profile/edit'
+              else
+                flash[:notice] = I18n.t('user_sessions_controller.logged_in')
+                if session[:openid_return_to] # for openid login, redirects back to openid auth process
+                  return_to = session[:openid_return_to]
+                  session[:openid_return_to] = nil
+                  redirect_to return_to
+                elsif session[:return_to]
+                  return_to = session[:return_to]
+                  session[:return_to] = nil
+                  redirect_to return_to
+                elsif params[:return_to]
+                  redirect_to params[:return_to]
+                else
+                  redirect_to '/dashboard'
+                end
+              end
+            else
+              # Login failed; probably bad password.
+              # Errors will display on login form:
+              render action: 'new'
+            end
+          end
+        else # not a native user
+          if !DrupalUser.find_by(name: username).nil?
+            # this is a user from the old site who hasn't registered on the new site
+            redirect_to controller: :users, action: :create, user: { openid_identifier: username }
+          else # totally new user!
+            flash[:warning] = I18n.t('user_sessions_controller.sign_up_to_join')
+            redirect_to '/signup'
+          end
+        end
+      elsif params[:user_session].nil? || @user&.drupal_user&.status == 5
+        flash[:error] = I18n.t('user_sessions_controller.user_has_been_moderated', username: @user.username).html_safe
+        redirect_to '/'
+      else
+        flash[:error] = I18n.t('user_sessions_controller.user_has_been_banned', username: @user.username).html_safe
+        redirect_to '/'
+      end
     end
   end
 
