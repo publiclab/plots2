bcb34b7aa3ef5a50db955cd722d0933551af03ed
Sidharth <bansal.sidharth2996@gmail.com>
2018-06-17 23:29:12 +0530

    Comments added

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
+#this file is used to store the variable needed for omniauth login and signup
 Rails.application.config.middleware.use OmniAuth::Builder do
   #the provider is google_oauth2 and the app-key of google developers app is stored in OAUTH_GOOGLE_APP_KEY
   #the app-secret of google developers app is stored in variable OAUTH_GOOGLE_APP_SECRET
