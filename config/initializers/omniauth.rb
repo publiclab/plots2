950539c8e7f89bfd5761616e6458adb804ae293b
Sidharth Bansal <bansal.sidharth2996@gmail.com>
2018-06-21 21:56:52 +0530

    Added github omniauth configurations (#2873)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,4 +3,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #the provider is google_oauth2 and the app-key of google developers app is stored in OAUTH_GOOGLE_APP_KEY
   #the app-secret of google developers app is stored in variable OAUTH_GOOGLE_APP_SECRET
   provider :google_oauth2, ENV["OAUTH_GOOGLE_APP_KEY"],ENV["OAUTH_GOOGLE_APP_SECRET"] , skip_jwt: true
+  #For provider github, app_id is stored in
+  provider :github, ENV["OAUTH_GITHUB_APP_KEY"], ENV["OAUTH_GITHUB_APP_SECRET"], { scope: 'user:email' }
 end
