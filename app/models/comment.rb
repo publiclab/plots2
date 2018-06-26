2e5b35d80f5d7e83cd14dc93d1fb426263f17f28
Vidit <chitkara.vidit@gmail.com>
2018-06-27 03:05:28 +0530

    Toopltip added to show users who reacted on a comment (#2926)
    * toopltip added to show users who reacted on a comment
    
    * code climate fixes and tests written

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -204,6 +204,22 @@ class Comment < ApplicationRecord
     likes.group(:emoji_type).count
   end
 
+  def user_reactions_map
+    likes_map = likes.includes(:user).group_by(&:emoji_type)
+    user_like_map={}
+    likes_map.each do |reaction, likes|
+      users = []
+      likes.each do |like|
+        users << like.user.name
+      end
+
+      emoji_type = reaction.underscore.humanize.downcase
+      users_string = (users.length > 1 ? users[0..-2].join(", ")+" and "+users[-1] : users[0]) + " reacted with " + emoji_type + " emoji"
+      user_like_map[reaction] = users_string
+    end
+    user_like_map
+  end
+
   def self.receive_mail(message)
     node_id = message.subject[/#([\d]+)/, 1] #This took out the node ID from the subject line
     puts node_id
