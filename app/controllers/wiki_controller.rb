2a5365da285da0b6939b980977099b5b1b921b45
gauravano <sachdeva.gaurav1997@gmail.com>
2018-06-28 15:14:04 +0530

    code climate

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -118,7 +118,7 @@ class WikiController < ApplicationController
   end
 
   def new
-    if current_user && current_user.first_time_poster
+    if current_user &.first_time_poster
       flash[:notice] = "Please post a question or other content before editing the wiki. Click <a href='https://publiclab.org/notes/tester/04-23-2016/new-moderation-system-for-first-time-posters'>here</a> to learn why."
       redirect_to '/'
       return
