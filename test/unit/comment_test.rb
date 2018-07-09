10aae5322b8e756b332583a87785505490a380a3
Naman Gupta <01namangupta@gmail.com>
2018-07-10 01:32:23 +0530

    Extended reply by email feature to reply to answer  (#2982)
    * Added feature to add comment to answer by reply by mail
    
    * Minor codeclimate fix
    
    * Fixed rubocop ofences
    
    * Fixed rubocop ofences
    
    * Fixed rubocop ofences

--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -223,7 +223,7 @@ class CommentTest < ActiveSupport::TestCase
   end
 
 
-  test 'should parse incoming mail from gmail service correctly' do
+  test 'should parse incoming mail from gmail service correctly and add comment' do
     require 'mail'
     mail = Mail.read('test/incoming_test_emails/gmail/incoming_gmail_email.eml')
     node = Node.last
@@ -231,10 +231,15 @@ class CommentTest < ActiveSupport::TestCase
     Comment.receive_mail(mail)
     f = File.open('test/incoming_test_emails/gmail/final_parsed_comment.txt', 'r')
     comment = Comment.last
+    user_email = mail.from.first
     assert_equal comment.comment, f.read
+    assert_equal comment.nid, node.id
+    assert_equal comment.message_id, mail.message_id
+    assert_equal comment.comment_via, 1
+    assert_equal User.find(comment.uid).email, user_email
   end
 
-  test 'should parse incoming mail from yahoo service correctly' do
+  test 'should parse incoming mail from yahoo service correctly and add comment' do
     require 'mail'
     mail = Mail.read('test/incoming_test_emails/yahoo/incoming_yahoo_email.eml')
     node = Node.last
@@ -242,7 +247,47 @@ class CommentTest < ActiveSupport::TestCase
     Comment.receive_mail(mail)
     f = File.open('test/incoming_test_emails/yahoo/final_parsed_comment.txt', 'r')
     comment = Comment.last
+    user_email = mail.from.first
     assert_equal comment.comment, f.read
+    assert_equal comment.nid, node.id
+    assert_equal comment.message_id, mail.message_id
+    assert_equal comment.comment_via, 1
+    assert_equal User.find(comment.uid).email, user_email
+  end
+
+  test 'should parse incoming mail from gmail service correctly and add answer comment' do
+    require 'mail'
+    mail = Mail.read('test/incoming_test_emails/gmail/incoming_gmail_email.eml')
+    answer = Answer.last
+    mail.subject = "Re: (#a#{answer.id})"
+    Comment.receive_mail(mail)
+    f = File.open('test/incoming_test_emails/gmail/final_parsed_comment.txt', 'r')
+    comment = Comment.last
+    user_email = mail.from.first
+    assert_equal comment.comment, f.read
+    assert_equal comment.aid, answer.id
+    assert_equal comment.message_id, mail.message_id
+    assert_equal comment.comment_via, 1
+    assert_equal User.find(comment.uid).email, user_email
+    f.close()
+  end
+
+  test 'should parse incoming mail from yahoo service correctly and add answer comment' do
+    require 'mail'
+    mail = Mail.read('test/incoming_test_emails/yahoo/incoming_yahoo_email.eml')
+    # Mail contain ["01namangupta@gmail.com"] in from field.
+    answer = Answer.last
+    mail.subject = "Re: (#a#{answer.id})"
+    Comment.receive_mail(mail)
+    f = File.open('test/incoming_test_emails/yahoo/final_parsed_comment.txt', 'r')
+    comment = Comment.last
+    user_email = mail.from.first
+    assert_equal comment.comment, f.read
+    assert_equal comment.aid, answer.id
+    assert_equal comment.message_id, mail.message_id
+    assert_equal comment.comment_via, 1
+    assert_equal User.find(comment.uid).email, user_email
+    f.close()
   end
 
 end
