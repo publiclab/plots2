4c048282ee49a8d5d50a9eb66fbba4e06ea291ff
ibia <ibia.mah9@gmail.com>
2018-07-10 20:31:31 +0000

    new try

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -73,8 +73,7 @@ class WikiController < ApplicationController
 #      end
       flash[:notice] = "The wiki page does not exist, but here are notes tagged with #{params[:id]}."
       redirect_to '/tag/' + params[:id]
-      return		
-      
+    return		
     end
 
     unless @title # the page exists
