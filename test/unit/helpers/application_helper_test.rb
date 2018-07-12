06c38b850a981889b49526e8d0f4b20e9a20dd0d
Naman Gupta <01namangupta@gmail.com>
2018-07-13 01:06:12 +0530

    Added tests and moved emails to fixtures

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -3,25 +3,25 @@ require 'test_helper'
 class ApplicationHelperTest < ActionView::TestCase
 
   test 'should give filtered comment body' do
-    f = File.open('test/incoming_test_emails/gmail/final_parsed_comment.txt', 'r')
+    f = File.open('fixtures/incoming_test_emails/gmail/final_parsed_comment.txt', 'r')
     filtered_body = filtered_comment_body(f.read)
     f.close()
-    f = File.open('test/incoming_test_emails/gmail/filtered_comment.txt', 'r')
+    f = File.open('fixtures/incoming_test_emails/gmail/filtered_comment.txt', 'r')
     assert_equal filtered_body, f.read
     f.close()
   end
 
   test 'should give trimmed content of comment' do
-    f = File.open('test/incoming_test_emails/gmail/final_parsed_comment.txt', 'r')
+    f = File.open('fixtures/incoming_test_emails/gmail/final_parsed_comment.txt', 'r')
     trimmed_content = trimmed_body(f.read)
     f.close()
-    f = File.open('test/incoming_test_emails/gmail/trimmed_content.txt', 'r')
+    f = File.open('fixtures/incoming_test_emails/gmail/trimmed_content.txt', 'r')
     assert_equal trimmed_content, f.read
     f.close()
   end
 
   test 'should return true if contain trimmed content' do
-    f = File.open('test/incoming_test_emails/gmail/final_parsed_comment.txt', 'r')
+    f = File.open('fixtures/incoming_test_emails/gmail/final_parsed_comment.txt', 'r')
     contain_trimmed_body = contain_trimmed_body?(f.read)
     assert_equal contain_trimmed_body, true
     f.close()
