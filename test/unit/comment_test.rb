f892e91064247873f044f6c38278a559ef742ca0
Naman Gupta <01namangupta@gmail.com>
2018-07-17 21:18:12 +0530

    Added mail parsing for other email domains using google mail service (#3079)
    * Added mail parsing for other email domains using google mail service
    
    * Added mail parsing for other email domains using google mail service
    
    * Corrected serch api test

--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -318,4 +318,38 @@ class CommentTest < ActiveSupport::TestCase
     assert_equal false, comment.trimmed_content?
   end
 
+  test 'should parse incoming mail from other domain who use gmail service correctly and add answer comment' do
+    require 'mail'
+    mail = Mail.read('test/fixtures/incoming_test_emails/gmail/incoming_gmail_email.eml')
+    answer = Answer.last
+    mail.subject = "Re: (#a#{answer.id})"
+    mail.from = ["jeff@publiclab.org"]
+    Comment.receive_mail(mail)
+    f = File.open('test/fixtures/incoming_test_emails/gmail/final_parsed_comment.txt', 'r')
+    comment = Comment.last
+    assert_equal comment.comment, f.read
+    assert_equal comment.aid, answer.id
+    assert_equal comment.message_id, mail.message_id
+    assert_equal comment.comment_via, 1
+    assert_equal User.find(comment.uid).email, "jeff@publiclab.org"
+    f.close()
+  end
+
+  test 'should parse incoming mail from other domain who use gmail service correctly and add comment' do
+    require 'mail'
+    mail = Mail.read('test/fixtures/incoming_test_emails/gmail/incoming_gmail_email.eml')
+    node = Node.last
+    mail.subject = "Re: #{node.title} (##{node.nid})"
+    mail.from = ["jeff@publiclab.org"]
+    Comment.receive_mail(mail)
+    f = File.open('test/fixtures/incoming_test_emails/gmail/final_parsed_comment.txt', 'r')
+    comment = Comment.last
+    assert_equal comment.comment, f.read
+    assert_equal comment.nid, node.id
+    assert_equal comment.message_id, mail.message_id
+    assert_equal comment.comment_via, 1
+    assert_equal User.find(comment.uid).email, "jeff@publiclab.org"
+    f.close()
+  end
+
 end
