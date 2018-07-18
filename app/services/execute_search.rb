5c94fe54e1a3fed19fb0de2c910d5478f03d2391
Stefanni <stefannibrasil@gmail.com>
2018-07-18 07:32:40 -0700

    Remove API Search endpoints duplication (#3045)
    * WIP - Remove API Search enpoints duplication
    
    * Remove cognitive complexity from Search api
    
    * Changes requested and clodeclimate fixes
    
    * Adding limit=srchString to recentPeople method

---
+++ app/services/execute_search.rb
@@ -0,0 +1,32 @@
+class ExecuteSearch
+  def by(type, search_criteria)
+    execute(type, search_criteria)
+  end
+
+  private
+
+  def execute(type, search_criteria)
+    sservice = SearchService.new
+    sresult = DocList.new
+    case type
+     when :all
+       sresult = sservice.textSearch_all(search_criteria.query)
+     when :profiles
+       sresult = sservice.textSearch_profiles(search_criteria.query)
+     when :notes
+       sresult = sservice.textSearch_notes(search_criteria.query)
+     when :questions
+       sresult = sservice.textSearch_questions(search_criteria.query)
+     when :tags
+       sresult = sservice.textSearch_tags(search_criteria.query)
+     when :peoplelocations
+       sresult = sservice.recentPeople(search_criteria.query, search_criteria.tag)
+     when :taglocations
+       if search_criteria.query.include? ","
+         sresult = sservice.tagNearbyNodes(search_criteria.query, search_criteria.tag)
+       end
+     else
+       sresult = []
+     end
+  end
+end
--- test/functional/search_api_test.rb
+++ test/functional/search_api_test.rb
@@ -31,6 +31,51 @@ class SearchApiTest < ActiveSupport::TestCase
 
    end
 
+   test 'search all functionality with multiple responses' do
+      get '/api/srch/all?srchString=question'
+      assert last_response.ok?
+
+      # Expected search pattern
+      pattern = {
+        srchParams: {
+          srchString: 'question',
+          seq: nil,
+        }.ignore_extra_keys!
+      }.ignore_extra_keys!
+
+      matcher = JsonExpressions::Matcher.new(pattern)
+
+      json = JSON.parse(last_response.body)
+
+      assert_equal 15,               json['items'][0]['docId']
+      assert_equal 9,                json['items'][1]['docId']
+      assert_equal 15,               json['items'][2]['docId']
+      assert_equal 9,                json['items'][3]['docId']
+
+      assert matcher =~ json
+
+    end
+
+    test 'search all functionality without search query' do
+       get '/api/srch/all?srchString'
+       assert last_response.ok?
+
+       # Expected search pattern
+       pattern = {
+         srchParams: {
+           srchString: nil,
+           seq: nil,
+         }.ignore_extra_keys!
+       }.ignore_extra_keys!
+
+       matcher = JsonExpressions::Matcher.new(pattern)
+
+       json = JSON.parse(last_response.body)
+       puts json.inspect
+       assert matcher =~ json
+
+     end
+
    test 'search profiles functionality' do
      get '/api/srch/profiles?srchString=Jeff'
      assert last_response.ok?
@@ -119,7 +164,7 @@ class SearchApiTest < ActiveSupport::TestCase
      json = JSON.parse(last_response.body)
      assert matcher =~ json
 
-  end
+    end
 
   test 'search Tag Nearby Nodes functionality' do
     get '/api/srch/taglocations?srchString=71.00,52.00&tagName=awesome'
