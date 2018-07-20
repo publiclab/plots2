fffc9ec9da98a9f3ea55c8719900ca23a41f9ad7
milaaraujo <ca.mila.souzaaraujo@gmail.com>
2018-07-19 19:56:02 -0700

    Code climate fix

--- app/services/execute_typeahead.rb
+++ app/services/execute_typeahead.rb
@@ -8,21 +8,21 @@ class ExecuteTypeahead
   def execute(type, search_criteria, limit)
     sservice = TypeaheadService.new
     sresult = TagList.new
-    case type
+    result = case type
       when :all
-        sresult = sservice.search_all(search_criteria.query, limit)
+        sservice.search_all(search_criteria.query, limit)
       when :profiles
-        sresult = sservice.search_profiles(search_criteria.query, limit)
+        sservice.search_profiles(search_criteria.query, limit)
       when :notes
-        sresult = sservice.search_notes(search_criteria.query, limit)
+        sservice.search_notes(search_criteria.query, limit)
       when :questions
-        sresult = sservice.search_questions(search_criteria.query, limit)
+        sservice.search_questions(search_criteria.query, limit)
       when :tags
-        sresult = sservice.search_tags(search_criteria.query, limit)
+        sservice.search_tags(search_criteria.query, limit)
       when :comments
-        sresult = sservice.search_comments(search_criteria.query, limit)
+        sservice.search_comments(search_criteria.query, limit)
       else
-        sresult = []
+        []
       end
     end
 end
