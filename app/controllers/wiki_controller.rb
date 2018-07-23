d18d8b453fc2d3e40addfb5cfadf6924e940ebc2
nataliyah123 <nataliyah123@users.noreply.github.com>
2018-07-24 03:48:39 +0500

    wiki controller file (If wiki page doesn't exist) (#3037)
    * wiki controller file changed
    
    * second try to make changes
    
    * new try
    
    * check
    
    * check2
    
    * check2
    
    * Updated wikipgissue
    
    * minor

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -64,13 +64,9 @@ class WikiController < ApplicationController
       @tags = @node.tags
       @tags += [Tag.find_by(name: params[:id])] if Tag.find_by(name: params[:id])
     else # it's a new wiki page!
-      @title = I18n.t('wiki_controller.new_wiki_page')
-      if current_user
-        new
-      else
-        flash[:warning] = I18n.t('wiki_controller.pages_does_not_exist')
-        redirect_to '/login'
-      end
+      flash[:notice] = "The wiki page does not exist, but here are notes tagged with #{params[:id]}."
+      redirect_to URI.parse('/tag/' + params[:id]).path
+      return
     end
 
     unless @title # the page exists
