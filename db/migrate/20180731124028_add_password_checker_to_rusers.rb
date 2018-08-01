38e0d09c482d49179926e324481711cfac1a5ce1
Sidharth <bansal.sidharth2996@gmail.com>
2018-08-01 20:36:25 +0530

    Two migrations merged into one

--- db/migrate/20180731124028_add_password_checker_to_rusers.rb
+++
@@ -1,5 +0,0 @@
-class AddPasswordCheckerToRusers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :rusers, :password_checker, :integer, default: 0
-  end
-end
--- db/migrate/20180731170712_remove_password_checker_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemovePasswordCheckerFromUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :password_checker, :integer
-  end
-end
