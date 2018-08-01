bc84a5ad63d47500b7a03678248640cd71111c00
Stefanni <stefannibrasil@gmail.com>
2018-07-31 19:50:15 -0700

    Trying to fix travis error

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -50,8 +50,7 @@ class SearchService
   # If no sort_by value present, then it returns a list of profiles ordered by id DESC
   # a recent activity may be a node creation or a node revision
   def textSearch_profiles(search_criteria)
-    user_scope = SrchScope.find_users(search_criteria.query, limit = nil)
-                          .reorder('')
+    user_scope = User.where('username LIKE ? AND rusers.status = 1', '%' + search_criteria.query + '%')
 
     user_scope =
       if search_criteria.sort_by == "recent"
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -43,6 +43,16 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test 'user mysql native fulltext search 2' do
+    assert User.count > 0
+    if ActiveRecord::Base.connection.adapter_name == 'Mysql2'
+      users = User.search('steff')
+      assert users.first.username 'steff'
+      assert_not_nil users
+      assert users.length > 0
+    end
+  end
+
   test 'user questions' do
     user = users(:jeff)
     assert !user.questions.empty?
