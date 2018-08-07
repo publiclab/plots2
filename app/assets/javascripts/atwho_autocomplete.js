ceb6b67ccd65c4937d8ffd077886da4763d42f9a
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-08-07 11:27:45 -0700

    moved error code out of script to form, wrapped it in a function

--- app/assets/javascripts/atwho_autocomplete.js
+++ app/assets/javascripts/atwho_autocomplete.js
@@ -35,26 +35,4 @@
        }
 
       $('textarea#text-input').atwho(at_config).atwho(hashtags_config).atwho(emojis_config);
-      $('#comment-form').bind('ajax:beforeSend', function(event){
-        $("#text-input").prop('disabled',true)
-        $("#comment-form .btn-primary").button('loading',true);
-      });
-
-      $('#comment-form').bind('ajax:success', function(e, data, status, xhr){
-        $('#text-input').prop('disabled',false);
-        $('#text-input').val('');
-        $('#comments-container').append(xhr.responseText);
-        $('#comment-count')[0].innerHTML = parseInt($('#comment-count')[0].innerHTML)+1;
-        $("#comment-form .btn-primary").button('reset');
-        $('#preview').hide();
-        $('#text-input').show();
-        $('#preview-btn').button('toggle');
-      });
-
-      $('#comment-form').bind('ajax:error', function(e,response){
-        notyNotification('mint', 3000, 'success', 'topRight', 'Some error occured while adding comment');
-        $('#comment-form .control-group').addClass('has-error')
-        $('#comment-form .control-group .help-block ').remove()
-        $('#comment-form .control-group').append('<span class="help-block ">Error: there was a problem.</span>')
-      });
       })();
