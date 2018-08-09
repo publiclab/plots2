9e88993442b1440ea3cd45a2846b865c10cb1399
Naman Gupta <01namangupta@gmail.com>
2018-08-09 13:54:10 +0530

    Added bundle exec for rake/rails tasks in schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,6 +22,8 @@
 # Cron Job log file
 set :output, "#{Dir.pwd}/public/cron_log.log"
 
+set :bundle_command, 'bundle exec'
+
 # To simply print date into the log file for checking if cron job is working properly
 every 1.minutes do
 	puts Dir.pwd
