3ceb0c227de8c9ad0a6f8dbf8450a6610d585d87
Naman Gupta <01namangupta@gmail.com>
2018-08-09 11:18:14 +0530

    Changed migration

--- db/migrate/20180809125000_add_tweet_column_to_comment.rb
+++ db/migrate/20180809125000_add_tweet_column_to_comment.rb
@@ -1,4 +1,4 @@
-class AddSinceColumnToComment < ActiveRecord::Migration[5.2]
+class AddTweetColumnToComment < ActiveRecord::Migration[5.2]
   def change
   	add_column :comments, :tweet_id, :string
   end
