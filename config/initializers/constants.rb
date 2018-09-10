ba6f9dc1ad9e6c6771a7842a5c80fd115717e87a
cthulhu-uu <sky.resendez@gmail.com>
2018-09-10 16:08:28 +1000

    changed FINDER non-capture group to a capture group

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,12 +1,12 @@
 module Callouts
-  FINDER = /(?:\s|^)([^`\w]?)\@([\w-]+)/
+  FINDER = /(\s|^)([^`\w]?)\@([\w-]+)/
   HASHTAG = /(\s)\#([:a-zA-Z0-9_-]+)/
   HASHTAGWITHOUTNUMBER = /(\s)\#([:0-9_-]*[a-zA-Z]+[:0-9_-]*)/
-  PRETTYLINKMD = '\1[@\2](/profile/\2)'
+  PRETTYLINKMD = '\1\2[@\3](/profile/\3)'
   HASHLINKMD = '\1[#\2](/tag/\2)'
   HASHTAGNUMBER = /(\s)\#([:0-9]+)/
   NODELINKMD = '\1[#\2](/n/\2)'
-  PRETTYLINKHTML = '\1<a href="/profile/\2">@\2</a>'
+  PRETTYLINKHTML = '\1\2<a href="/profile/\3">@\3</a>'
   HASHLINKHTML = '\1<a href="/tag/\2">#\2</a>'
   NODELINKHTML = '\1<a href="/n/\2">#\2</a>'
 end
