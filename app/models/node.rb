f5a9073e7a29f3557f2fbfba6dd80c3c16b04c99
Jeffrey Warren <jeff@unterbahn.com>
2018-09-13 16:39:44 -0400

    Update node.rb

--- app/models/node.rb
+++ app/models/node.rb
@@ -316,6 +316,8 @@ class Node < ActiveRecord::Base
     end
   end
 
+  # scan for first image in the body and use this instead
+  # (in future, maybe just do this for all images?)
   def scraped_image
     latest.render_body.scan(/<img(.*?)\/>/).first.first
   end
