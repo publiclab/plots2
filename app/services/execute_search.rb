453d7781b7a1e265489ca9d7ca53c4e3f7578152
milaaraujo <ca.mila.souzaaraujo@gmail.com>
2018-09-19 18:40:36 -0700

    removing 'returns'

--- app/services/execute_search.rb
+++ app/services/execute_search.rb
@@ -9,21 +9,21 @@ class ExecuteSearch
     sservice = SearchService.new
     case type
      when :all
-       return sservice.search_all(search_criteria)
+       sservice.search_all(search_criteria)
      when :profiles
-       return sservice.search_profiles(search_criteria)
+       sservice.search_profiles(search_criteria)
      when :notes
-       return sservice.search_notes(search_criteria.query, search_criteria.limit)
+       sservice.search_notes(search_criteria.query, search_criteria.limit)
      when :wikis
-       return sservice.search_wikis(search_criteria.query, search_criteria.limit)
+       sservice.search_wikis(search_criteria.query, search_criteria.limit)
      when :questions
-       return sservice.search_questions(search_criteria.query, search_criteria.limit)
+       sservice.search_questions(search_criteria.query, search_criteria.limit)
      when :tags
-       return sservice.search_tags(search_criteria.query, search_criteria.limit)
+       sservice.search_tags(search_criteria.query, search_criteria.limit)
      when :peoplelocations
-       return sservice.people_locations(search_criteria.query, search_criteria.tag)
+       sservice.people_locations(search_criteria.query, search_criteria.tag)
      when :taglocations
-       return sservice.tagNearbyNodes(search_criteria.query, search_criteria.tag)
+       sservice.tagNearbyNodes(search_criteria.query, search_criteria.tag)
      else
        sresult = []
      end
