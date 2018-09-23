ad3786b9112dcc4f5f8f1ad35e030a2a0592269b
Rahul Bhatia <rbhatia46@gmail.com>
2018-09-23 15:05:43 +0530

    Improved Back-to-top Button

--- app/assets/javascripts/header_footer.js
+++ app/assets/javascripts/header_footer.js
@@ -1,11 +1,19 @@
 jQuery(document).ready(function() {	 
-	var duration = 300;
+	
 
-	$(".back-to-top").click(function(event) {
-		event.preventDefault();
-		jQuery("html, body").animate({scrollTop: 0}, duration);		 
-		return false;
-	})
+	$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('.back-to-top').fadeIn(200);    // Fade in the button
+    } else {
+        $('.back-to-top').fadeOut(200);   // Else fade out the button
+    }
+});
+$('.back-to-top').click(function() {      // When button is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+
 	
 });
  
