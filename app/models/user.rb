5cc3af64b9e41bd2e1da54acac7fc96faf606f04
Saloni Garg <salonigarg2310@gmail.com>
2018-09-30 12:19:58 +0530

    Added first_time_commenters function

--- app/models/user.rb
+++ app/models/user.rb
@@ -343,6 +343,10 @@ class User < ActiveRecord::Base
   def first_time_poster
     notes.where(status: 1).count == 0
   end
+  
+  def first_time_commenter
+    comments.where(status: 1).count == 0
+  end
 
   def follow(other_user)
     active_relationships.create(followed_id: other_user.id)
