b1ca2ac9a4aac71362c49a2e967b8af062ea876b
Kanikashrivastav <kanika17@navgurukul.org>
2018-10-13 04:12:33 +0530

    added total contributors for all time

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -81,5 +81,7 @@ class StatsController < ApplicationController
 
     @all_notes = nids.uniq.length
     @all_contributors = users.uniq.length
+    # TODO Caching
+    @all_time_contributors = User.count_all_time_contributor
   end
 end
