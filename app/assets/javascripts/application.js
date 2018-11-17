b39c46e3264aa952407483a8440ecf0c64db67b4
Rohan Singh <rohan.clevduct@gmail.com>
2018-11-17 18:15:36 +0530

    Included typeahead.jquery.js
    typeahead.jquery.js is required for post.js to work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require typeahead.js/dist/typeahead.jquery.js
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap/dist/js/bootstrap.min.js
