4b1fb14ec534098412f24ed66cc6bb10d5a75d1d
Oorjit Chowdhary <39333058+oorjitchowdhary@users.noreply.github.com>
2018-11-18 18:36:00 +0530

    Update comment_test.rb

--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -240,22 +240,6 @@ class CommentTest < ActiveSupport::TestCase
     f.close()
   end
 
-  test 'should parse incoming mail from yahoo service correctly and add comment' do
-    require 'mail'
-    mail = Mail.read('test/fixtures/incoming_test_emails/yahoo/incoming_yahoo_email.eml')
-    node = Node.last
-    mail.subject = "Re: #{node.title} (##{node.nid})"
-    Comment.receive_mail(mail)
-    f = File.open('test/fixtures/incoming_test_emails/yahoo/final_parsed_comment.txt', 'r')
-    comment = Comment.last
-    user_email = mail.from.first
-    assert_equal comment.comment, f.read
-    assert_equal comment.nid, node.id
-    assert_equal comment.message_id, mail.message_id
-    assert_equal comment.comment_via, 1
-    assert_equal User.find(comment.uid).email, user_email
-    f.close()
-  end
 
   test 'should parse text containing "On ____ <email@email.com> wrote:" from comments on display' do
     node = Node.last
