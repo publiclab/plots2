f7de476299bd70a7fdde33a4556428f4fc19d7d1
Sarthak Aggarwal <getsarthakaggarwal@gmail.com>
2018-11-19 19:04:23 +0530

    Added Test
    Added Test 'should parse incoming mail from gmail service correctly and add comment'

--- test/integration/token_comment_test.rb
+++ test/integration/token_comment_test.rb
@@ -33,4 +33,21 @@ class TokenCommentTest < ActionDispatch::IntegrationTest
     f.close()
   end
   
+    test 'should parse incoming mail from gmail service correctly and add comment' do
+    require 'mail'
+    mail = Mail.read('test/fixtures/incoming_test_emails/gmail/incoming_gmail_email.eml')
+    node = Node.last
+    mail.subject = "Re: #{node.title} (##{node.nid})"
+    Comment.receive_mail(mail)
+    f = File.open('test/fixtures/incoming_test_emails/gmail/final_parsed_comment.txt', 'r')
+    comment = Comment.last
+    user_email = mail.from.first
+    assert_equal comment.comment, f.read
+    assert_equal comment.nid, node.id
+    assert_equal comment.message_id, mail.message_id
+    assert_equal comment.comment_via, 1
+    assert_equal User.find(comment.uid).email, user_email
+    f.close()
+  end
+  
 end
