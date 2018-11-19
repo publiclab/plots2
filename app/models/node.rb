425c2873a62626fa337b847f80aff0a828c44922
Rohan Singh <rohan.clevduct@gmail.com>
2018-11-19 08:05:03 +0530

    Update node.rb

--- app/models/node.rb
+++ app/models/node.rb
@@ -32,13 +32,11 @@ class Node < ActiveRecord::Base
                   elsif order == :views
                     { views: :desc }
                   end
-    
-  
-  
-  query = connection.quote(query.to_s)
+
     if ActiveRecord::Base.connection.adapter_name == 'Mysql2'
       if order == :natural
         if type == :boolean
+          query = connection.quote(query.to_s)
           nids = Revision.select("node_revisions.nid, node_revisions.body, node_revisions.title, MATCH(node_revisions.body, node_revisions.title) AGAINST(#{query} IN BOOLEAN MODE) AS score")
             .where("MATCH(node_revisions.body, node_revisions.title) AGAINST(#{query} IN BOOLEAN MODE)")
             .limit(limit)
