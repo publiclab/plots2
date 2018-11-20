4b3fb844e2aee80e9b39b42dff1a41827a08d6c8
Oorjit Chowdhary <39333058+oorjitchowdhary@users.noreply.github.com>
2018-11-20 16:26:23 +0530

    Update token_comment_test.rb

--- test/integration/token_comment_test.rb
+++ test/integration/token_comment_test.rb
@@ -32,17 +32,18 @@ class TokenCommentTest < ActionDispatch::IntegrationTest
     assert_equal User.find(comment.uid).email, "jeff@publiclab.org"
     f.close()
   end
-    test 'should parse incoming mail from yahoo service correctly and add comment' do
+   test 'should parse incoming mail from yahoo service correctly and add answer comment' do
     require 'mail'
     mail = Mail.read('test/fixtures/incoming_test_emails/yahoo/incoming_yahoo_email.eml')
-    node = Node.last
-    mail.subject = "Re: #{node.title} (##{node.nid})"
+    # Mail contain ["01namangupta@gmail.com"] in from field.
+    answer = Answer.last
+    mail.subject = "Re: (#a#{answer.id})"
     Comment.receive_mail(mail)
     f = File.open('test/fixtures/incoming_test_emails/yahoo/final_parsed_comment.txt', 'r')
     comment = Comment.last
     user_email = mail.from.first
     assert_equal comment.comment, f.read
-    assert_equal comment.nid, node.id
+    assert_equal comment.aid, answer.id
     assert_equal comment.message_id, mail.message_id
     assert_equal comment.comment_via, 1
     assert_equal User.find(comment.uid).email, user_email
