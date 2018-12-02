f1574368964e2a61960c20dfef95f4668a677f8e
okonek <jandominikokonski@gmail.com>
2018-12-02 17:36:14 +0000

    Added an I did this comment pop-up

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,31 +1,44 @@
 (function() {
-  $('#text-input').bind('keypress',function(e){
-    if (e.ctrlKey && e.keyCode === 10) {
-    $("#comment-form .btn-primary").click();
+
+  $('.comment-form').each(function() {
+    if(!$(this).hasClass('bound-success')) {
+      $(this).addClass('bound-success').bind('ajax:success', function(e, data, status, xhr){
+        $(this).find('#text-input').prop('disabled',false);
+        $(this).find('#text-input').val('');
+        $('#comments-container').append(xhr.responseText);
+        $('#comment-count')[0].innerHTML = parseInt($('#comment-count')[0].innerHTML, 10)+1;
+        $(this).find(".btn-primary").button('reset');
+        $(this).find('#preview').hide();
+        $(this).find('#text-input').show();
+        $(this).find('#preview-btn').button('toggle');
+      });
     }
-  })
 
-  $('#comment-form').bind('ajax:beforeSend', function(event){
-    $("#text-input").prop('disabled',true)
-    $("#comment-form .btn-primary").button('loading',true);
-  });
+    if(!$(this).hasClass('bound-beforeSend')) {
+      $(this).addClass('bound-beforeSend').bind('ajax:beforeSend', function(event){
+        $(this).find("#text-input").prop('disabled',true)
+        $(this).find(".btn-primary").button('loading',true);
+      });
+    }
 
-  $('#comment-form').bind('ajax:success', function(e, data, status, xhr){
-    $('#text-input').prop('disabled',false);
-    $('#text-input').val('');
-    $('#comments-container').append(xhr.responseText);
-    $('#comment-count')[0].innerHTML = parseInt($('#comment-count')[0].innerHTML, 10)+1;
-    $("#comment-form .btn-primary").button('reset');
-    $('#preview').hide();
-    $('#text-input').show();
-    $('#preview-btn').button('toggle');
-  });
+    if(!$(this).hasClass('bound-error')) {
+      $(this).addClass('bound-error').bind('ajax:error', function(e,response){
+        notyNotification('mint', 3000, 'success', 'topRight', 'Some error occured while adding comment');
+        $(this).find('.control-group').addClass('has-error')
+        $(this).find('.control-group .help-block ').remove()
+        $(this).find('.control-group').append('<span class="help-block ">Error: there was a problem.</span>')
+      });
+    }
 
-  $('#comment-form').bind('ajax:error', function(e,response){
-    notyNotification('mint', 3000, 'success', 'topRight', 'Some error occured while adding comment');
-    $('#comment-form .control-group').addClass('has-error')
-    $('#comment-form .control-group .help-block ').remove()
-    $('#comment-form .control-group').append('<span class="help-block ">Error: there was a problem.</span>')
+    if(!$(this).hasClass('bound-keypress')) {
+      $(this).addClass('bound-keypress');
+      $(this).find('#text-input').bind('keypress',function(e){
+        if (e.ctrlKey && e.keyCode === 10) {
+        $(this).find(".btn-primary").click();
+        }
+      })
+    }
+    
   });
 }());
 
