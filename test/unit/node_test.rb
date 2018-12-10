31d8ecf24d2387ce355237414951a36f3c48253e
d1g1t4ld1n4 <dinaelhanan@gmail.com>
2018-12-10 13:33:58 -0500

    Update node_test.rb

--- test/unit/node_test.rb
+++ test/unit/node_test.rb
@@ -210,11 +210,9 @@ class NodeTest < ActiveSupport::TestCase
     assert !node.node_tags.empty?
     assert_not_nil node.tagnames
     assert node.tagnames.first.is_a?(String)
-    assert_equal 'test awesome spectrometer activity:spectrometer', node.tagnames.join(' ')
-    assert_equal 'test cool spectrometer activity:spectrometer', node.tagnames.join(' ')
+    assert_equal 'test awesome cool spectrometer activity:spectrometer', node.tagnames.join(' ')
     # used to generate CSS classes:
-    assert_equal 'tag-test tag-awesome tag-spectrometer tag-activity-spectrometer', node.tagnames_as_classes
-    assert_equal 'tag-test tag-cool tag-spectrometer tag-activity-spectrometer', node.tagnames_as_classes
+    assert_equal 'tag-test tag-awesome tag-cool tag-spectrometer tag-activity-spectrometer', node.tagnames_as_classes
   end
 
   test 'should have subscribers' do
