b7581b624e13845d0311436452cafccba2ee94f4
Jeffrey Warren <jeff@unterbahn.com>
2018-12-14 17:07:14 -0500

    Update admin_controller.rb

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -377,11 +377,11 @@ class AdminController < ApplicationController
         s.print "RCPT TO: <example@publiclab.org>\n"
       end
       if line.include? '250 Accepted'
-        render :text => "Email gateway OK"
+        render plain: "Email gateway OK"
         s.close_write
       elsif line.include? '550'
-        render :text => "Email gateway NOT OK"
-        render :status => 500
+        render plain: "Email gateway NOT OK"
+        render status: 500
         s.close_write
       end
     end
