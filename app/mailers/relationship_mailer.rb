86c316bb7ade6046133d0887022af9326b1b6c01
Shubham Sangamnerkar <shubhamsangamnerkar9@gmail.com>
2018-12-25 18:12:07 +0530

    Fixed typo

--- app/mailers/relationship_mailer.rb
+++ app/mailers/relationship_mailer.rb
@@ -5,8 +5,8 @@ class RelationshipMailer < ApplicationMailer
 
   def notify_the_user_who_is_followed(followed_user, user_who_started_following)
     subject = "[PublicLab] #{user_who_started_following.username} started following you"
-    @followed_user = followed_user
+    @user_who_started_following = user_who_started_following
     @footer = feature('email-footer')
-    mail(to: @followed_user.email, subject: subject)
+    mail(to: followed_user.email, subject: subject)
   end
 end
