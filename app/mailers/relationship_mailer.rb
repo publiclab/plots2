30a61800f0cc47e942b83a5decd4312f7b054858
Shubham Sangamnerkar <shubhamsangamnerkar9@gmail.com>
2018-12-25 17:20:29 +0530

    Fixed tabs

--- app/mailers/relationship_mailer.rb
+++ app/mailers/relationship_mailer.rb
@@ -2,12 +2,11 @@ class RelationshipMailer < ApplicationMailer
   helper :application
   include ApplicationHelper
   default from: "notifications@#{ActionMailer::Base.default_url_options[:host]}"
-
-	def notify_the_user_who_is_followed(followed_user, user_who_started_following)
-	  subject = "[PublicLab] #{user_who_started_following.username} started following you"
-	  @followed_user = followed_user
-	  @footer = feature('email-footer')
+  
+  def notify_the_user_who_is_followed(followed_user, user_who_started_following)
+    subject = "[PublicLab] #{user_who_started_following.username} started following you"
+    @followed_user = followed_user
+    @footer = feature('email-footer')
     mail(to: @followed_user.email, subject: subject)
-	end
-
+  end
 end
