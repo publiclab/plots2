bd2c8aad2d4ccd73fa18796e3bd051a463fbbe7e
Shubham Sangamnerkar <shubhamsangamnerkar9@gmail.com>
2018-12-25 17:24:23 +0530

    Fixed blank line and blank spaces

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,4 +2,3 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
 end
-
--- app/mailers/relationship_mailer.rb
+++ app/mailers/relationship_mailer.rb
@@ -2,7 +2,7 @@ class RelationshipMailer < ApplicationMailer
   helper :application
   include ApplicationHelper
   default from: "notifications@#{ActionMailer::Base.default_url_options[:host]}"
-  
+
   def notify_the_user_who_is_followed(followed_user, user_who_started_following)
     subject = "[PublicLab] #{user_who_started_following.username} started following you"
     @followed_user = followed_user
