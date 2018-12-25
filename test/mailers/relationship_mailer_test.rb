4cf9a8cb4cbc1c77ade53bbacd0ebac898c91726
Shubham Sangamnerkar <shubhamsangamnerkar9@gmail.com>
2018-12-25 18:11:39 +0530

    Added tests

--- test/mailers/relationship_mailer_test.rb
+++ test/mailers/relationship_mailer_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class RelationshipMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'notify upon following' do
+    user_follower = User.find_by_username('jeff')
+    user_followed = User.find_by_username('Bob')
+    email = RelationshipMailer.notify_the_user_who_is_followed(user_followed, user_follower)
+    assert_emails 1 do
+      email.deliver_now
+    end
+    assert_equal ["notifications@#{ActionMailer::Base.default_url_options[:host]}"], email.from
+    assert_equal [user_followed.email], email.to
+  end
 end
