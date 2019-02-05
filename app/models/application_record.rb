097c117164150925e4f4b5e7c39cd1d6324cc18b
Cess <cessmbuguar@gmail.com>
2019-02-06 01:59:07 +0300

    Raw data from stats page (#4654)
    * Download data as json
    
    * add comments to stats
    
    * download stats with month ranges
    
    * add maps as download content  and style
    
    * implement downlod as csv
    
    * move download logic to range page
    
    * resctrict download of stats to admin

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
+  require 'csv'
   self.abstract_class = true
+
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |object|
+        csv << object.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
