e86000828a7363796dea09474833c7551ad2f24b
Sebastian Silva <sebastian@fuentelibre.org>
2019-02-08 15:15:54 -0500

    Revert previous test

--- db/migrate/20180721114724_change_comments_to_utf8mb4.rb
+++ db/migrate/20180721114724_change_comments_to_utf8mb4.rb
@@ -1,8 +1,8 @@
 class ChangeCommentsToUtf8mb4 < ActiveRecord::Migration[5.2]
    def change
     # changing table that will store unicode execute:
-    # execute "ALTER TABLE comments CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
+    execute "ALTER TABLE comments CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
     # changin string/text column with unicode content execute:
-    # execute "ALTER TABLE comments MODIFY comment VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
+    execute "ALTER TABLE comments MODIFY comment VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
   end
 end
