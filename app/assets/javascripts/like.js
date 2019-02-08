8a0ba254f7630de52286a77106e821c3b5e0ce06
avsingh999 <201651011@iiitvadodara.ac.in>
2019-02-09 03:29:25 +0530

    improve like count

--- app/assets/javascripts/like.js
+++ app/assets/javascripts/like.js
@@ -1,27 +1,35 @@
 /* Like button functionality of nodes */
 
 // functionalize appearance changes
-function changelikecount(value, node_id) {
+function changelikecount(node_id) {
 
   var count = $('#like-count-' + node_id).html();
   // strip parens and convert to number
   count = parseInt(count);
-  count += value;
+  var previous_like_state = getLikeState();
+  var new_like_state = !previous_like_state;
+  count += new_like_state - previous_like_state;
+
+  setLikeState(new_like_state);
   // push value back out
   $('#like-count-' + node_id).html(count);
 
 }
 
 function shownotliked(node_id) {
-
   $('#like-star-' + node_id)[0].className = "fa fa-star-o";
-
 }
 
 function showliked(node_id) {
-
   $('#like-star-' + node_id)[0].className = "fa fa-star";
+}
 
+function setLikeState(new_like_state) {
+  window._like_state = new_like_state;
+}
+
+function getLikeState() {
+  return window._like_state;
 }
 
 // support AJAX button clicking
@@ -33,7 +41,7 @@ function clickliked() {
    .done(function(response) {
     notyNotification('mint', 3000, 'success', 'topRight', 'Unliked!');
     shownotliked(node_id);
-    changelikecount(parseInt(response), node_id);
+    changelikecount(node_id);
     $('#like-button-' + node_id).on('click', clicknotliked);
     $('#like-button-' + node_id).off('click', clickliked);
 
@@ -49,7 +57,7 @@ function clicknotliked() {
    .done(function(response) {
     notyNotification('mint', 3000, 'success', 'topRight', 'Liked!');
     showliked(node_id);
-    changelikecount(parseInt(response), node_id);
+    changelikecount(node_id);
     $('#like-button-' + node_id).on('click', clickliked);
     $('#like-button-' + node_id).off('click', clicknotliked);
 
