2704262b3b80cd7a8969e1e4d8140416745bac04
Shubham Sangamnerkar <shubhamsangamnerkar9@gmail.com>
2019-02-17 18:07:45 +0530

    add helper functions

--- lib/text_search.rb
+++ lib/text_search.rb
@@ -5,4 +5,13 @@ module TextSearch
     lem = Lemmatizer.new
     lem.lemma(word)
   end
+
+  def non_hyphenate_query(word)
+  	word.gsub('-','')
+  end
+
+  def results_with_probable_hyphens(word)
+  	lem = Lemmatizer.new("hyphenated_words.dict.txt")
+  	lem.lemma(word)
+  end
 end
