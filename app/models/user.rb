2eacfa58ce6387e070890a8a1e60048a8a30f959
Emmanuel Hayford <siawmensah@gmail.com>
2019-03-01 00:40:22 +0100

    Remove inherited method

--- app/models/user.rb
+++ app/models/user.rb
@@ -430,13 +430,4 @@ class User < ActiveRecord::Base
     revisions = Revision.where(status: 1).pluck(:uid)
     contributors = (notes + answers + questions + comments + revisions).compact.uniq.length
   end
-
-  def self.to_csv(options = {})
-    CSV.generate(options) do |csv|
-      csv << column_names
-      all.each do |object|
-        csv << object.attributes.values_at(*column_names)
-      end
-    end
-  end
 end
