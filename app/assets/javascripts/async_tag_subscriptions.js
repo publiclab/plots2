03ff90eb6b42b5ccb265754aea4b1fa15090d0fe
Shubham Sangamnerkar <shubhamsangamnerkar9@gmail.com>
2019-03-03 06:43:23 +0530

    cc fix try

--- app/assets/javascripts/async_tag_subscriptions.js
+++ app/assets/javascripts/async_tag_subscriptions.js
@@ -23,7 +23,7 @@ $(document).ready(function()
 	    console.log("Hello");
 	    var data_recv = JSON.parse(JSON.stringify(status));
 	    notyNotification('relax', 3000, 'success', 'top', data_recv.message + 'Click <a href="../subscriptions"> here </a> to manage your subscriptions. ');
-	    var html_new = '<a rel="tooltip" title=Following' +' class="btn btn-default btn-sm active" href="'+'/unsubscribe/tag/'+ data_recv.tagname + '"> <i class="fa fa-user-plus" aria-hidden="true"></i>Following' +'</a>';
+	    var html_new = '<a rel="tooltip" title=Following class="btn btn-default btn-sm active" href="/unsubscribe/tag/'+ data_recv.tagname + '"> <i class="fa fa-user-plus" aria-hidden="true"></i>Following</a>';
 	    $('#follow-unfollow-column-'+data_recv.id).html(html_new); 
 	  });
       $('.index-follow-buttons').on('ajax:error', function(data, status, xhr){
