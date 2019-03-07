3b03ed62ca172ae6bf5312a35a6af65f9539b63d
Jeffrey Warren <jeff@unterbahn.com>
2019-03-07 17:28:00 -0500

    sentry (#4944)
    * sentry
    
    * gemfile.lock

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,14 @@ class ApplicationController < ActionController::Base
 
   before_action :set_locale
 
+  before_action :set_raven_context
+
   private
+
+  def set_raven_context
+    Raven.user_context(id: session[:current_user_id]) # or anything else in session
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
 
   # eventually videos could be a power tag
   def set_sidebar(type = :generic, data = :all, args = {})
