1d76bf10e3843881e880046540ab064bee0c72ca
Yash Gupta <yash@Yashs-MacBook-Pro.local>
2019-03-08 19:06:40 +0530

    Add some css transitions effect #4688

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -1,10 +1,10 @@
-function toggle_sidebar() { 
-  if ($('#sidebar').hasClass('hidden-sm')) { 
-    $('#sidebar').removeClass('hidden-sm') 
-                 .removeClass('hidden-xs') 
-  } else { 
-    $('#sidebar').addClass('hidden-sm') 
-                 .addClass('hidden-xs') 
-  } 
+function toggle_sidebar() {
+  // made changes transition effect
+  if ($('#sidebar').hasClass('hidden-sm')) {
+    $('#sidebar').removeClass('hidden-sm')
+                 .removeClass('hidden-xs')
+  } else {
+    $('#sidebar').addClass('hidden-sm')
+                 .addClass('hidden-xs')
+  }
 }
- 
