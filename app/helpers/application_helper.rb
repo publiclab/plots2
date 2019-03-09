913873a18172ad40981ff40bbedae56e244b7e0b
Gaurav Sachdeva <sachdeva.gaurav1997@gmail.com>
2019-03-09 10:28:37 +0530

    Addition of Translation prompts (#4787)
    * translation prompt helper created
    
    * blog page and subscriptions page helper addition
    
    * fix icon size

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -134,4 +134,16 @@ module ApplicationHelper
   def current_page?(page)
     'is-active' if request.path == page
   end
+
+  def translation(key, options = {})
+    translated_string = t(key, options)
+
+    if translated_string.length > 3 && current_user &.has_tag('translation-helper') && !translated_string.include?("translation missing")
+      %(<span>#{translated_string} <a href="https://www.transifex.com/publiclab/publiclaborg/translate/#de/$?q=text%3A#{translated_string}">
+          <i data-toggle='tooltip' data-placement='top' title='Needs translation? Click to help translate this text.' style='position:relative; right:2px; color:#bbb; font-size: 15px;' class='fa fa-globe'></i></a>
+       </span>)
+    else
+      translated_string
+    end
+  end
 end
