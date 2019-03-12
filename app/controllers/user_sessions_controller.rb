556bef1340a8f6c3d60ed8eb2ec6d130847509a0
Anjali Jain <punny@punny.jaini>
2019-03-12 08:25:36 +0530

    fix return_to anomaly

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -138,7 +138,7 @@ class UserSessionsController < ApplicationController
                 elsif session[:return_to]
                   return_to = session[:return_to]
                   session[:return_to] = nil
-                  redirect_to return_to + hash_params
+                  redirect_to '/dashboard'
                 elsif params[:return_to]
                   redirect_to params[:return_to] + hash_params
                 else
