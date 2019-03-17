1cf84d06ba2d977f6aa0976b863fd1a848fee18d
divyabaid16 <divyabaid16@gmail.com>
2019-03-18 00:24:35 +0530

    Added unit test

--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -308,4 +308,12 @@ class CommentTest < ActiveSupport::TestCase
     assert_not_nil graph
     assert graph.class, Hash
   end
+
+  test 'find email using twitter user name' do
+    require 'yaml'
+    config = YAML.load(File.read('test/fixtures/user_tags.yml'))
+    username = config["twitter3"]["data"]["info"]["nickname"]
+    email = Comment.find_email(username)
+    assert_equal email, "01namangupta@gmail.com"
+  end
 end
