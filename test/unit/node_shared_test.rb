d71d8554b5ca97803003310a21260e0dabfb912a
d1g1t4ld1n4 <dinaelhanan@gmail.com>
2019-03-20 13:34:31 -0400

    Update node_shared_test.rb

--- test/unit/node_shared_test.rb
+++ test/unit/node_shared_test.rb
@@ -25,7 +25,7 @@ class NodeSharedTest < ActiveSupport::TestCase
     before = "Here are some notes in a table: \n\n[button:Press me:/questions] \n\n[button:Cancel:https://google.com]\n\n`[button:Cancel:https://google.com]` This shouldn't get recognized because it's in ` ticks.\n\nMake sense?"
     html = NodeShared.button(before)
     assert html
-    assert_equal 1, html.scan('<a class="btn btn-primary inline-button-shortcode"').length
+    assert_equal 1, html.scan('<a class="btn btn-primary inline-button-shortcode').length
     assert_equal 1, html.scan('href=').length
     assert_equal 5, html.scan('inline-button-shortcode').length
     assert html.scan('<td class="title">').length > 1
