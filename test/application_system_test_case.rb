6c001a2861821317108a42178bc211ffd535e311
Jeffrey Warren <jeff@unterbahn.com>
2019-03-24 13:42:45 -0400

    Update application_system_test_case.rb

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
-  argument = { arg: ["headless", "disable-gpu", "no-sandbox", "disable-dev-shm-usage"] }
-
-  Selenium::WebDriver::Chrome::Options.new.add_argument(argument)
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => %w(--headless)})
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { desired_capabilities: caps }
+  # argument = { arg: ["headless", "disable-gpu", "no-sandbox", "disable-dev-shm-usage"] }
+  # Selenium::WebDriver::Chrome::Options.new.add_argument(argument)
 end
