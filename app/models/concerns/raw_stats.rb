400c6b26f399a9c1700f45df55104626eba026c0
Cess <cessmbuguar@gmail.com>
2019-03-25 18:14:54 +0300

    Extract to_csv method in a module

---
+++ app/models/concerns/raw_stats.rb
@@ -0,0 +1,14 @@
+require 'csv'
+
+module RawStats
+  extend ActiveSupport::Concern
+
+  def to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |object|
+        csv << object.attributes.values_at(*column_names)
+      end
+    end
+  end
+end
