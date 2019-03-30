bd2400646d890934373c7938655ef70b7dfd7092
Jeffrey Warren <jeff@unterbahn.com>
2019-03-30 09:54:20 -0400

    Turn off binary option

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => %w(--headless)})
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { desired_capabilities: caps, binary: "/usr/bin/chromedriver" }
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { desired_capabilities: caps } #, binary: "/usr/bin/chromedriver" }
   # argument = { arg: ["headless", "disable-gpu", "no-sandbox", "disable-dev-shm-usage"] }
   # Selenium::WebDriver::Chrome::Options.new.add_argument(argument)
 end
