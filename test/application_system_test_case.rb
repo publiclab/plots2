d7cd5f629751d6bf6170ecfc7c3624fa01e07af0
Jeffrey Warren <jeff@unterbahn.com>
2019-03-30 18:54:33 -0400

    Add chrome flags to ruby initializer

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => %w(--headless)})
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => %w(--headless  --disable-gpu --no-sandbox --remote-debugging-port=9222)})
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { desired_capabilities: caps } #, binary: "/usr/bin/chromedriver" }
   # argument = { arg: ["headless", "disable-gpu", "no-sandbox", "disable-dev-shm-usage"] }
   # Selenium::WebDriver::Chrome::Options.new.add_argument(argument)
