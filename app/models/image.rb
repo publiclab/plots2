c016076677efcbba83058c1be4f7e7b781c3ed43
Jeffrey Warren <jeff@unterbahn.com>
2019-04-10 11:57:32 -0400

    change to validates_with AttachmentSizeValidator, :photo attribute

--- app/models/image.rb
+++ app/models/image.rb
@@ -15,7 +15,7 @@ class Image < ApplicationRecord
   # validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/jpg', 'image/gif']
   # validates_attachment_content_type :photo_file_name, :content_type => %w(image/jpeg image/jpg image/png)
   # validates :title, :presence => true, :format => {:with => /\A[a-zA-Z0-9\ -_]+\z/, :message => "Only letters, numbers, and spaces allowed"}, :length => { :maximum => 60 }
-  validates_attachment_size :image, :in => 0.megabytes..25.megabytes
+  validates_with AttachmentSizeValidator, attributes: :photo, less_than: 25.megabytes
 
   before_validation :download_remote_image, if: :remote_url_provided?
   validates :remote_url, presence: true, if: :remote_url_provided? # , :message => "is invalid or inaccessible" # this message thing is old-style rails 2.3.x
