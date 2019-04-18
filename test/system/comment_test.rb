9b36bd7db0bd186759e9c213189c6d0fcda1827c
Jeffrey Warren <jeff@unterbahn.com>
2019-04-18 13:09:34 -0400

    Create comment_test.rb

---
+++ test/system/comment_test.rb
@@ -0,0 +1,27 @@
+require "application_system_test_case"
+# https://guides.rubyonrails.org/testing.html#implementing-a-system-test
+
+class SearchTest < ApplicationSystemTestCase
+  test 'adding an image into a comment box' do
+    UserSession.create(users(:bob)) # log in
+
+    visit nodes(:blog).path
+
+    # we could see if an image gets created:
+    # assert_no_difference 'Image.count' do
+      attach_file('Image', '/public/images/pl.png')
+    # end
+
+    assert_selector('.comment-form textarea#textinput', text: '![](/some/imageid.png)')
+
+    # click_button 'Preview'
+    # assert_equal 1, page.evaluate_script("$('.comment-form div#preview').is(':visible')")
+
+    click_button 'Publish'
+
+    assert_selector('.comment-form textarea#textinput', text: '')
+
+    # assert some javascript:
+    # assert_equal 1, page.evaluate_script("$('.typeahead.dropdown-menu').find('li').length")
+  end
+end
