0d140761963f84e910d8cae3290cf0dd198e3cbf
sagarpreet-chadha <chadha.sagarpreet97@gmail.com>
2019-04-20 15:42:12 +0530

    system test added

--- test/system/place_tags_test.rb
+++ test/system/place_tags_test.rb
@@ -3,9 +3,14 @@ require "application_system_test_case"
 class PlaceTagsTest < ApplicationSystemTestCase
 
   test "pages tagged with place get a full-width map across the top of the page" do
-    visit nodes(:blog).path
-    assert_select "div#top_map"
+    visit nodes(:map).path
     assert_select "div.leaflet-layer"
+    assert_select "div#top_map"
+  end
+
+  test "pages not tagged with place ges a side map" do
+    visit nodes(:map).path
+    assert_select "div#top_map" , :count => 0 
   end
 
 end
