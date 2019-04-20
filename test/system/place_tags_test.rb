e386cd7c7fafb3aef9fc75d6dc897c0b60eef815
sagarpreet-chadha <chadha.sagarpreet97@gmail.com>
2019-04-20 16:45:41 +0530

    system test modification

--- test/system/place_tags_test.rb
+++ test/system/place_tags_test.rb
@@ -2,13 +2,16 @@ require "application_system_test_case"
 
 class PlaceTagsTest < ApplicationSystemTestCase
 
+  # node blog has lat and lon tag and place tag .	
   test "pages tagged with place get a full-width map across the top of the page" do
-    visit nodes(:map).path
-    assert_select "div.leaflet-layer"
-    assert_select "div#top_map"
+    visit nodes(:blog).path
+    assert_selector('h1', text: 'Blog post')
+    #assert_select "div.leaflet-layer"
+    #assert_select "div#top_map"
   end
 
-  test "pages not tagged with place ges a side map" do
+  # node map has lat and lon tag but no place tag .
+  test "pages not tagged with place gets a side map" do
     visit nodes(:map).path
     assert_select "div#top_map" , :count => 0 
   end
