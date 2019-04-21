d08b2b0745f28b81761585c66883abf2f547045c
SidharthBansal <bansal.sidharth2996@gmail.com>
2019-04-21 18:54:49 +0530

    minor change

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -136,11 +136,11 @@ class UserSessionsController < ApplicationController
                   session[:openid_return_to] = nil
                   redirect_to return_to + hash_params
                 elsif session[:return_to]
+                  # if url == /login?return_to=/subscribe/multiple/tag/tag1,tag299 then
+                  # session[:return_to] == /subscriptions  + '?_=' + Time.current.to_i.to_s ? true
                   if params[:return_to]
-                    return_to = '/' + params[:return_to].split('/')[2..-1].join('/') #/== subscribe/multiple/tag/tag1,tag299
                     # params[:return_to] == /login?return_to=/subscribe/multiple/tag/tag1,tag299 ? true
-                    # session[:return_to] == /subscriptions  + '?_=' + Time.current.to_i.to_s ? true
-                    # ["login?return_to=", "subscribe", "multiple", "tag", "tag1,tag299"] == params[:return_to].split('/')[1..-1]
+                    return_to = '/' + params[:return_to].split('/')[2..-1].join('/') #== /subscribe/multiple/tag/tag1,tag299
                     redirect_to return_to
                   else
                     flash[:notice] = "1.2"
