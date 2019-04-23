7b26c97848b390455b1712f88e32d675bdf5d801
SidharthBansal <bansal.sidharth2996@gmail.com>
2019-04-23 21:41:03 +0530

    Removed redundant code

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -1,11 +1,6 @@
 require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit users_url
-  #
-  #   assert_selector "h1", text: "Users"
-  # end
   test "preserve return_to while switching from login to signup" do
     visit '/login?return_to=/subscribe/multiple/tag/tag1,tag2'
     click_on "sign up"
