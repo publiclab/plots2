994dde7a2440bd9f9b7da4fa6605e7c74774b33b
SidharthBansal <bansal.sidharth2996@gmail.com>
2019-04-23 06:43:44 +0530

    Adding system tests for authentication

---
+++ test/system/users_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class UsersTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit users_url
+  #
+  #   assert_selector "h1", text: "Users"
+  # end
+end
