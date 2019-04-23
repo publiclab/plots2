6fe7d54b4aba968062a911e2f222acc3699d9fd8
SidharthBansal <bansal.sidharth2996@gmail.com>
2019-04-23 07:12:52 +0530

    minor change

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -9,6 +9,8 @@ class UsersTest < ApplicationSystemTestCase
   test "preserve return_to while switching from login to signup" do
     visit '/login?return_to=/subscribe/multiple/tag/tag1,tag2'
     click_on "sign up"
-    assert_redirected_to '/signup?return_to=/subscribe/multiple/tag/tag1,tag2'
+    assert_text "Create a password"
+    assert_text "Confirmation"
+    assert_text "Bio"
   end
 end
