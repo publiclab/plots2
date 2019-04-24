d7e1a1108f5df54a64a69a9df0c6fbf0f7aaaa53
Anthonia Okafor <tonchuks@gmail.com>
2019-04-24 10:34:40 +0000

    comitting answer_like_controller

--- test/functional/answer_like_controller_test.rb
+++ test/functional/answer_like_controller_test.rb
@@ -6,34 +6,42 @@ class AnswerLikeControllerTest < ActionController::TestCase
   end
 
   test 'should get show' do
-    answer = answers(:one)
-    get :show, params: { id: answer.id }
-    assert_response :success
+    perform_enqueued_jobs do
+      answer = answers(:one)
+      get :show, params: { id: answer.id }
+      assert_response :success
+    end
   end
 
   test 'should get likes' do
-    UserSession.create(users(:admin))
-    answer = answers(:one)
-    get :likes, params: { aid: answer.id }, xhr: true
-    assert_response :success
-    assert_not_nil assigns(:answer)
+    perform_enqueued_jobs do
+      UserSession.create(users(:admin))
+      answer = answers(:one)
+      get :likes, params: { aid: answer.id }, xhr: true
+      assert_response :success
+      assert_not_nil assigns(:answer)
+    end
   end
 
   test 'should increase cached likes if liked' do
-    UserSession.create(users(:admin))
-    answer = answers(:one)
-    assert_difference 'answer.cached_likes' do
-      get :likes, params: { aid: answer.id }, xhr: true
-      answer.reload
+    perform_enqueued_jobs do
+      UserSession.create(users(:admin))
+      answer = answers(:one)
+      assert_difference 'answer.cached_likes' do
+        get :likes, params: { aid: answer.id }, xhr: true
+        answer.reload
+      end
     end
   end
 
   test 'should decrease cached likes if unliked' do
-    UserSession.create(users(:bob))
-    answer = answers(:one)
-    assert_difference 'answer.cached_likes', -1 do
-      get :likes, params: { aid: answer.id }, xhr: true
-      answer.reload
+    perform_enqueued_jobs do
+      UserSession.create(users(:bob))
+      answer = answers(:one)
+      assert_difference 'answer.cached_likes', -1 do
+        get :likes, params: { aid: answer.id }, xhr: true
+        answer.reload
+      end
     end
   end
 end
