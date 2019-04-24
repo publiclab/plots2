1c39bf7fd22c1216c59dc97d306dc97fa0b70a2c
SidharthBansal <bansal.sidharth2996@gmail.com>
2019-04-24 23:28:41 +0530

    test modified

--- test/integration/openid_test.rb
+++ test/integration/openid_test.rb
@@ -35,13 +35,13 @@ class LoginFlowTest < ActionDispatch::IntegrationTest
     follow_redirect!
 
     get '/openid', params: {
-      'openid.claimed_id': "https://spectralworkbench.org/openid/#{users(:jeff).username}",
-      'openid.identity': "https://spectralworkbench.org/openid/#{users(:jeff).username}",
+      'openid.claimed_id': "https://mapknitter.org/openid/#{users(:jeff).username}",
+      'openid.identity': "https://mapknitter.org/openid/#{users(:jeff).username}",
       'openid.mode': 'checkid_setup',
       'openid.ns': 'http://specs.openid.net/auth/2.0',
       'openid.ns.sreg': 'http://openid.net/extensions/sreg/1.1',
-      'openid.realm': 'https://spectralworkbench.org/',
-      'openid.return_to': "https://spectralworkbench.org/session/new?authenticity_token=RcLcGH3lzSTCC24UpPnNm56sllNaMrHg5%2FSrQzNxB%2B4%3D&back_to=&open_id=#{users(:jeff).username}&return_to=",
+      'openid.realm': 'https://mapknitter.org/',
+      'openid.return_to': "https://mapknitter.org/session/new?authenticity_token=RcLcGH3lzSTCC24UpPnNm56sllNaMrHg5%2FSrQzNxB%2B4%3D&back_to=&open_id=#{users(:jeff).username}&return_to=",
       'openid.sreg.required': 'nickname,email'
     }
 
@@ -54,13 +54,13 @@ class LoginFlowTest < ActionDispatch::IntegrationTest
     # More complete parameters:
     # {"authenticity_token"=>"RcLcGH3lzSTCC24UpPnNm56sllNaMrHg5/SrQzNxB+4=", "back_to"=>"/", "open_id"=>"warren", "openid.assoc_handle"=>"{HMAC-SHA1}{5b1d5a10}{bGMKfQ==}", "openid.claimed_id"=>"http://localhost:3000/openid/warren", "openid.identity"=>"http://localhost:3000/openid/warren", "openid.mode"=>"check_authentication", "openid.ns"=>"http://specs.openid.net/auth/2.0", "openid.ns.sreg"=>"http://openid.net/extensions/sreg/1.1", "openid.op_endpoint"=>"http://localhost:3000/openid", "openid.response_nonce"=>"2018-06-10T17:04:16ZSTb7YI", "openid.return_to"=>"http://localhost:3001/session/new?authenticity_token=RcLcGH3lzSTCC24UpPnNm56sllNaMrHg5%2FSrQzNxB%2B4%3D&back_to=%2F&open_id=warren&return_to=%2F", "openid.sig"=>"cElPJYRTb7IDCsZe3eLx639cchg=", "openid.signed"=>"assoc_handle,claimed_id,identity,mode,ns,ns.sreg,op_endpoint,response_nonce,return_to,signed,sreg.email,sreg.nickname", "openid.sreg.email"=>"jeff@unterbahn.com", "openid.sreg.nickname"=>"warren", "return_to"=>"/"}
     post '/openid?openid.claimed_id=' + users(:jeff).username, params: {
-      'openid.claimed_id': "https://spectralworkbench.org/openid/#{users(:jeff).username}",
-      'openid.identity': "https://spectralworkbench.org/openid/#{users(:jeff).username}",
+      'openid.claimed_id': "https://mapknitter.org/openid/#{users(:jeff).username}",
+      'openid.identity': "https://mapknitter.org/openid/#{users(:jeff).username}",
       'openid.mode': 'checkid_setup',
       'openid.ns': 'http://specs.openid.net/auth/2.0',
       'openid.ns.sreg': 'http://openid.net/extensions/sreg/1.1',
-      'openid.realm': 'https://spectralworkbench.org/',
-      'openid.return_to': "https://spectralworkbench.org/session/new?authenticity_token=RcLcGH3lzSTCC24UpPnNm56sllNaMrHg5%2FSrQzNxB%2B4%3D&back_to=&open_id=#{users(:jeff).username}&return_to=",
+      'openid.realm': 'https://mapknitter.org/',
+      'openid.return_to': "https://mapknitter.org/session/new?authenticity_token=RcLcGH3lzSTCC24UpPnNm56sllNaMrHg5%2FSrQzNxB%2B4%3D&back_to=&open_id=#{users(:jeff).username}&return_to=",
       'openid.sreg.required': 'nickname,email'
     }
 
@@ -80,7 +80,7 @@ class LoginFlowTest < ActionDispatch::IntegrationTest
     }
 
     # redirects back to originating site
-    assert_match /https:\/\/spectralworkbench.org\/session\/new/, @response.redirect_url
+    assert_match /https:\/\/mapknitter.org\/session\/new/, @response.redirect_url
   end
 
 end
