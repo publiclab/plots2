70cd31ab75d1a094757e0f7cde78e4cccc38c3de
eplata31 <eplata31@outlook.com>
2019-04-25 21:54:04 -0500

    BUGFIX #5527-Include hyphens in tags

--- test/unit/revision_test.rb
+++ test/unit/revision_test.rb
@@ -121,6 +121,14 @@ class RevisionsTest < ActiveSupport::TestCase
     assert_includes tag_names, 'hashtags'
   end
 
+  test 'should make tag with hyphens' do
+    revision = revisions(:hashtag_with_hyphens)
+    revision.save
+    associated_tags = revision.parent.tag
+    tag_names = associated_tags.map(&:name)
+    assert_includes tag_names, 'purple-air-ok'
+  end
+
   test 'should ignore subheaders' do
     revision = revisions(:subheader)
     revision.save
