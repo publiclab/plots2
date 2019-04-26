9d172533476ca648a5431c1d5792088d2964cd94
Jeffrey Warren <jeff@unterbahn.com>
2019-04-26 14:04:12 -0400

    Trying login via modal

--- test/system/post_test.rb
+++ test/system/post_test.rb
@@ -8,7 +8,12 @@ class PostTest < ApplicationSystemTestCase
   end
 
   test 'posting from the editor' do
-    UserSession.create(users(:bob)) # log in
+    click_on 'Log in'
+    fill_in("#username-login", with: "Bob")
+    fill_in("#password-signup", with: "secretive")
+    click_on "Log in"
+
+    follow_redirect!
 
     visit '/post'
 
