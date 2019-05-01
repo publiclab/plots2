fdcb91dab732c7f71ee88a68df224d8a33fc6092
Anthonia Okafor <tonchuks@gmail.com>
2019-05-01 12:15:36 +0000

    Revert "comitting answer_like_controller"
    This reverts commit d7e1a1108f5df54a64a69a9df0c6fbf0f7aaaa53.

--- test/functional/answer_like_controller_test.rb
+++ test/functional/answer_like_controller_test.rb
@@ -6,42 +6,34 @@ class AnswerLikeControllerTest < ActionController::TestCase
   end
 
   test 'should get show' do
-    perform_enqueued_jobs do
-      answer = answers(:one)
-      get :show, params: { id: answer.id }
-      assert_response :success
-    end
+    answer = answers(:one)
+    get :show, params: { id: answer.id }
+    assert_response :success
   end
 
   test 'should get likes' do
-    perform_enqueued_jobs do
-      UserSession.create(users(:admin))
-      answer = answers(:one)
-      get :likes, params: { aid: answer.id }, xhr: true
-      assert_response :success
-      assert_not_nil assigns(:answer)
-    end
+    UserSession.create(users(:admin))
+    answer = answers(:one)
+    get :likes, params: { aid: answer.id }, xhr: true
+    assert_response :success
+    assert_not_nil assigns(:answer)
   end
 
   test 'should increase cached likes if liked' do
-    perform_enqueued_jobs do
-      UserSession.create(users(:admin))
-      answer = answers(:one)
-      assert_difference 'answer.cached_likes' do
-        get :likes, params: { aid: answer.id }, xhr: true
-        answer.reload
-      end
+    UserSession.create(users(:admin))
+    answer = answers(:one)
+    assert_difference 'answer.cached_likes' do
+      get :likes, params: { aid: answer.id }, xhr: true
+      answer.reload
     end
   end
 
   test 'should decrease cached likes if unliked' do
-    perform_enqueued_jobs do
-      UserSession.create(users(:bob))
-      answer = answers(:one)
-      assert_difference 'answer.cached_likes', -1 do
-        get :likes, params: { aid: answer.id }, xhr: true
-        answer.reload
-      end
+    UserSession.create(users(:bob))
+    answer = answers(:one)
+    assert_difference 'answer.cached_likes', -1 do
+      get :likes, params: { aid: answer.id }, xhr: true
+      answer.reload
     end
   end
 end
