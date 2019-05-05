11d58212061ca29849b0f65c25539e82bdae990e
Jeffrey Warren <jeff@unterbahn.com>
2019-05-05 18:42:08 -0400

    Update screenshots_test.rb

--- test/system/screenshots_test.rb
+++ test/system/screenshots_test.rb
@@ -6,20 +6,12 @@ class ScreenshotsTest < ApplicationSystemTestCase
     visit '/'
 
     fill_in("searchform_input", with: "Canon")
+
+    take_screenshot # remove this later
+    # we set RAILS_SYSTEM_TESTING_SCREENSHOT to 'inline', could be 'artifact' too... see Rails API guide
     
-    # could try http://blog.mechanicles.com/2018/03/04/gotchas-rails-system-testing.html for "waiting"
-    # or sleep(5.seconds) but this might just freeze everything
-    # or assert_select ".selector", wait: 5 # i think...
-    # or https://robots.thoughtbot.com/automatically-wait-for-ajax-with-capybara
-
-    # apparently this will wait for ajax to complete? 
-    # also, not sure we can nest css like this:
-    assert_select "ul.dropdown-menu li", count: 2 #, wait: 5
-
-    # this is cool: 
-    # page.evaluate_script('jQuery.active').zero?
+    assert_selector ".typeahead li", text: "Canon A1200 IR conversion at PLOTS Barnraising at LUMCON", wait: 10
 
     take_screenshot
-    # we set RAILS_SYSTEM_TESTING_SCREENSHOT to 'inline', could be 'artifact' too... see Rails API guide
   end
 end
