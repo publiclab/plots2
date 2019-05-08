206ac79330b70e8c34f216f04db256e94ff58da0
Jeffrey Warren <jeff@unterbahn.com>
2019-05-08 17:04:44 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Plots2::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
-  # config.active_job.queue_adapter     = :resque
+  config.active_job.queue_adapter = :sidekiq
   # config.active_job.queue_name_prefix = "railsdiff_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
