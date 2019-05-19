218254a373ada6c605fc52f6bfd25ab5eac90297
Naman Gupta <01namangupta@gmail.com>
2019-05-19 20:47:54 +0530

    Minor Change

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -7,7 +7,7 @@ class RoomChannel < ApplicationCable::Channel
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def speak (message)
+  def speak(message)
     ActionCable.server.broadcast 'room_channel', message
   end
 end
