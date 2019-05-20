2c272510bf2389e45280ab6f0af920af29de214a
Jeffrey Warren <jeff@unterbahn.com>
2019-05-20 19:32:13 -0400

    Update node_shared_test.rb

--- test/unit/node_shared_test.rb
+++ test/unit/node_shared_test.rb
@@ -20,7 +20,7 @@ class NodeSharedTest < ActiveSupport::TestCase
     assert_equal 1, html.scan('<table class="table inline-grid nodes-grid nodes-grid-test nodes-grid-test-').length
     assert_equal 1, html.scan('<table').length
     assert_equal 5, html.scan('nodes-grid-test').length
-    assert html.scan('<td class="author">').length > 2 # but not 3 because it shouldn't appear twice
+    assert_equal 2, html.scan('<td class="author">').length # but not 3 because it shouldn't appear twice
     assert_equal 1, html.scan(nodes(:one).title).length
   end
 
