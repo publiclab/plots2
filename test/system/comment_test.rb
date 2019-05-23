aecf4c602f813beefd5db5e70d9f207926a5d687
Jeffrey Warren <jeff@unterbahn.com>
2019-05-23 17:56:05 -0400

    adding wait_for_ajax

--- test/system/comment_test.rb
+++ test/system/comment_test.rb
@@ -23,19 +23,12 @@ class SearchTest < ApplicationSystemTestCase
     # we could see if an image gets created:
     # assert_no_difference 'Image.count' do
 
-      # could try this too:
-      #within('comment-form') do
-      #  click_link('choose one')
-      #end
-
       # note this would be more complex with more than one comment bc of "reply" form
       attach_file('fileinput', "#{Rails.root.to_s}/public/images/pl.png", visible: false)
 
     # end
 
-    #within('comment-form') do
-    #end
-
+    wait_for_ajax # wait for it to upload and re-appear
     assert_selector('textarea#text-input', text: '![](/some/imageid.png)')
 
     # click_button 'Preview'
@@ -43,9 +36,23 @@ class SearchTest < ApplicationSystemTestCase
 
     click_button 'Publish'
 
-    assert_selector('.comment-form textarea#text-input', text: '')
+    assert_selector('.comment-form textarea#text-input', text: '') # the textarea should clear
 
     # assert some javascript:
     # assert_equal 1, page.evaluate_script("$('.typeahead.dropdown-menu').find('li').length")
+
   end
+end
+
+# https://stackoverflow.com/questions/36536111/waiting-for-ajax-with-capybara-poltergeist
+def wait_for_ajax
+  Timeout.timeout(Capybara.default_max_wait_time) do
+    loop until finished_all_ajax_requests?
+  end
+end
+
+def finished_all_ajax_requests?
+  request_count = page.evaluate_script("$.active").to_i
+  request_count && request_count.zero?
+rescue Timeout::Error
 end
