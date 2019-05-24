c62119b58f8bcc96d7d3296e70cc73c9bb52f1bc
Emmanuel Hayford <siawmensah@gmail.com>
2019-05-24 23:59:10 +0200

    Tiny refactor (#5779)
    * Tiny refactor
    
    * Put mysql gem back to where it belongs
    
    * Put msql gem back to where it belongs
    
    * Fix old testing review PR
    
    * Remove unused file [skip ci]
    
    * Remove unused folder [skip ci]
    
    * Update configs

--- test/unit/answer_mailer_test.rb
+++ test/unit/answer_mailer_test.rb
@@ -12,9 +12,9 @@ class AnswerMailerTest < ActionMailer::TestCase
     email = ActionMailer::Base.deliveries.last
     assert_equal ["notifications@#{request_host}"], email.from
     assert_equal [user.email], email.to
-    assert_equal '[PLab] Question: ' + answer.node.title.truncate(30,omission: '...?') + ' An answer has been posted on Public Lab' + + " (#a#{answer.id})", email.subject
-    assert email.body.include?("Hi! <a href='https://#{request_host}/profile/#{ answer.author.name }'>#{ answer.author.name }</a> responded :
-<hr /><p>#{ answer.content }</p><hr />")
+    assert_equal '[PLab] Question: ' + answer.node.title.truncate(30, omission: '...?') + ' An answer has been posted on Public Lab' + + " (#a#{answer.id})", email.subject
+    assert email.body.include?("Hi! <a href='https://#{request_host}/profile/#{answer.author.name}'>#{answer.author.name}</a> responded :
+<hr /><p>#{answer.content}</p><hr />")
   end
 
   test 'notify other answer authors' do
@@ -43,32 +43,39 @@ class AnswerMailerTest < ActionMailer::TestCase
     email = ActionMailer::Base.deliveries.last
     assert_equal ["notifications@#{request_host}"], email.from
     assert_equal [user.email], email.to
-    assert_equal "[PublicLab] New answer to Question: " + answer.node.title + " (#a#{answer.id})" , email.subject
+    assert_equal "[PublicLab] New answer to Question: " + answer.node.title + " (#a#{answer.id})", email.subject
     assert email.body.include?("Hi! There's been a new answer posted for the question '<a href='https://#{request_host}#{answer.node.path(:question)}'>#{answer.node.title}</a>' that you liked")
   end
 
   test 'notify answer author when answer is accepted' do
     user = users(:bob)
     answer = answers(:one)
+
     assert_difference 'ActionMailer::Base.deliveries.size', 1 do
       AnswerMailer.notify_answer_accept(user, answer).deliver_now
     end
-    assert !ActionMailer::Base.deliveries.empty?
+
+    assert_not ActionMailer::Base.deliveries.empty?
 
     email = ActionMailer::Base.deliveries.last
+    answer_accepted = '[PublicLab] Your answer has been accepted'
+    answer_for_question = "Your answer for the question <a href='https://#{request_host}#{answer.node.path(:question)}'>#{answer.node.title}</a> has been accepted"
+
     assert_equal ["notifications@#{request_host}"], email.from
     assert_equal [user.email], email.to
-    assert_equal '[PublicLab] Your answer has been accepted', email.subject
-    assert email.body.include?("Your answer for the question <a href='https://#{request_host}#{answer.node.path(:question)}'>#{answer.node.title}</a> has been accepted")
+    assert_equal answer_accepted, email.subject
+    assert email.body.include?(answer_for_question)
   end
 
   test 'notify answer author when answer is liked' do
     user = users(:bob)
     answer = answers(:one)
+
     assert_difference 'ActionMailer::Base.deliveries.size', 1 do
       AnswerMailer.notify_answer_like(user, answer).deliver_now
     end
-    assert !ActionMailer::Base.deliveries.empty?
+
+    assert_not ActionMailer::Base.deliveries.empty?
 
     email = ActionMailer::Base.deliveries.last
     assert_equal ["notifications@#{request_host}"], email.from
