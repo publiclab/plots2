402552b7cd99d431ac709df19e2b1d5a78ebc6e3
viditchitkara <chitkara.vidit@gmail.com>
2019-06-01 23:29:59 +0530

    minor changes

--- app/controllers/answer_like_controller.rb
+++ app/controllers/answer_like_controller.rb
@@ -11,7 +11,6 @@ class AnswerLikeController < ApplicationController
       AnswerSelection.set_likes(current_user.uid, @answer.id, false)
     else
       AnswerSelection.set_likes(current_user.uid, @answer.id, true)
-      User.find(current_user.uid)
     end
     @answer.reload
     respond_to do |format|
