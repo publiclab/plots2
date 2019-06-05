28bd63ef1de8588b8461604d34083c65018affe9
Lekhika Dugtal <lekhidugtal@gmail.com>
2019-06-05 20:28:10 +0530

    Side column

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,13 +1,8 @@
 module SearchHelper
   def create_nav_links(active_page, query)
     links = [
-      { section: "search-all", text: "All Content", path: "/search/#{query}" },
-      { section: "search-notes", text: "Notes", path: "/search/notes/#{query}" },
-      { section: "search-wikis", text: "Wikis", path: "/search/wikis/#{query}/" },
-      { section: "search-profiles", text: "Profiles", path: "/search/profiles/#{query}/" },
-      { section: "search-tags", text: "Tags", path: "/search/tags/#{query}/" },
-      { section: "search-questions", text: "Questions", path: "/search/questions/#{query}/" },
-      { section: "search-places", text: "Places", path: "/search/places/#{query}/" }
+      { section: "search-all", text: "Content", path: "/search/#{query}" },
+      { section: "search-profiles", text: "People", path: "/search/profiles/#{query}/" }
     ]
 
     result = ""
