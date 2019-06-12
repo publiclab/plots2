1be7d2abaf7618285406be4ecf02c97de9c3fd6e
Jeffrey Warren <jeff@unterbahn.com>
2019-06-12 13:32:36 -0400

    Connect tags search to suggested tags instead of tagged notes (#5825)
    * Connect tags search to suggested tags instead of tagged notes
    
    https://github.com/publiclab/plots2/blob/master/app/controllers/tag_controller.rb#L376-L392
    
    `SearchService.new.search_tags('awesome')`
    
    * Update tag_controller.rb
    
    * add title, nid, path so api/srch can assemble a DocList from tags
    
    * Update tag_test.rb
    
    * Update tag.rb
    
    * Update search_service.rb

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -33,6 +33,19 @@ class Tag < ApplicationRecord
     tid
   end
 
+  # alias
+  def nid
+    id
+  end
+
+  def title
+    name
+  end
+
+  def path
+    "/#{name}"
+  end
+
   def run_count
     self.count = NodeTag.where(tid: tid).count
     save
@@ -53,8 +66,8 @@ class Tag < ApplicationRecord
     hash.sort_by { |_, v| v }.reverse.first(10).to_h
   end
 
-  def belongs_to(current_user, nid)
-    node_tag = node_tag.find_by(nid: nid)
+  def belongs_to(current_user, node_id)
+    node_tag = node_tag.find_by(nid: node_id)
     node_tag && node_tag.uid == current_user.uid || node_tag.node.uid == current_user.uid
   end
 
