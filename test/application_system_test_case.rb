417979dc61fd48ad2491b42211a9c16e149ea240
Jeffrey Warren <jeff@unterbahn.com>
2019-06-14 11:02:44 -0400

    Add MobileSystemTestCase
    http://ruby.11.x6.nabble.com/Feature-Request-Change-screensize-while-running-System-Test-td5014544.html

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -5,3 +5,10 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => chromeOptions})
   driven_by :selenium, using: :chrome, screen_size: [1400, 2000], options: { desired_capabilities: caps }
 end
+
+class MobileSystemTestCase < ActionDispatch::SystemTestCase
+  # iphone 6 screen width in points: https://www.paintcodeapp.com/news/ultimate-guide-to-iphone-resolutions
+  chromeOptions = %w(--headless  --disable-gpu --no-sandbox --remote-debugging-port=9222)
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => chromeOptions})
+  driven_by :selenium, using: :chrome, screen_size: [375, 1400], options: { desired_capabilities: caps }
+end
