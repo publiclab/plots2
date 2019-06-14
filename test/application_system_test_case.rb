b949918bc8d0544a1c574f3a9782eab56e5e80d3
Jeffrey Warren <jeff@unterbahn.com>
2019-06-14 10:27:15 -0400

    bump regular screenshots to 1400x2000

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,5 +3,5 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   chromeOptions = %w(--headless  --disable-gpu --no-sandbox --remote-debugging-port=9222)
   caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => chromeOptions})
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { desired_capabilities: caps }
+  driven_by :selenium, using: :chrome, screen_size: [1400, 2000], options: { desired_capabilities: caps }
 end
