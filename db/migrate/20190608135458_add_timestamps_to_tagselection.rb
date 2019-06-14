0cf2ca8c2a160e0cb00d422c95c3d3668cbcf48e
Cess <cessmbuguar@gmail.com>
2019-06-14 13:51:36 +0300

    Change timestamps of existing records
    have them a date prior to start of the application to avoid invalid data and server load
    
    Thanks @skilfullycurled :smiley:

--- db/migrate/20190608135458_add_timestamps_to_tagselection.rb
+++ db/migrate/20190608135458_add_timestamps_to_tagselection.rb
@@ -2,9 +2,11 @@ class AddTimestampsToTagselection < ActiveRecord::Migration[5.2]
   def change
     add_timestamps :tag_selections, null: true
 
-    TagSelection.update_all(created_at: DateTime.now, updated_at: DateTime.now)
+    date =  Date.new(2000,1,1).to_datetime
+    TagSelection.update_all(created_at: date, updated_at: DateTime.now)
 
-    change_column :tag_selections, :created_at,  :datetime, null: false
+
+    change_column :tag_selections, :created_at, :datetime, null: false
     change_column :tag_selections, :updated_at, :datetime, null: false
   end
 end
--- db/migrate/20190613193708_add_timestamp_to_community_tags.rb
+++ db/migrate/20190613193708_add_timestamp_to_community_tags.rb
@@ -2,7 +2,14 @@ class AddTimestampToCommunityTags < ActiveRecord::Migration[5.2]
   def change
     add_timestamps :community_tags, null: true
 
-    NodeTag.update_all(created_at: DateTime.now, updated_at: DateTime.now)
+    NodeTag.where('date != 0').map do |nd|
+      nd.update(created_at: Time.at(nd.date).to_datetime, updated_at: DateTime.now)
+    end
+
+    date =  Date.new(2000,1,1).to_datetime
+    NodeTag.where(created_at: nil).update(created_at: date, updated_at: DateTime.now)
+
+
 
     change_column :community_tags, :created_at, :datetime, null: false
     change_column :community_tags, :updated_at, :datetime, null: false
