c9ab609312b024141b27b3bd75afe7eb374eb6d1
jywarren <jeff@unterbahn.com>
2019-06-17 16:16:19 -0400

    fixes for incoming mail integration tests

--- test/integration/incoming_mail_parsing_test/yahoo_parsing_test.rb
+++ test/integration/incoming_mail_parsing_test/yahoo_parsing_test.rb
@@ -5,14 +5,14 @@ class YahooParsingTest < ActionDispatch::IntegrationTest
     require 'mail'
     mail = Mail.read('test/fixtures/incoming_test_emails/yahoo/incoming_yahoo_email.eml')
     # Mail contain ["01namangupta@gmail.com"] in from field.
-    answer = Answer.last
-    mail.subject = "Re: (#a#{answer.id})"
+    node = Node.find(21) # this is the nid used in the .eml fixture
+    mail.subject = "Re: (##{node.id})"
     Comment.receive_mail(mail)
     f = File.open('test/fixtures/incoming_test_emails/yahoo/final_parsed_comment.txt', 'r')
-    comment = Comment.last
+    comment = Comment.last # this should be the just-created comment
     user_email = mail.from.first
     assert_equal comment.comment, f.read
-    assert_equal comment.aid, answer.id
+    assert_equal comment.nid, node.id
     assert_equal comment.message_id, mail.message_id
     assert_equal comment.comment_via, 1
     assert_equal User.find(comment.uid).email, user_email
