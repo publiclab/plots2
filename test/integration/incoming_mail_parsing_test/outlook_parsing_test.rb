397dcb2af68bb52ff98b15c241e57b0e3c80128d
Jeffrey Warren <jeff@unterbahn.com>
2019-06-17 16:05:55 -0400

    refactor outlook comment reply from answer comment

--- test/integration/incoming_mail_parsing_test/outlook_parsing_test.rb
+++ test/integration/incoming_mail_parsing_test/outlook_parsing_test.rb
@@ -1,19 +1,19 @@
 require 'test_helper'
 class OutlookParsingTest < ActionDispatch::IntegrationTest
-  test 'should parse incoming mail from outlook service correctly and add answer comment' do
+  test 'should parse incoming mail from outlook service correctly and add comment reply' do
     require 'mail'
     mail = Mail.read('test/fixtures/incoming_test_emails/outlook/incoming_outlook_email.eml')
-    answer = Answer.last
-    mail.subject = "Re: (#a#{answer.id})"
+    comment = comments(:first)
+    mail.subject = "Re: (##{comment.id})"
     Comment.receive_mail(mail)
     f = File.open('test/fixtures/incoming_test_emails/outlook/final_parsed_comment.txt', 'r')
-    comment = Comment.last
+    reply = Comment.last
     user_email = mail.from.first
-    assert_equal comment.comment, f.read
-    assert_equal comment.aid, answer.id
-    assert_equal comment.message_id, mail.message_id
-    assert_equal comment.comment_via, 1
-    assert_equal User.find(comment.uid).email, user_email
+    assert_equal reply.comment, f.read
+    assert_equal reply.reply_to, comment.id
+    assert_equal reply.message_id, mail.message_id
+    assert_equal reply.comment_via, 1
+    assert_equal User.find(reply.uid).email, user_email
     f.close()
   end
 end
