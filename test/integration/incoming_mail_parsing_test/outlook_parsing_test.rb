409c04fea5a968d10ab0f3642866490a9b1a0589
viditchitkara <chitkara.vidit@gmail.com>
2019-06-18 21:32:47 +0530

    fix reply to comment via email feature

--- test/integration/incoming_mail_parsing_test/outlook_parsing_test.rb
+++ test/integration/incoming_mail_parsing_test/outlook_parsing_test.rb
@@ -5,7 +5,7 @@ class OutlookParsingTest < ActionDispatch::IntegrationTest
   test 'should parse incoming mail from outlook service correctly and add comment reply' do
     mail = Mail.read('test/fixtures/incoming_test_emails/outlook/incoming_outlook_email.eml')
     comment = comments(:first)
-    mail.subject = "Re: (##{comment.nid})"
+    mail.subject = "Re: (##{comment.nid}) - #c#{comment.id}"
     Comment.receive_mail(mail)
     f = File.open('test/fixtures/incoming_test_emails/outlook/final_parsed_comment.txt', 'r')
     reply = Comment.last
