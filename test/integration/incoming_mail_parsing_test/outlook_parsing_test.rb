6e671614a7acb5e9ac850a75f2c4473e8ad9b0f4
Jeffrey Warren <jeff@unterbahn.com>
2019-06-18 08:56:24 -0400

    testing tests (#5885)
    * testing tests
    
    * try adding regular 'rails test'
    
    * Update comment_test.rb
    
    * Update tags.html.erb
    
    * change questions.count to questions
    
    * Update search.rb
    
    * Update _comments.html.erb
    
    * separate test tasks
    
    * Update search_service_test.rb
    
    Adjusted for #5825
    
    * remove answer comment test
    
    * Update incoming_outlook_email.eml
    
    * refactor outlook comment reply from answer comment
    
    * fixes for incoming mail integration tests
    
    * Update stats_controller_test.rb
    
    * Update search_controller_test.rb
    
    * intercept comment id and correct test
    
    * Update outlook_parsing_test.rb
    
    * Update gmail_parsing_test.rb
    
    * changes in I18n tests
    
    * changes
    
    * email test changes
    
    * stats test fix
    
    * We are not using reply to for comments in emails
    
    Removing this assertion.

--- test/integration/incoming_mail_parsing_test/outlook_parsing_test.rb
+++ test/integration/incoming_mail_parsing_test/outlook_parsing_test.rb
@@ -1,19 +1,19 @@
 require 'test_helper'
+require 'mail'
+
 class OutlookParsingTest < ActionDispatch::IntegrationTest
-  test 'should parse incoming mail from outlook service correctly and add answer comment' do
-    require 'mail'
+  test 'should parse incoming mail from outlook service correctly and add comment reply' do
     mail = Mail.read('test/fixtures/incoming_test_emails/outlook/incoming_outlook_email.eml')
-    answer = Answer.last
-    mail.subject = "Re: (#a#{answer.id})"
+    comment = comments(:first)
+    mail.subject = "Re: (##{comment.nid})"
     Comment.receive_mail(mail)
     f = File.open('test/fixtures/incoming_test_emails/outlook/final_parsed_comment.txt', 'r')
-    comment = Comment.last
+    reply = Comment.last
     user_email = mail.from.first
-    assert_equal comment.comment, f.read
-    assert_equal comment.aid, answer.id
-    assert_equal comment.message_id, mail.message_id
-    assert_equal comment.comment_via, 1
-    assert_equal User.find(comment.uid).email, user_email
+    assert_equal reply.comment, f.read
+    assert_equal reply.message_id, mail.message_id
+    assert_equal reply.comment_via, 1
+    assert_equal User.find(reply.uid).email, user_email
     f.close()
   end
 end
