c9280fc6ffcc1a4dc0f9be0cc2cd773f363b2315
Naman Gupta <01namangupta@gmail.com>
2019-06-28 00:37:19 +0530

    Basic Setup of Action Cable (#5744)
    * Added room channel
    
    * Basic Setup Of Action Cable
    
    * Basic Implementation of Testing Channel Completed
    
    * Minor Change
    
    * Minor Change
    
    * Minor Change
    
    * Added room channel code in js and removed code in coffeescript
    
    * Added ActionCable Config Url and Action Cable Allowed Request Origin
    
    * Added ActionCable Config Url and Action Cable Allowed Request Origin
    
    * Added ActionCable Config Url and Action Cable Allowed Request Origin
    
    * Added Cookie for identifying user in websocket connection
    
    * Minor Changes

---
+++ app/channels/application_cable/connection.rb
@@ -0,0 +1,21 @@
+module ApplicationCable
+  class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+    def connect
+      self.current_user = find_verified_user
+    end
+    
+    def find_verified_user
+      if cookies.signed['user_token'] != nil
+        user = User.where(persistence_token: cookies.signed['user_token'])
+        if user.any?
+          return user.first
+        else
+          return nil
+        end
+      else
+        return nil
+      end
+    end
+  end
+end
