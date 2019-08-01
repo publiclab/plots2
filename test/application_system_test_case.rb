2f1a29e930d8ed7c63ba593827c234af9bc73f04
Gaurav Sachdeva <sachdeva.gaurav1997@gmail.com>
2019-08-02 03:07:23 +0530

    retry

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  chromeOptions = %w(--headless  --disable-gpu --no-sandbox --remote-debugging-port=9222)
+  chromeOptions = %w(--headless  --disable-gpu --no-sandbox --disable-dev-shm-usage --remote-debugging-port=9222)
   caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => chromeOptions})
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { desired_capabilities: caps }
 end
