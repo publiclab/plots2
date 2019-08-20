20df5ac85bc89bf0ea44ed68a163675e75384dea
IshaGupta18 <ishagupta1828@gmail.com>
2019-08-20 21:18:32 +0530

    system test

---
+++ test/system/csvfiles_controllers_test.rb
@@ -0,0 +1,17 @@
+require "application_system_test_case"
+
+class CsvfilesControllersTest < ApplicationSystemTestCase
+  Capybara.default_max_wait_time = 60
+
+  test 'simple-data-grapher powertag' do
+    visit '/post?body=[simple-data-grapher:i/2/6]'
+
+    take_screenshot
+  end
+
+  test 'visit simple-data-grapher' do
+    visit '/graph'
+
+    assert_selector("h2", text: " Simple Data Grapher")
+  end
+end
