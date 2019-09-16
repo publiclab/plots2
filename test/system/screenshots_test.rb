f29f90d6690d5a3c6d578221af79c0cd2b7bafee
Isha Gupta <40794215+IshaGupta18@users.noreply.github.com>
2019-09-16 23:16:28 +0530

    Adding a followers column to tag page (#5632)
    * adding a followers column to tag page
    
    * Update screenshots_test.rb

--- test/system/screenshots_test.rb
+++ test/system/screenshots_test.rb
@@ -47,6 +47,16 @@ class ScreenshotsTest < ApplicationSystemTestCase
     take_screenshot
   end
 
+  test 'tag wildcard' do
+    visit '/tag/spect*'
+    take_screenshot
+  end
+
+  test 'tag contributors page' do
+    visit '/contributors/spectrometer'
+    take_screenshot
+  end
+
   test 'wiki' do
     visit '/wiki'
     take_screenshot
