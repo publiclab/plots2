ce899a3e89b2d54211db8d822c532ee7931f13ac
Jade Dickinson <14929975+JadeDickinson@users.noreply.github.com>
2019-10-11 12:23:52 +0100

    Fix rubocop offenses in search_helper

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 module SearchHelper
   def create_nav_links(active_page, query)
     links = [
-      { section: "search-all", text: "Content", path: "/search/#{query}" },
-      { section: "search-profiles", text: "People", path: "/search/profiles/#{query}" }
+      { section: 'search-all', text: 'Content', path: "/search/#{query}" },
+      { section: 'search-profiles', text: 'People', path: "/search/profiles/#{query}" }
     ]
 
-    result = ""
+    result = ''
     links.each do |link|
       active_link =
         if active_page == link[:section]
-          "list-group-item list-group-item-action active"
+          'list-group-item list-group-item-action active'
         else
-          "list-group-item list-group-item-action"
+          'list-group-item list-group-item-action'
         end
       result += " <li><a href='#{link[:path]}' class='#{active_link}'>#{link[:text]}</a> </li>"
     end
@@ -20,19 +22,19 @@ module SearchHelper
 
   def create_nav_links_for_by_type(active_page, query)
     links = [
-      { section: "search-all", text: "All content types", path: "/search/content/#{query}" },
-      { section: "search-questions", text: "Questions", path: "/search/questions/#{query}" },
-      { section: "search-notes", text: "Notes", path: "/search/notes/#{query}" },
-      { section: "search-wikis", text: "Wikis", path: "/search/wikis/#{query}" }
+      { section: 'search-all', text: 'All content types', path: "/search/content/#{query}" },
+      { section: 'search-questions', text: 'Questions', path: "/search/questions/#{query}" },
+      { section: 'search-notes', text: 'Notes', path: "/search/notes/#{query}" },
+      { section: 'search-wikis', text: 'Wikis', path: "/search/wikis/#{query}" }
     ]
 
-    result = ""
+    result = ''
     links.each do |link|
       active_link =
         if active_page == link[:section]
-          "dropdown-item active"
+          'dropdown-item active'
         else
-          "dropdown-item"
+          'dropdown-item'
         end
       result += " <a href='#{link[:path]}' class='#{active_link}'>#{link[:text]}</a>"
     end
