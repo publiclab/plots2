b85b3128a153c332eb1ffa97f13a2d1f623de4fc
Jade Dickinson <14929975+JadeDickinson@users.noreply.github.com>
2019-10-11 12:44:48 +0100

    Fix rubocop offenses in comment_helper

--- app/helpers/comment_helper.rb
+++ app/helpers/comment_helper.rb
@@ -1,14 +1,13 @@
+# frozen_string_literal: true
+
 module CommentHelper
   class CommentError < ArgumentError
   end
 
   def create_comment(node, user, body)
     @comment = node.add_comment(uid: user.uid, body: body)
-    if user && @comment.save
-      @comment.notify user
-      return @comment
-    else
-      raise CommentError
-    end
+    raise CommentError unless user && @comment.save
+
+    @comment
   end
 end
