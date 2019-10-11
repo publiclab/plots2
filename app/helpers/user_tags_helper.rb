8fd34bb9c634cb5625e5393ac28b89dfc929fb20
Jade Dickinson <14929975+JadeDickinson@users.noreply.github.com>
2019-10-11 12:41:59 +0100

    Fix Style/IfUnlessModifier offense in user_tags_helper

--- app/helpers/user_tags_helper.rb
+++ app/helpers/user_tags_helper.rb
@@ -4,9 +4,7 @@ module UserTagsHelper
   def fetch_tags(uid, type)
     tag_types = %w[skill gear role tool]
     tags = []
-    if tag_types.include? type
-      tags = UserTag.where(uid: uid).where('value LIKE ?', type + ':' + '%')
-    end
+    tags = UserTag.where(uid: uid).where('value LIKE ?', type + ':' + '%') if tag_types.include? type
     tags
   end
 end
