fe55f99acdcf9dae3c035012582cd515cc57c622
Dave Menninger <vagrant@vagrant.vm>
2019-10-17 17:26:08 +0000

    fix indentation

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -400,11 +400,13 @@ class WikiController < ApplicationController
       nids = @nodes.collect(&:nid) || []
       @notes = Node.where(status: 1, type: %w(page))
                    .where('node.nid IN (?)', nids)
-                   .where('(type = "note" OR type = "page" OR type = "map") AND node.status = 1 AND (node.title LIKE ? OR node_revisions.title LIKE ? OR node_revisions.body LIKE ? OR term_data.name = ?)',
-                          '%' + params[:topic] + '%',
-                          '%' + params[:topic] + '%',
-                          '%' + params[:topic] + '%',
-                          params[:topic])
+                   .where(
+                     '(type = "note" OR type = "page" OR type = "map") AND node.status = 1 AND (node.title LIKE ? OR node_revisions.title LIKE ? OR node_revisions.body LIKE ? OR term_data.name = ?)',
+                     '%' + params[:topic] + '%',
+                     '%' + params[:topic] + '%',
+                     '%' + params[:topic] + '%',
+                     params[:topic]
+                   )
                    .includes(:revision, :tag)
                    .references(:node_revision, :term_data)
                    .order('node_revisions.timestamp DESC')
@@ -413,11 +415,13 @@ class WikiController < ApplicationController
       nids = @nodes.collect(&:nid) || []
       @nodes = Node.where(status: 1, type: %w(page))
                    .where('node.nid IN (?)', nids)
-                   .where('(type = "note" OR type = "page" OR type = "map") AND node.status = 1 AND (node.title LIKE ? OR node_revisions.title LIKE ? OR node_revisions.body LIKE ? OR term_data.name = ?)',
-                          '%' + params[:topic] + '%',
-                          '%' + params[:topic] + '%',
-                          '%' + params[:topic] + '%',
-                          params[:topic])
+                   .where(
+                     '(type = "note" OR type = "page" OR type = "map") AND node.status = 1 AND (node.title LIKE ? OR node_revisions.title LIKE ? OR node_revisions.body LIKE ? OR term_data.name = ?)',
+                     '%' + params[:topic] + '%',
+                     '%' + params[:topic] + '%',
+                     '%' + params[:topic] + '%',
+                     params[:topic]
+                   )
                    .includes(:revision, :tag)
                    .references(:node_revision, :term_data)
                    .order('node_revisions.timestamp DESC')
