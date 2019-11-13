fce58a50bb732e4a4b5aff9aff08dc64b749ca16
Nicolei Ocana <nicoleiocana@gmail.com>
2019-11-13 18:17:08 +0000

    Update install_cloudnine.sh for AWS Cloud9 for Amazon Linux Platform

--- install_cloudnine.sh
+++ install_cloudnine.sh
@@ -1,12 +1,14 @@
-# updated to ruby version in gem file
+# updated for AWS Cloud9 with Amazon Linux Platform
 
 read -p "Enter your cloud9 username: " un
 rvm install ruby-2.4.4
 source $(rvm 2.4.4 do rvm env --path)
-rvm use ruby-2.4.4
+bash -l -c "rvm use ruby-2.4.4"
 gem install rails -v 5.2.0
-sudo apt-get update
-sudo apt-get -y install imagemagick ruby-rmagick
+sudo yum -y update
+sudo yum install -y ImageMagick-devel 
+gem install rmagick
+sudo yum install -y redis
 npm install -g yarn
 yarn install
 gem install bundler
@@ -15,4 +17,7 @@ cp db/schema.rb.example db/schema.rb
 cp config/database.yml.sqlite.example config/database.yml
 rake db:setup
 rake cloud9 username=$un
-echo "Done! Run the application with 'rails s -b \$IP -p \$PORT'"
+echo "Done! 1. Change directory out then back into plots2/
+      2. Start the redis-server with 'redis-server'
+      3. Run SideKiq: 'bundle exec sidekiq'
+      4. Run the application with 'rails s -b \$IP -p \$PORT'"
