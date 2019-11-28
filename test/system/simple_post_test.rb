ab814a9bcdfd0dc5d31415c36595f76104925559
Cess <cessmbuguar@gmail.com>
2019-11-28 16:56:10 +0300

    add more tests

---
+++ test/system/simple_post_test.rb
@@ -0,0 +1,22 @@
+require "application_system_test_case"
+
+class SimplePostTest < ApplicationSystemTestCase
+  Capybara.default_max_wait_time = 60
+
+  test 'simple editor post' do
+    visit '/'
+    click_on 'Login'
+    fill_in('username-login', with: 'steff1')
+    fill_in('password-signup', with: 'secretive')
+    click_on 'Log in'
+    visit(simple_editor_path)
+
+    find('button#location_button').click
+    take_screenshot
+
+    fill_in('title-input', with: 'My story')
+    find('button.ple-publish').click
+
+    assert_selector('h1', text: "My story")
+  end
+end
