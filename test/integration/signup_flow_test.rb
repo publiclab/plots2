bb3f52e374ec1a47575980ecb0f3aa8a5d0bbfcf
Robert <robertchen@live.com>
2019-12-08 20:50:35 +0000

    Added additional tests

--- test/integration/signup_flow_test.rb
+++ test/integration/signup_flow_test.rb
@@ -101,10 +101,30 @@ class SignUpTest < ActionDispatch::IntegrationTest
     assert response.body.include? 'Email should look like an email address.'
   end
 
-  test 'no redundant email error messages' do
+  test 'custom blank email error messages' do
     post '/register', params: { 
       user: {
-        username: '', 
+        username: 'validusername', 
+        email: '',
+        password: 'validpassword',
+        password_confirmation: 'validpassword',
+      },
+      spamaway: {
+        follow_instructions: '',
+        statement1: '',
+        statement2: '',
+        statement3: '',
+        statement4: ''
+      }
+    }
+
+    assert response.body.include? 'Email cannot be blank'
+  end
+
+  test 'no redundant username error messages' do
+    post '/register', params: { 
+      user: {
+        username: '^', 
         email: '',
         password: '',
         password_confirmation: '',
@@ -118,8 +138,27 @@ class SignUpTest < ActionDispatch::IntegrationTest
       }
     }
 
+    assert !(response.body.include? 'Username is invalid')
+  end
+
+  test 'no redundant email error messages' do
+    post '/register', params: { 
+      user: {
+        username: 'validusername', 
+        email: 'notanemail',
+        password: '',
+        password_confirmation: '',
+      },
+      spamaway: {
+        follow_instructions: '',
+        statement1: '',
+        statement2: '',
+        statement3: '',
+        statement4: ''
+      }
+    }
+
     assert !(response.body.include? 'Email is invalid')
-    assert response.body.include? 'Email should look like an email address.'
   end
 
 end
