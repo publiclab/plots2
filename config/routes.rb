64cd326e3131b733e6b260b288f069739875e900
Cess <cessmbuguar@gmail.com>
2019-12-22 13:16:27 +0300

    redesign image upload div

--- config/routes.rb
+++ config/routes.rb
@@ -338,7 +338,7 @@ Plots2::Application.routes.draw do
   get 'post' => 'editor#post', :as => :editor_post
   post 'post' => 'editor#post', :as => :editor_path
   get 'post/choose' => 'editor#choose'
-  get '/simple_post' => 'editor#simple', :as => :simple_editor
+  get 'simple_post' => 'editor#simple', :as => :simple_editor
   get 'legacy' => 'editor#legacy'
   get 'editor' => 'editor#editor'
   get 'editor/rich/(:n)' => 'editor#rich'
