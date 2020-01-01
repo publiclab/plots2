8ea6ec71f2c29ef5aa671317dd3ea88f44924778
Natalie <natalie.stjean@gmail.com>
2020-01-01 10:22:01 -0500

    clean up code

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -21,28 +21,29 @@ class MapController < ApplicationController
   end
 
   def map
-    return if current_user&.has_power_tag("lat").blank? || current_user&.has_power_tag("lon").blank?
+    @lat = 0
+    @lon = 0
+    @zoom = 3
 
-    @user_lat = current_user.get_value_of_power_tag("lat").to_f
-    @user_lon = current_user.get_value_of_power_tag("lon").to_f
+    if current_user&.has_power_tag("lat") && current_user&.has_power_tag("lon")
+      @lat = current_user.get_value_of_power_tag("lat").to_f
+      @lon = current_user.get_value_of_power_tag("lon").to_f
+    end
+    @zoom = current_user.get_value_of_power_tag("zoom").to_f if current_user&.has_power_tag("zoom")
   end
 
   def wiki
     @node = Node.find_wiki(params[:id])
 
-    if @node.blank?
-      flash[:warning] = "Wiki page not found."
+    if @node.blank? || @node.has_power_tag("lat").blank? || @node.has_power_tag("lon").blank?
+      flash[:warning] = @node.blank? ? "Wiki page not found." : "No location found for wiki page."
       redirect_to controller: 'map', action: 'map'
       return
     end
 
-    if @node.has_power_tag("lat").blank? && @node.has_power_tag("lon").blank?
-      flash[:warning] = "No location found for this wiki page."
-    else
-      @lat = @node.power_tag("lat").to_f
-      @lon = @node.power_tag("lon").to_f
-      @zoom = @node.power_tag("zoom").to_f if @node.has_power_tag("zoom")
-    end
+    @lat = @node.power_tag("lat").to_f
+    @lon = @node.power_tag("lon").to_f
+    @zoom = @node.has_power_tag("zoom") ? @node.power_tag("zoom").to_f : 6
 
     render :map
   end
