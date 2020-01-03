eeb1674a8184657bdd2ba3921038cc4aa0bac7a4
Uzay-G <52892257+Uzay-G@users.noreply.github.com>
2020-01-03 15:27:24 +0100

    codeclimate

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -27,7 +27,7 @@ class UserSessionsController < ApplicationController
       hash_params = URI.parse("#" + params[:hash_params]).to_s
     end
     @user = User.where(email: auth["info"]["email"]) ? User.find_by(email: auth["info"]["email"]) : User.find(@identity.user.id)
-    if @user && @user.status == 0
+    if @user && @user.status.zero?
       flash[:error] = I18n.t('user_sessions_controller.user_has_been_banned', username: @user.username).html_safe
       redirect_to root_url
     elsif @user && @user.status == 5
