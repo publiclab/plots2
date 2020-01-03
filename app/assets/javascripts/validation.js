951c1fe1fae6bb155eb81fd4474071c242e43561
Vladimir <vladimir.mikulic2@gmail.com>
2020-01-03 01:53:09 +0100

    ADD: Support for pl.org/register form validation

--- app/assets/javascripts/validation.js
+++ app/assets/javascripts/validation.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('#edit-form').validate({
+  $("#edit-form").validate({
     rules: {
       email: {
         required: true,
@@ -10,21 +10,21 @@ $(document).ready(function() {
         minlength: 8
       },
       password2: {
-        equalTo: '#password1'
+        equalTo: "#password1"
       }
     },
     messages: {
       password1: {
-        required: 'Please enter password',
-        minlength: 'Password should be minimum 8 characters long'
+        required: "Please enter password",
+        minlength: "Password should be minimum 8 characters long"
       },
       email: {
-        required: 'Please enter email',
-        email: 'Invalid email address'
+        required: "Please enter email",
+        email: "Invalid email address"
       },
       password2: {
-        required: 'Please enter password',
-        minlength: 'Password should be minimum 8 characters long',
+        required: "Please enter password",
+        minlength: "Password should be minimum 8 characters long",
         equalTo: "Passwords doesn't match"
       }
     },
@@ -35,158 +35,203 @@ $(document).ready(function() {
 });
 
 $(document).ready(function () {
-  var signUpForm = document.querySelector('#create-form');
+  // The two forms have same ID
+  var forms = document.querySelectorAll("#create-form");
+
+  // Sign up modal form
+  forms[0].classList.add("signup-modal-form");
+  var signUpModalForm = new SignUpFormValidator(".signup-modal-form");
+
+  // publiclab.org/register form
+  if (forms[1]) {
+    forms[1].classList.add("signup-register-form");
+    var signUpRegisterForm = new SignUpFormValidator(".signup-register-form");
+  }
+});
+
+// Form validation class
+function SignUpFormValidator(formClass) {
+  var signUpForm = document.querySelector(formClass);
 
   if (!signUpForm) return;
 
-  var validationTracker = {};
-  var submitBtn = document.querySelector("#create-form [type='submit']");
-  var usernameElement = document.querySelector("[name='user[username]']");
-  var emailElement = document.querySelector("[name='user[email]']");
-  var passwordElement = document.querySelector("[name='user[password]']");
+  this.validationTracker = {};
+  this.submitBtn = document.querySelector(formClass + ' [type="submit"');
+
+  this.isFormValid();
+
+  var usernameElement = document.querySelector(
+    formClass + " [name='user[username]']"
+  );
+  var emailElement = document.querySelector(
+    formClass + " [name='user[email]']"
+  );
+  var passwordElement = document.querySelector(
+    formClass + " [name='user[password]']"
+  );
   var confirmPasswordElement = document.querySelector(
-    "[name='user[password_confirmation]']"
+    formClass + " [name='user[password_confirmation]']"
   );
 
-  isFormValid();
+  // Every time user types something, corresponding event listener are triggered
+  usernameElement.addEventListener(
+    "input",
+    validateUsername.bind(usernameElement, this)
+  );
+  emailElement.addEventListener(
+    "input",
+    validateEmail.bind(emailElement, this)
+  );
+  passwordElement.addEventListener(
+    "input",
+    validatePassword.bind(passwordElement, confirmPasswordElement, this)
+  );
+  confirmPasswordElement.addEventListener(
+    "input",
+    validateConfirmPassword.bind(confirmPasswordElement, passwordElement, this)
+  );
+}
 
-  // Every time user types something, it triggers corresponding event listener
-  usernameElement.addEventListener('input', validateUsername);
-  emailElement.addEventListener('input', validateEmail);
-  passwordElement.addEventListener('input', validatePassword);
-  confirmPasswordElement.addEventListener('input', validateConfirmPassword);
+// Typing the form triggers the function
+// Updates UI depending on the value of <valid> parameter
+SignUpFormValidator.prototype.updateUI = function(element, valid, errorMsg) {
+  var elementName = element.getAttribute("name");
 
-  function validateUsername(e) {
-    var username = e.target.value;
-
-    if (username.length < 3) {
-      restoreOriginalStyle(this);
-    } else {
-      $.get('/api/srch/profiles?query=' + username, function(data) {
-        if (data.items) {
-          $.map(data.items, function(userData) {
-            if (userData.doc_title === username) {
-              updateUI(usernameElement, false, 'Username already exists');
-            } else {
-              updateUI(usernameElement, true);
-            }
-          });
-        } else {
-          updateUI(usernameElement, true);
-        }
-      });
-    }
+  if (valid) {
+    this.validationTracker[elementName] = true;
+    styleElement(element, "form-element-invalid", "form-element-valid");
+    removeErrorMsg(element);
+  } else {
+    this.validationTracker[elementName] = false;
+    styleElement(element, "form-element-valid", "form-element-invalid");
+    renderErrorMsg(element, errorMsg);
   }
 
-  function validateEmail(e) {
-    var email = e.target.value;
-    var regexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    var isValid = regexp.test(email);
+  this.isFormValid();
+};
 
-    updateUI(emailElement, isValid, 'Invalid email');
+SignUpFormValidator.prototype.disableSubmitBtn = function() {
+  this.submitBtn.setAttribute("disabled", "");
+};
+
+SignUpFormValidator.prototype.enableSubmitBtn = function() {
+  this.submitBtn.removeAttribute("disabled");
+};
+
+SignUpFormValidator.prototype.isFormValid = function() {
+  // Form is valid if all elements have passsed validation successfully
+  var isValidForm =
+    Object.values(this.validationTracker).filter(Boolean).length === 4;
+
+  if (isValidForm) this.enableSubmitBtn();
+  else this.disableSubmitBtn();
+};
+
+function validateUsername(obj) {
+  var username = this.value;
+  var self = this;
+
+  if (username.length < 3) {
+    restoreOriginalStyle(this);
+  } else {
+    $.get("/api/srch/profiles?query=" + username, function(data) {
+      if (data.items) {
+        $.map(data.items, function(userData) {
+          if (userData.doc_title === username) {
+            obj.updateUI(self, false, "Username already exists");
+          } else {
+            obj.updateUI(self, true);
+          }
+        });
+      } else {
+        obj.updateUI(self, true);
+      }
+    });
+  }
+}
+
+function validateEmail(obj) {
+  var email = this.value;
+  var emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var isValidEmail = emailRegExp.test(email);
+
+  obj.updateUI(this, isValidEmail, "Invalid email");
+}
+
+function validatePassword(confirmPasswordElement, obj) {
+  var password = this.value;
+
+  if (!isPasswordValid(password)) {
+    obj.updateUI(
+      this,
+      false,
+      "Please make sure password is atleast 8 characters long with minimum one numeric value"
+    );
+    return;
   }
 
-  function validatePassword(e) {
-    var password = e.target.value;
-
-    if (!isPasswordValid(password)) {
-      updateUI(this, false, 'Minimum 8 characters including 1 number');
-      return;
-    }
-
-    if (password === confirmPasswordElement.value) {
-      updateUI(confirmPasswordElement, true);
-    }
-
-    updateUI(passwordElement, true);
+  if (password === confirmPasswordElement.value) {
+    obj.updateUI(confirmPasswordElement, true);
   }
 
-  function validateConfirmPassword(e) {
-    var password = passwordElement.value;
-    var confirmPassword = e.target.value;
+  obj.updateUI(this, true);
+}
 
-    if (!isPasswordValid(confirmPassword)) {
-      updateUI(this, false, 'Minimum 8 characters including 1 number');
-      return;
-    }
+function validateConfirmPassword(passwordElement, obj) {
+  var confirmPassword = this.value;
+  var password = passwordElement.value;
 
-    if (confirmPassword !== password) {
-      updateUI(confirmPasswordElement, false, 'Passwords must be equal');
-      return;
-    }
-
-    updateUI(confirmPasswordElement, true);
+  if (confirmPassword !== password) {
+    obj.updateUI(this, false, "Passwords must be equal");
+    return;
   }
 
-  // Password is valid if it is at least 8 characaters long and contains a number
-  // This is password's validation logic, no UI
-  function isPasswordValid(element, password) {
-    var doesContainNum = /\d+/g.test(password);
-    var isValid = password.length >= 8 && doesContainNum;
+  obj.updateUI(this, true);
+}
 
-    return isValid;
+// Password is valid if it is at least 8 characaters long and contains a number
+// Password's validation logic, no UI updates
+function isPasswordValid(password) {
+  var doesContainNumber = /\d+/g.test(password);
+  var isValidPassword = password.length >= 8 && doesContainNumber;
+
+  return isValidPassword;
+}
+
+function renderErrorMsg(element, message) {
+  if (!message) return;
+
+  // Error messages are rendered inside of a <small> HTML element
+  var errorMsgElement = element.nextElementSibling;
+  if (!errorMsgElement) {
+    // On publiclab.org/register invalid elements are wrapped in a div.
+    errorMsgElement = element.parentElement.nextElementSibling;
   }
 
-  // Every time user types something in the form, it triggers this function
-  // It will update UI depending on the value of <valid> parameter
-  function updateUI(element, valid, errorMsg) {
-    var elementName = element.getAttribute('name');
+  errorMsgElement.textContent = message;
+  errorMsgElement.style.color = "red";
+  errorMsgElement.classList.remove("invisible");
+}
 
-    if (valid) {
-      validationTracker[elementName] = true;
-      styleElement(element, 'form-element-invalid', 'form-element-valid');
-      removeErrorMsg(element);
-    } else {
-      validationTracker[elementName] = false;
-      styleElement(element, 'form-element-valid', 'form-element-invalid');
-      renderErrorMsg(element, errorMsg);
-    }
-
-    isFormValid();
+function removeErrorMsg(element) {
+  var errorMsgElement = element.nextElementSibling;
+  if (!errorMsgElement) {
+    errorMsgElement = element.parentElement.nextElementSibling;
   }
 
-  function renderErrorMsg(element, message) {
-    if (!message) return;
+  errorMsgElement.classList.add("invisible");
+}
 
-    // Error messages are rendered inside of a <small> HTML element
-    var errorMsgElement = element.nextElementSibling;
-    errorMsgElement.textContent = message;
-    errorMsgElement.style.color = 'red';
-    errorMsgElement.classList.remove('invisible');
+function restoreOriginalStyle(element) {
+  element.classList.remove("form-element-valid");
+  element.classList.remove("form-element-invalid");
+}
+
+// Makes input element red or green
+function styleElement(element, classToRemove, classToAdd) {
+  if (element.classList.contains(classToRemove)) {
+    element.classList.remove(classToRemove);
   }
 
-  function removeErrorMsg(element) {
-    var errorMsgElement = element.nextElementSibling;
-    errorMsgElement.classList.add('invisible');
-  }
-
-  function restoreOriginalStyle(element) {
-    element.classList.remove('form-element-valid');
-    element.classList.remove('form-element-invalid');
-  }
-
-  // Makes input element red/green
-  function styleElement(element, classToRemove, classToAdd) {
-    if (element.classList.contains(classToRemove)) {
-      element.classList.remove(classToRemove);
-    }
-
-    element.classList.add(classToAdd);
-  }
-
-  function disableSubmitBtn() {
-    submitBtn.setAttribute('disabled', '');
-  }
-
-  function enableSubmitBtn() {
-    submitBtn.removeAttribute('disabled');
-  }
-
-  function isFormValid() {
-    // Form is valid if all elements have passsed validation successffully
-    var isValid = Object.values(validationTracker).filter(Boolean).length === 4;
-
-    if (isValid) enableSubmitBtn();
-    else disableSubmitBtn();
-  }
-});
+  element.classList.add(classToAdd);
+}
