e87963f0777375d778d49dc6dadfe3d46df6a0bb
Uzay-G <52892257+Uzay-G@users.noreply.github.com>
2020-01-03 18:54:59 +0100

    fix error

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -66,14 +66,7 @@ class UserSessionsController < ApplicationController
           session[:openid_return_to] = nil
           redirect_to return_to + hash_params
         else
-          if session[:openid_return_to] # for openid login, redirects back to openid auth process
-            return_to = session[:openid_return_to]
-            session[:openid_return_to] = nil
-            redirect_to return_to + hash_params
-            redirect_to return_to + hash_params
-          else
-            redirect_to return_to + hash_params, notice: I18n.t('user_sessions_controller.logged_in')
-          end
+          redirect_to return_to + hash_params, notice: I18n.t('user_sessions_controller.logged_in')
         end
       else # identity does not exist so we need to either create a user with identity OR link identity to existing user
         if User.where(email: auth["info"]["email"]).empty?
