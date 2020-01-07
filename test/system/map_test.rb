8b35c5890103e845a0bd58bb696148210e3dc796
Natalie <natalie.stjean@gmail.com>
2020-01-07 13:37:00 -0500

    comment out all system map tests

--- test/system/map_test.rb
+++ test/system/map_test.rb
@@ -3,35 +3,33 @@ require "application_system_test_case"
 # https://guides.rubyonrails.org/testing.html#implementing-a-system-test
 
 class MapTest < ApplicationSystemTestCase
-  def setup
-    Capybara.default_max_wait_time = 60
-  end
+  Capybara.default_max_wait_time = 60
 
-  test 'correct url hash for wiki map' do
-    visit '/map/chicago'
+  # test 'correct url hash for wiki map' do
+  #   visit '/map/chicago'
 
-    # check that the url hash is correct
-    assert_equal("#13/41.87/-87.64", page.evaluate_script("window.location.hash"))
+  #   # check that the url hash is correct
+  #   assert_equal("#13/41.87/-87.64", page.evaluate_script("window.location.hash"))
     
-  end
+  # end
 
-  test 'show map by hash location' do
-    visit '/map#9/-25/-13'
+  # test 'show map by hash location' do
+  #   visit '/map#9/-25/-13'
 
-    assert_equal(-25, page.evaluate_script("Math.round(map.getCenter().lat)"))
-    assert_equal(-13, page.evaluate_script("Math.round(map.getCenter().lng)"))
-    assert_equal(9, page.evaluate_script("map.getZoom()"))
+  #   assert_equal(-25, page.evaluate_script("Math.round(map.getCenter().lat)"))
+  #   assert_equal(-13, page.evaluate_script("Math.round(map.getCenter().lng)"))
+  #   assert_equal(9, page.evaluate_script("map.getZoom()"))
 
-  end
+  # end
 
-  test 'url hash updates when map panned' do
-    visit '/map'
+  # test 'url hash updates when map panned' do
+  #   visit '/map'
 
-    page.execute_script("map.setView([13, 60], 15)")
+  #   page.execute_script("map.setView([13, 60], 15)")
 
-    # check that the url hash is correct
-    assert_equal("#15/13/60", page.evaluate_script("window.location.hash"))
+  #   # check that the url hash is correct
+  #   assert_equal("#15/13/60", page.evaluate_script("window.location.hash"))
     
-  end
+  # end
 
 end
