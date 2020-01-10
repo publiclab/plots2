dba7c4d789ac54fb569b9a9746f16b367e3d7ac9
Vladimir <vladimir.mikulic2@gmail.com>
2020-01-10 19:22:52 +0100

    REFACTOR: return_to system test
    return_to query parameter system test moved to sessions test.

--- test/system/screenshots_test.rb
+++ test/system/screenshots_test.rb
@@ -71,14 +71,6 @@ class ScreenshotsTest < ApplicationSystemTestCase
     take_screenshot
   end
 
-  test 'return_to query parameter preserved while switching from /login to /signup' do
-    visit '/login?return_to=page'
-    click_on 'sign up'
-
-    path = URI.parse(current_url).request_uri
-    assert_equal path, '/signup?return_to=page'
-  end
-
   test 'tags' do
     visit '/tags'
     take_screenshot
