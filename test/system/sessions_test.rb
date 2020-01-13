4dd1b92f099846acb53b6508a86dc4a13340f7df
Vladimir <vladimir.mikulic2@gmail.com>
2020-01-13 18:26:17 +0100

    ADD: Test for comments section page redirection
    After the user logs in, it should redirect him to the note he was trying
    to comment previously.

--- test/system/sessions_test.rb
+++ test/system/sessions_test.rb
@@ -10,4 +10,18 @@ class SessionsTest < ApplicationSystemTestCase
     assert_equal path, '/signup?return_to=page'
   end
 
+  test 'the user after login is redirected to the note he was trying to comment' do
+    visit '/wiki/wiki-page-path/comments'
+
+    find('a[data-target="#loginModal"]', text: 'Login').click()
+
+    fill_in 'user_session[username]', with: 'jeff'
+    fill_in 'user_session[password]', with: 'secretive'
+
+    click_on 'Log in'
+
+    path = URI.parse(current_url).request_uri
+    assert_equal path, '/wiki/wiki-page-path/comments'
+  end
+
 end
