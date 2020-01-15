c2bff983f69b46f057c2b72e314b15efbb772304
Uzay-G <52892257+Uzay-G@users.noreply.github.com>
2020-01-15 17:40:30 +0100

    FIX: Oauth redirection after signup (#7244)
    * fix signup redirection
    
    * added tests
    
    * added omniauth user
    
    * add fixture users for all providers
    
    * added tests for other providers
    
    * Update user_sessions_controller_test.rb
    
    * Update user_sessions_controller_test.rb
    
    * fix mixup

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -90,11 +90,8 @@ class UserSessionsController < ApplicationController
             flash[:notice] = "You are now following '#{params[:return_to].split('/')[4]}'."
             subscribe_multiple_tag(params[:return_to].split('/')[4])
             redirect_to '/dashboard', notice: "You have successfully signed in. Please change your password using the link sent to you via e-mail."
-          elsif params[:return_to] && params[:return_to] != "/signup" && params[:return_to] != "/login"
-            flash[:notice] += " " + I18n.t('users_controller.continue_where_you_left_off', url1: params[:return_to].to_s)
-            redirect_to return_to + hash_params, notice: "You have successfully signed in. Please change your password using the link sent to you via e-mail."
           else
-            redirect_to return_to + hash_params, notice: "You have successfully signed in. Please change your password using the link sent to you via e-mail."
+            redirect_to "/dashboard", notice: "You have successfully signed in. Please change your password using the link sent to you via e-mail."
           end
         else # email exists so link the identity with existing user and log in the user
           user = User.where(email: auth["info"]["email"])
