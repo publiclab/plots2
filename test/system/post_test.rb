f70b0ad50837c9851ea9c45511c84a1f74f8e21c
Uzay-G <52892257+Uzay-G@users.noreply.github.com>
2020-01-18 23:02:33 +0100

    fix travis for real this time :+1:

--- test/system/post_test.rb
+++ test/system/post_test.rb
@@ -127,26 +127,22 @@ class PostTest < ApplicationSystemTestCase
     # save text of wiki before edit
     old_wiki_content = find("#content").text
 
-    # edit content
-    find("a[data-original-title='Try the beta inline Rich Wiki editor.']").click()
-    first("div.inline-section").hover()
-    using_wait_time(2) { first("a.inline-edit-btn").click() }
-    find("div.wk-wysiwyg").set("wiki text")
-    click_on "Save"
+    find("a#edit-btn").click()
+    find("#text-input").set("wiki text")
+    find("a#publish").click()
 
     # view wiki
-    find("a[href='#{wiki.path}'").click()
     current_wiki_content = find("#content").text
     # make sure edits worked and text is different
     assert current_wiki_content != old_wiki_content
 
-    find("a[data-original-title='View previous versions of this page.']").click()
+    find("a[data-original-title='View all revisions for this page.']").click()
     accept_confirm "Are you sure?" do
       # revert to previous version of wiki
       all("a", text: "Revert")[1].click()
     end
     visit wiki.path
-    
+
     # check old wiki content is the same as current content after revert
     assert old_wiki_content == find("#content").text 
   end
@@ -156,16 +152,11 @@ class PostTest < ApplicationSystemTestCase
 
     visit wiki.path
 
-    # edit content
-    find("a[data-original-title='Try the beta inline Rich Wiki editor.']").click()
-    first("div.inline-section").hover()
-    using_wait_time(2) { first("a.inline-edit-btn").click() }
-    find("div.wk-wysiwyg").native.send_keys(:enter, "wiki text")
-    click_on "Save"
+    find("a#edit-btn").click()
+    find("#text-input").native.send_keys(:enter, :enter, "wiki text")
+    find("a#publish").click()
 
-    find("a[href='#{wiki.path}'").click()
-    find("a[data-original-title='View previous versions of this page.']").click()
-
+    find("a[data-original-title='View all revisions for this page.']").click()
 
     # verify additions are displayed as green `<ins>` tags
     page.assert_selector("ins", text: "<p>wiki")
