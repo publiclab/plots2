4d7086b80b0cd8c2c723fec453f05bb4c05383f0
Vladimir Mikulic <35997844+VladimirMikulic@users.noreply.github.com>
2020-01-22 10:02:24 -0500

    ADD: Missing system tests (#7322)
    Tests added:
     - "adding a location to the wiki"
     - "notification settings are working correctly"
     - "subscribing to a tag"
     - "unsubscribing from a tag"
    
    Part of #5316

--- test/system/settings_test.rb
+++ test/system/settings_test.rb
@@ -3,19 +3,50 @@ require "application_system_test_case"
 class SettingsTest < ApplicationSystemTestCase
   Capybara.default_max_wait_time = 60
 
-  test 'viewing the settings page' do
+  def setup
     visit '/'
 
-    click_on 'Login'
-
-    fill_in("username-login", with: "namangupta")
+    find(".nav-link.loginToggle").click()
+    fill_in("username-login", with: "jeff")
     fill_in("password-signup", with: "secretive")
-    click_on "Log in"
 
+    find(".login-modal-form #login-button").click()
+  end
+
+  test 'viewing the settings page' do
     visit '/settings'
 
     take_screenshot
   end
 
-end
+  test 'notification settings are working correctly' do
+    visit '/settings'
 
+    # Enable "Notifcation when you are mentioned"
+    find('input[name="notifications:mentioned"] ~ span').click()
+    # Enable "Browser notification for all"
+    find('input[name="notifications:all"] ~ span').click()
+
+    find('button#save', text: 'Save').click()
+
+    # Wait for settings to update (POST request)
+    wait_for_ajax
+
+    # Make sure that success message appears
+    assert_selector(".alert.alert-success", text: "×\nSettings updated successfully!")
+
+    # Checkboxes are hidden and this rule needs to be disabled in order to fetch them
+    Capybara.ignore_hidden_elements = false
+    visit '/settings'
+
+    notifications_mentioned_checkbox = find('input[name="notifications:mentioned"]')
+    notifications_all_checkbox = find('input[name="notifications:all"]')
+
+    # Make sure the updated settings are permanent
+    assert_equal( notifications_mentioned_checkbox.checked?, true )
+    assert_equal( notifications_all_checkbox.checked?, true )
+
+    Capybara.ignore_hidden_elements = true
+  end
+
+end
