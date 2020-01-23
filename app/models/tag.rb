475531a676d026034ac7c3dd3553c71f50da3016
imajit <ajitmujumdar25999@gmail.com>
2020-01-24 03:42:43 +0530

    changed .each to .select

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -409,11 +409,11 @@ class Tag < ApplicationRecord
 
   def self.all_tags_by_popularity
     not_power_tags = []
-    Tag.all.order('count DESC').each do |tag|
+    Tag.all.order('count DESC').select{|tag|
       if tag.name.match(':').nil?
         not_power_tags.push(tag.name)
       end
-    end
+    }
     not_power_tags.uniq
   end
 
