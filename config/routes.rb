eb3d34008514b41c913e46d6d7028e49f8b9eb0f
Natalie <natalie.stjean@gmail.com>
2020-02-27 14:03:03 -0500

    add choose page with params

--- config/routes.rb
+++ config/routes.rb
@@ -310,6 +310,7 @@ Plots2::Application.routes.draw do
 
   get 'post' => 'editor#post', :as => :editor_post
   post 'post' => 'editor#post', :as => :editor_path
+  get 'post/choose' => 'editor#choose'
   get 'legacy' => 'editor#legacy'
   get 'editor' => 'editor#editor'
   get 'editor/rich/(:n)' => 'editor#rich'
