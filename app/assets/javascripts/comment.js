5f3ba9d48724a45ba11d8521c75e5a5dd0040d7f
Suyash Choudhary <57896905+sssash18@users.noreply.github.com>
2020-03-12 23:25:57 +0530

    Added function

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -64,3 +64,8 @@ function addComment(comment, submitTo, parentID = 0) {
   }
   sendFormSubmissionAjax(data, submitTo);
 }
+
+function changeicon(messageid, buttonid){
+    $(messageid).toggle();
+    $(buttonid).toggleClass("fa-bell-o fa-bell");
+}
