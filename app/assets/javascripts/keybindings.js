ee733384986afad7f135ee811b6390b61d17dede
Jeffrey Warren <jeff@unterbahn.com>
2020-05-05 16:43:34 -0400

    Fix search keybinding "/" (#7872)

--- app/assets/javascripts/keybindings.js
+++ app/assets/javascripts/keybindings.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-  $('input[type="text"], textarea, input[type="password"], .wk-wysiwyg, .wk-prompt').css('cursor','text');
-  $(window).on('keypress', function(e) {
-    if (e.target.style.cursor !== 'text') {
-      if (e.which === 47) {
-        $("#searchform_input").focus();
-        e.preventDefault();
+  $('input[type="text"], textarea, input[type="password"], .wk-wysiwyg, .wk-prompt')
+    .css('cursor','text')
+    .on('keypress', function(e) {
+      if (e.target.style.cursor !== 'text') {
+        if (e.which === 47) {
+          $("#searchform_input").focus();
+          e.preventDefault();
+        }
       }
-    }
-  });
+    });
 });
