b0d30e910a461e6c2ad4cbb5f6b05f6b90a6c4f2
Jeffrey Warren <jeff@unterbahn.com>
2020-05-26 17:34:35 -0400

    fixes for subscription stats caching and counting (#7955)

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,20 +1,15 @@
 class StatsController < ApplicationController
   def subscriptions
-      @tags = TagSelection.where(following: true)
-        .joins("LEFT JOIN community_tags ON community_tags.tid = tag_selections.tid")
-        .joins(:tag)
-        .group("term_data.name")
-        .count
-#     Rails.cache.fetch("stats/subscriptions/query", expires_in: 24.hours) do
-#       @tags = TagSelection.where(following: true)
-#         .joins("LEFT JOIN community_tags ON community_tags.tid = tag_selections.tid")
-#         .joins(:tag)
-#         .joins("INNER JOIN node ON node.nid = community_tags.nid")
-#         .where("node.status = 1")
-#         .group("term_data.name")
-#         .count
-      @tags = @tags.group_by { |_k, v| v / 10 }.sort_by { |k, _v| -k }
-#    end
+     @tags = Rails.cache.fetch("stats-subscriptions-query-1", expires_in: 24.hours) do
+       TagSelection.where(following: true)
+         .joins("LEFT JOIN community_tags ON community_tags.tid = tag_selections.tid")
+         .joins(:tag)
+         .joins("INNER JOIN node ON node.nid = community_tags.nid")
+         .where("node.status = 1")
+         .group("term_data.name")
+         .count
+    end
+    @tags = @tags.group_by { |_k, v| v / 10 }.sort_by { |k, _v| -k }
   end
 
   def range
