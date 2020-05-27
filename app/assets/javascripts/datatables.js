d2d86a0e8c7b1828aa8e5c66e74f5862de654eb1
keshav sethi <keshav.sethi0004@gmail.com>
2020-05-28 01:26:41 +0530

    responsive

--- app/assets/javascripts/datatables.js
+++ app/assets/javascripts/datatables.js
@@ -2,7 +2,3 @@
 //= require datatables/jquery.dataTables
 //Bootstrap4 theme
 //= require datatables/dataTables.bootstrap4
-
-//Optional Datatables extensions
-//= require datatables/extensions/Responsive/dataTables.responsive
-//= require datatables/extensions/Responsive/responsive.bootstrap4
