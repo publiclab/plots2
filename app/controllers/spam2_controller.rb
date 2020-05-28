a53492eceebbcdbdd781690f9ef5324f4af046c7
keshav sethi <keshav.sethi0004@gmail.com>
2020-05-29 03:22:23 +0530

    new spam2 ui

--- app/controllers/spam2_controller.rb
+++ app/controllers/spam2_controller.rb
@@ -1,10 +1,9 @@
 class Spam2Controller < ApplicationController
   before_action :require_user, only: %i(spam spam_revisions mark_comment_spam publish_comment spam_comments)
 
-  def spam
-    if logged_in_as(['admin', 'moderator'])
+  def _spam
+    if %w(admin moderator)
       @nodes = Node.paginate(page: params[:page])
-                   .order('nid DESC')
       @nodes = if params[:type] == 'wiki'
                 @nodes.where(type: 'page', status: 1)
                else
@@ -16,44 +15,42 @@ class Spam2Controller < ApplicationController
     end
   end
 
-  def spam_revisions
-    if logged_in_as(['admin', 'moderator'])
+  def _spam_revisions
+    if %w(admin moderator)
       @revisions = Revision.paginate(page: params[:page])
-                           .order('timestamp DESC')
                            .where(status: 0)
-      render template: 'spam2/spam'
+      render template: 'spam2/_spam'
     else
       flash[:error] = 'Only moderators can moderate revisions.'
       redirect_to '/dashboard'
     end
   end
 
-  def spam_comments
-    if current_user &. can_moderate?
+  def _spam_comments
+    if %w(admin moderator)
       @comments = Comment.paginate(page: params[:page])
-                       .order('timestamp DESC')
                        .where(status: 0)
-      render template: 'spam2/spam'
+      render template: 'spam2/_spam'
     else
       flash[:error] = 'Only moderators can moderate comments.'
       redirect_to '/dashboard'
     end
   end
 
-  def batch
-    if logged_in_as(['admin', 'moderator'])
-      nodes = 0
+  def batch_spam
+    if %w(admin moderator)
       users = []
+      nodes = 0
       params[:ids].split(',').uniq.each do |nid|
         node = Node.find nid
         node.spam
-        nodes += 1
         user = node.author
         user.ban
         users << user.id
+        nodes += 1
       end
       flash[:notice] = nodes.to_s + ' nodes spammed and ' + users.length.to_s + ' users banned.'
-      redirect_to '/spam2/wiki'
+      redirect_to '/spam2'
     else
       flash[:error] = 'Only admins can batch moderate.'
       redirect_to '/dashboard'
@@ -61,7 +58,7 @@ class Spam2Controller < ApplicationController
   end
 
   def batch_publish
-    if logged_in_as(['admin', 'moderator'])
+    if %w(admin moderator)
       users = []
       nodes = 0
       params[:ids].split(',').uniq.each do |nid|
@@ -73,7 +70,7 @@ class Spam2Controller < ApplicationController
         nodes += 1
       end
       flash[:notice] = nodes.to_s + ' nodes published and ' + users.length.to_s + ' users unbanned.'
-      redirect_to '/spam2'
+      redirect_to '/spam2/wiki'
     else
       flash[:error] = 'Only admins can batch moderate.'
       redirect_to '/dashboard'
@@ -81,7 +78,7 @@ class Spam2Controller < ApplicationController
   end
 
   def batch_delete
-    if logged_in_as(['admin', 'moderator'])
+    if %w(admin moderator)
       nodes = 0
       params[:ids].split(',').uniq.each do |nid|
         node = Node.find nid
@@ -97,17 +94,18 @@ class Spam2Controller < ApplicationController
   end
 
   def batch_ban
-    if logged_in_as(['admin', 'moderator'])
-      users = []
+    if %w(admin moderator)
       nodes = 0
+      users = []
       params[:ids].split(',').uniq.each do |nid|
         node = Node.find nid
         user = node.author
         user.ban
+        flash[:notice] = users
         users << user.id
         nodes += 1
       end
-      flash[:notice] = users.length.to_s + ' users banned.'
+      flash[:notice] = users.length.to_s + ' users unbanned.'
       redirect_to '/spam2'
     else
       flash[:error] = 'Only admins can batch moderate.'
