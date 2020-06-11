7b3b27e8a504e9579a74d67837342e7ee2c31902
keshav sethi <keshav.sethi0004@gmail.com>
2020-06-12 05:16:10 +0530

    stable issue

---
+++ test/system/spam2_test.rb
@@ -0,0 +1,24 @@
+require "application_system_test_case"
+
+class SpamTest < ApplicationSystemTestCase
+    def setup
+        visit "/spam2"
+        click_on "Login"
+        fill_in 'user_session[username]', with: 'palpatine'
+        fill_in 'user_session[password]', with: 'secretive'
+        click_on "Log in"
+      end
+    
+    test "Delete post in spam2" do
+      spam_page = nodes(:one)
+      visit spam_page.path
+      first("span[data-original-title='Tools']").click()
+      click_on "Spam"
+      visit "/spam2"
+      accept_confirm 'Are you sure you want to delete "'+spam_page.path+'"?' do
+      find("a[href='/notes/delete/#{spam_page.id}'").click()
+      end
+      assert_selector('div.alert', text: 'Node Deleted')
+      end
+end
+	
