19db55ea358e08197c063cc10d097097c10e9a3d
Kaïs Rouini <46888094+Jolios@users.noreply.github.com>
2020-07-31 18:06:13 +0200

    Replaced .count and .length in comment model with .size

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -30,14 +30,14 @@ class Comment < ApplicationRecord
     (0..span).each do |week|
       weeks[span - week] = Comment.select(:timestamp)
         .where(timestamp: time.to_i - week.weeks.to_i..time.to_i - (week - 1).weeks.to_i)
-        .count
+        .size
     end
     weeks
   end
 
   def self.contribution_graph_making(start = Time.now - 1.year, fin = Time.now)
     date_hash = {}
-    week = start.to_date.step(fin.to_date, 7).count
+    week = start.to_date.step(fin.to_date, 7).size
 
     while week >= 1
       month = (fin - (week * 7 - 1).days)
@@ -206,7 +206,7 @@ class Comment < ApplicationRecord
   end
 
   def liked_by(user_id)
-    likes.where(user_id: user_id).count > 0
+    likes.where(user_id: user_id).size > 0
   end
 
   def likers
@@ -214,7 +214,7 @@ class Comment < ApplicationRecord
   end
 
   def emoji_likes
-    likes.group(:emoji_type).count
+    likes.group(:emoji_type).size
   end
 
   def user_reactions_map
@@ -227,7 +227,7 @@ class Comment < ApplicationRecord
       end
 
       emoji_type = reaction.underscore.humanize.downcase
-      users_string = (users.length > 1 ? users[0..-2].join(", ") + " and " + users[-1] : users[0]) + " reacted with " + emoji_type + " emoji"
+      users_string = (users.size > 1 ? users[0..-2].join(", ") + " and " + users[-1] : users[0]) + " reacted with " + emoji_type + " emoji"
       user_like_map[reaction] = users_string
     end
     user_like_map
@@ -442,7 +442,7 @@ class Comment < ApplicationRecord
       next unless url.include? "https://"
 
       if url.last == "."
-        url = url[0...url.length - 1]
+        url = url[0...url.size - 1]
       end
       response = Net::HTTP.get_response(URI(url))
       redirected_url = response['location']
