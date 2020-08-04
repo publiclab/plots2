580331c15bc1dee18c131a4369f1aa92cb4eb8fb
Jeffrey Warren <jeff@unterbahn.com>
2020-08-04 17:21:57 -0400

    topic card count fixes for #7965 (#8249)
    * fixes for #7965
    
    * count fixes

--- app/models/node_tag.rb
+++ app/models/node_tag.rb
@@ -8,13 +8,11 @@ class NodeTag < ApplicationRecord
   has_many :tag_selections, foreign_key: 'tid'
   accepts_nested_attributes_for :tag
 
-  after_create :increment_count
+  after_create :update_count
+  after_destroy :update_count
 
-  def increment_count
-    tag = self.tag
-    tag.count = 0 if tag.count.nil?
-    tag.count += 1
-    tag.save
+  def update_count
+    tag.run_count # update count of tag usage
   end
 
   def user
