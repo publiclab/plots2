8c3d404bf77ec68036d1a4e9f8826b9c59e06e45
Jeffrey Warren <jeff@unterbahn.com>
2020-08-05 13:17:34 -0400

    Don't let newcomers edit wiki pages (controller-level) (#8250)
    * Don't let newcomers edit wiki pages (controller-level)
    
    * Update wiki_controller.rb
    
    * Update wiki_controller_test.rb
    
    * Update wiki_controller_test.rb
    
    * Update wiki_controller_test.rb
    
    * Update wiki_controller_test.rb
    
    * Update wiki_controller.rb
    
    * Update wiki_controller.rb
    
    * Update wiki_controller_test.rb
    
    * Update wiki_controller_test.rb

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -102,6 +102,10 @@ class WikiController < ApplicationController
     if @node.has_tag('locked') && !current_user.can_moderate?
       flash[:warning] = "This page is <a href='/wiki/power-tags#Locking'>locked</a>, and only <a href='/wiki/moderators'>moderators</a> can edit it."
       redirect_to @node.path
+    elsif current_user &.first_time_poster
+      flash[:notice] = "Please post a question or other content before editing the wiki. Click <a href='https://publiclab.org/notes/tester/04-23-2016/new-moderation-system-for-first-time-posters'>here</a> to learn why."
+      redirect_to Node.find_wiki(params[:id]).path
+      return
     end
     if ((Time.now.to_i - @node.latest.timestamp) < 5.minutes.to_i) && @node.latest.author.uid != current_user.uid
       flash.now[:warning] = I18n.t('wiki_controller.someone_clicked_edit_5_minutes_ago')
