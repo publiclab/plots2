8c67b88638e67703a3482ef969cf9284c522b6ab
Sneha Mishra <33183263+Tlazypanda@users.noreply.github.com>
2020-08-12 02:26:31 +0530

    eliminate n+1 query of comment parent method (#8267)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -81,6 +81,7 @@ class HomeController < ApplicationController
     notes = notes.where('nid != (?)', blog.nid) if blog
 
     comments = Comment.joins(:node, :user)
+                   .includes(:node)
                    .order('timestamp DESC')
                    .where('timestamp - node.created > ?', 86_400) # don't report edits within 1 day of page creation
                    .where('node.status = ?', 1)
