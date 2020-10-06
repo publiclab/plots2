61b2ae051ed8604c94b840796e8e58807456d9ed
shreyaa-sharmaa <shreyasharma.ss305@gmail.com>
2020-10-07 03:02:27 +0530

    Add duplicate username test

--- test/system/signup_form_test.rb
+++ test/system/signup_form_test.rb
@@ -5,16 +5,24 @@ class SignupFormTest < ApplicationSystemTestCase
       visit "/"
     end
   
-    test "the signup form is validated and rejects a duplicate email" do 
+    test "the signup form is validated and rejects duplicate email and username" do 
         visit "/signup" 
        
         assert_difference 'User.count', 0 do
-            # Signs up with an already-registered email 
+            #Signs up with an already-registered email 
             fill_in("username-signup", with: "abc") 
             fill_in("email", with: "jeff@pxlshp.com") 
             fill_in("password1", with: "secretive") 
             fill_in("password-confirmation", with: "secretive") 
-        
+            find("#create-form #signup-button").click() 
+        end
+
+        assert_difference 'User.count', 0 do
+            #Signs up with an already-registered username 
+            fill_in("username-signup", with: "jeff") 
+            fill_in("email", with: "random@publiclab.com") 
+            fill_in("password1", with: "secretive") 
+            fill_in("password-confirmation", with: "secretive")    
             find("#create-form #signup-button").click() 
         end
     end 
