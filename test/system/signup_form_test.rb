143aa822432d9689cb65491abe8c80d95db1dc41
Shreya Sharma <66686803+shreyaa-sharmaa@users.noreply.github.com>
2020-10-07 04:40:55 +0530

    Sign up form validation test (#8468)
    * Sign up form validation test
    
    * Update test/system/signup_form_test.rb
    
    * Update test/system/signup_form_test.rb
    
    * Add duplicate username test
    
    Co-authored-by: Jeffrey Warren <jeff@unterbahn.com>

--- test/system/signup_form_test.rb
+++ test/system/signup_form_test.rb
@@ -1,4 +1,29 @@
 require "application_system_test_case"
 
 class SignupFormTest < ApplicationSystemTestCase
+    def setup
+      visit "/"
+    end
+  
+    test "the signup form is validated and rejects duplicate email and username" do 
+        visit "/signup" 
+       
+        assert_difference 'User.count', 0 do
+            #Signs up with an already-registered email 
+            fill_in("username-signup", with: "abc") 
+            fill_in("email", with: "jeff@pxlshp.com") 
+            fill_in("password1", with: "secretive") 
+            fill_in("password-confirmation", with: "secretive") 
+            find("#create-form #signup-button").click() 
+        end
+
+        assert_difference 'User.count', 0 do
+            #Signs up with an already-registered username 
+            fill_in("username-signup", with: "jeff") 
+            fill_in("email", with: "random@publiclab.com") 
+            fill_in("password1", with: "secretive") 
+            fill_in("password-confirmation", with: "secretive")    
+            find("#create-form #signup-button").click() 
+        end
+    end 
 end
