ff5f2f9eb6aea8616a412e832bacec43a637eb06
shreyaa-sharmaa <shreyasharma.ss305@gmail.com>
2020-10-07 02:41:38 +0530

    Sign up form validation test

--- test/system/signup_form_test.rb
+++ test/system/signup_form_test.rb
@@ -1,4 +1,21 @@
 require "application_system_test_case"
 
 class SignupFormTest < ApplicationSystemTestCase
+    def setup
+      visit "/"
+    end
+  
+    test "the signup form is validated" do 
+        visit "/signup" 
+       
+        assert_difference 'User.count', 0 do
+            #Signs up with registered email 
+            fill_in("username-signup", with: "abc") 
+            fill_in("email", with: "jeff@pxlshp.com") 
+            fill_in("password1", with: "secretive") 
+            fill_in("password-confirmation", with: "secretive") 
+        
+            find("#create-form #signup-button").click() 
+        end
+    end 
 end
