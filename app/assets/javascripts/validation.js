930e33f5d79704e839fa416c5cfe3157c5fef648
shreyaa-sharmaa <shreyasharma.ss305@gmail.com>
2020-10-09 18:56:22 +0530

    Refactor validate condition to reduce complexity

--- app/assets/javascripts/validation.js
+++ app/assets/javascripts/validation.js
@@ -48,21 +48,13 @@ $(document).ready(function() {
     var signUpRegisterForm = new SignUpFormValidator(".signup-register-form");
 
     if (signUpErrorMessages.innerHTML.includes("error")) {
-      if (signUpErrorMessages.innerHTML.includes("Email") && signUpErrorMessages.innerHTML.includes("Username")) {
+      signUpRegisterForm.updateUI(signUpRegisterForm.emailElement, true);
+      signUpRegisterForm.updateUI(signUpRegisterForm.usernameElement, true);
+      if (signUpErrorMessages.innerHTML.includes("Email")) {
         signUpRegisterForm.updateUI(signUpRegisterForm.emailElement, false, "Email already exists");
+      }
+      if (signUpErrorMessages.innerHTML.includes("Username")) {
         signUpRegisterForm.updateUI(signUpRegisterForm.usernameElement, false, "Username already exists");
-      }
-      else if (signUpErrorMessages.innerHTML.includes("Email") && !(signUpErrorMessages.innerHTML.includes("Username"))) {
-        signUpRegisterForm.updateUI(signUpRegisterForm.emailElement, false, "Email already exists");
-        signUpRegisterForm.updateUI(signUpRegisterForm.usernameElement, true);
-      }
-      else if (!(signUpErrorMessages.innerHTML.includes("Email")) && signUpErrorMessages.innerHTML.includes("Username")) {
-        signUpRegisterForm.updateUI(signUpRegisterForm.emailElement, true);
-        signUpRegisterForm.updateUI(signUpRegisterForm.usernameElement, false, "Username already exists");
-      }
-      else {
-        signUpRegisterForm.updateUI(signUpRegisterForm.emailElement, true);
-        signUpRegisterForm.updateUI(signUpRegisterForm.usernameElement, true);
       }
     }
   }
