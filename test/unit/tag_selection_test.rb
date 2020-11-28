d3a316222a31b1d0ae0bb39fdcb299f6858b6ab1
Peter Karth <falonofthetower@gmail.com>
2020-11-28 13:11:26 -0500

    Remove TimeCop from the tag_selection_test (#8774)
    By setting the date with Time.now just once we remove the need for
    freezing time within the spec. Now the dates used are always consistent
    with each other.

--- test/unit/tag_selection_test.rb
+++ test/unit/tag_selection_test.rb
@@ -3,16 +3,15 @@ require 'test_helper'
 class TagSelectionTest < ActiveSupport::TestCase
 
   test 'graph' do
-    Time.zone.now.change(nsec: 0)
-    Timecop.freeze
+    end_time = Time.now
+    start_time = end_time - 1.year
     ts_count = TagSelection.select(:following, :created_at)
-      .where(following: true, created_at: (Time.now - 1.year..Time.now))
+      .where(following: true, created_at: (start_time..end_time))
       .count
 
-    graph = TagSelection.graph(Time.now - 1.year, Time.now)
+    graph = TagSelection.graph(start_time, end_time)
 
     assert_equal ts_count, graph.values.sum
     assert_equal Hash, graph.class
-    Timecop.return
   end
 end
