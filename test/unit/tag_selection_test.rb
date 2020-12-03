7c8bbe4d69a5a812411fb30e54fea712b7b4c55f
Cess <cessmbuguar@gmail.com>
2020-12-03 10:14:53 +0300

    bring back timecop

--- test/unit/tag_selection_test.rb
+++ test/unit/tag_selection_test.rb
@@ -3,15 +3,15 @@ require 'test_helper'
 class TagSelectionTest < ActiveSupport::TestCase
 
   test 'graph' do
-    end_time = Time.now
-    start_time = end_time - 1.year
+    Time.zone.now.change(nsec: 0)
+    Timecop.freeze
     ts_count = TagSelection.select(:following, :created_at)
-      .where(following: true, created_at: (start_time..end_time))
+    where(following: true, created_at: (Time.now - 1.year..Time.now))
       .count
-
-    graph = TagSelection.graph(start_time, end_time)
+    graph = TagSelection.graph(Time.now - 1.year, Time.now)
 
     assert_equal ts_count, graph.values.sum
     assert_equal Hash, graph.class
+    Timecop.return
   end
 end
