bbbda2308dfbc4509e883baac0a42b8756c94081
Manasa <43354059+Manasa2850@users.noreply.github.com>
2021-01-06 19:47:16 +0530

    Remove Less and less-rails gems, including the-ruby-racer (#8918)
    * removed Less and less-rails gems
    
    * trigger build
    
    * resolve merge conflict
    
    * updated application.rb

--- config/application.rb
+++ config/application.rb
@@ -77,8 +77,6 @@ module Plots2
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.paths << Rails.root.join("public","lib")
-    config.less.paths << Rails.root.join("assets","less")
-    config.less.compress = true
 
     # Add environments to skylight
     config.skylight.environments += ["staging_unstable", "staging"]
