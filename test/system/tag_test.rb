e9bed7908a0ebd19b2f18df176dc14289a16ddc5
Manasa <43354059+Manasa2850@users.noreply.github.com>
2021-01-15 00:54:27 +0530

    Update tag_test.rb

--- test/system/tag_test.rb
+++ test/system/tag_test.rb
@@ -54,7 +54,7 @@ class TagTest < ApplicationSystemTestCase
   
   test 'adding a location tag to a user profile' do
     visit "/profile/jeff"
-    find('#location-btn').click
+    find('.blurred-location-input').click
     page.should have_content('Enter a location')
   end
 end
