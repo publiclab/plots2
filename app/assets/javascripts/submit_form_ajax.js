a8819165001afc56f46bf58b7245ebfe67a1ce3c
Ruth Njeri <ruthnwaiganjo@gmail.com>
2021-02-02 18:06:00 +0300

    notification when user saves location

--- app/assets/javascripts/submit_form_ajax.js
+++ app/assets/javascripts/submit_form_ajax.js
@@ -1,7 +1,7 @@
 // Takes in a data object that contains the info to be submitted in the form property: dataString
 //   and the url to submit to the controller
 // Allows data to be submitted from anywhere on the page using Javascript without using the form itself
-function sendFormSubmissionAjax(dataObj, submitTo, responseEl = "") {
+function sendFormSubmissionAjax(dataObj, submitTo, responseEl = "", callback) {
   let url = '';
   if(submitTo.slice(0,1) === "/") {
     url = submitTo;
@@ -15,6 +15,7 @@ function sendFormSubmissionAjax(dataObj, submitTo, responseEl = "") {
       if (responseEl !== "") {
         $(responseEl).trigger('ajax:success', event);
       }
+      if (callback) callback(event);
     }
   });
 }
