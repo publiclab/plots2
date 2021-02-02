40c72d65fc236971d7dab27148d251ad812a0584
Ruth Njeri <ruthnwaiganjo@gmail.com>
2021-02-02 19:21:49 +0300

    minor changes and feedback

--- app/assets/javascripts/submit_form_ajax.js
+++ app/assets/javascripts/submit_form_ajax.js
@@ -2,20 +2,25 @@
 //   and the url to submit to the controller
 // Allows data to be submitted from anywhere on the page using Javascript without using the form itself
 function sendFormSubmissionAjax(dataObj, submitTo, responseEl = "", callback) {
-  let url = '';
-  if(submitTo.slice(0,1) === "/") {
-    url = submitTo;
-  } else {
-    url = $(submitTo).attr('action');
-  }
+  let url = urlValue(submitTo);
   $.ajax({
     url: url,
     data: dataObj
   })
   .done((event) =>{
-    if (responseEl !== "") {
+    if (!!responseEl) {
       $(responseEl).trigger('ajax:success', event);
     }
-    if (callback) callback(event);
+    if (callback) return  callback(event);
   })
+}
+
+function urlValue(submitTo){
+  let value = '';
+  if(submitTo.slice(0,1) === "/") {
+    value = submitTo;
+  } else {
+    value = $(submitTo).attr('action');
+  }
+  return value
 }
