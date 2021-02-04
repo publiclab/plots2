1c51ffa2c28e9452cebdea19919290f3c6269a7b
noi5e <will.gutierrez@gmail.com>
2021-02-03 16:38:39 -0800

    Prune Editor State Management (#9108)
    * merge editor refactor branches #9004
    
    * delete refresh & isSingleFormPage methods #9004
    
    * delete some D.selected assignments #9004
    
    * add dropzone class to choose one input elements
    
    * E.setState on image select upload #9004
    
    * remove D.selected from progress bar handlers #9004
    
    * add unique IDs to image upload progress bars #9004
    
    * updated image upload progress bar selectors #9004
    
    * update progress bar selector #9004
    
    * update selectors for image upload text #9004

--- test/system/comment_test.rb
+++ test/system/comment_test.rb
@@ -275,8 +275,8 @@ class CommentTest < ApplicationSystemTestCase
       # find edit comment's fileinput:
       page.find('#fileinput-button-edit-' + comment_id_num).set("#{Rails.root.to_s}/public/images/pl.png")
       Capybara.ignore_hidden_elements = true
-      assert_selector('#c' + comment_id_num + 'progress')
-      assert_selector('#c' + comment_id_num + 'uploading')
+      assert_selector('#image-upload-progress-container-edit-' + comment_id_num)
+      assert_selector('#image-upload-text-edit-' + comment_id_num)
     end
   end
 
