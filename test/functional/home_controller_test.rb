65c1617fdb97a6018fc2bea64ecd33e001915179
Ruth <ruthnwaiganjo@gmail.com>
2021-02-04 08:44:39 +0300

    [Outreachy] Add Trending Tag Test (#9117)
    * add trending tag test
    
    * change wording

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -142,4 +142,12 @@ class HomeControllerTest < ActionController::TestCase
     assert_includes response.body, alert
   end
 
+  test 'trending tags are returned when a user has not subscribed to any topics' do
+    current_user = users(:user_without_tag_subscriptions)
+    expected_tag = Tag.trending.pluck(:name).sample
+    UserSession.create(current_user)
+    get :dashboard_v2
+    assert_includes response.body, expected_tag
+  end
+
 end
