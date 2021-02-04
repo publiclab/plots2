27948db26aa8688c8dd35d9dde789252b1663e52
Manasa2850 <manasakashyap28@gmail.com>
2021-02-04 17:03:33 +0530

    added reply email

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -245,6 +245,14 @@ class Comment < ApplicationRecord
           add_comment(mail, comment.nid, user, [true, comment.id])
         end
       end
+    else
+      email = mail.select { |s| s.match(/.*@.*/) }
+      ActionMailer::Base.mail(
+        from: "me@example.com", # @jywarren what should this be?
+        to: email,
+        subject: "Could not post your reply",
+        body: "Your reply wasn't posted since we couldn't find your account on publiclab.org. Please sign up on publiclab.org"
+      ).deliver
     end
   end
 
