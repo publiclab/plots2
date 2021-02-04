c9476760b7a1b2b3e630aacb3f2c3078f5c5576e
Ruth <ruthnwaiganjo@gmail.com>
2021-02-04 10:21:21 +0300

    [Outreachy] Notification When User Saves Location (#9113)
    * notification when user saves location
    
    * add semicolon
    
    * update ajax syntax
    
    * minor changes and feedback
    
    * resolve codeclimate
    
    * remove extra spacing
    
    * && boolean logic
    
    * convert responseEL to bool

--- app/assets/javascripts/submit_form_ajax.js
+++ app/assets/javascripts/submit_form_ajax.js
@@ -1,20 +1,26 @@
 // Takes in a data object that contains the info to be submitted in the form property: dataString
 //   and the url to submit to the controller
 // Allows data to be submitted from anywhere on the page using Javascript without using the form itself
-function sendFormSubmissionAjax(dataObj, submitTo, responseEl = "") {
-  let url = '';
-  if(submitTo.slice(0,1) === "/") {
-    url = submitTo;
-  } else {
-    url = $(submitTo).attr('action');
-  }
+function sendFormSubmissionAjax(dataObj, submitTo, responseEl = "", callback) {
+  let url = urlValue(submitTo);
   $.ajax({
     url: url,
-    data: dataObj,
-    success: (event, success) => {
-      if (responseEl !== "") {
-        $(responseEl).trigger('ajax:success', event);
-      }
+    data: dataObj
+  })
+  .done((event) =>{
+    if (!!responseEl) {
+      $(responseEl).trigger('ajax:success', event);
     }
-  });
+    callback && callback(event);
+  })
+}
+
+function urlValue(submitTo){
+  let value = '';
+  if(submitTo.slice(0,1) === "/") {
+    value = submitTo;
+  } else {
+    value = $(submitTo).attr('action');
+  }
+  return value;
 }
