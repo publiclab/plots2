bc9612290b65249c98e96420bc3ed4644e5c5ed3
noi5e <will.gutierrez@gmail.com>
2021-02-09 07:57:38 -0800

    Fix Rich-Text & Image Upload in FRESHLY Posted Comments (#9162)
    * delete unused code; add semicolon #9072
    
    * attach eventHandlers to #legacy-editor-container #9072
    
    * add image upload functionality to new comments #9072
    
    * add imagebar caption #9072
    
    * add #legacy-editor-container class to attach eventHandlers #9072
    
    * add system test for rich-text & image upload on fresh comments #9072

--- app/assets/javascripts/editorToolbar.js
+++ app/assets/javascripts/editorToolbar.js
@@ -7,125 +7,129 @@
 
 // * * * * * * * * * * * 
 
+// used to create fileupload functionality for: 
+//   1. existing comments at document.load
+//   2. dynamically inserted (freshly posted) comments, see /app/views/comments/create.js.erb
+function getFileUploadOptions(dropZone, isSmallDropzone = false) {
+  return {
+    url: "/images",
+    paramName: "image[photo]",
+    dropZone: isSmallDropzone ? null : dropZone,
+    dataType: 'json',
+    formData: {
+      'uid':$D.uid,
+      'nid':$D.nid
+    },
+    // 'start' function runs:
+      //   1. when user drag-and-drops image
+      //   2. when user clicks on upload button.
+    start: function(e) {
+      $E.setState(e.target.dataset.formId); // string that is: "main", "reply-123", "edit-123" etc.
+      $(e.target).removeClass('hover');
+      $("#image-upload-progress-container-" + $E.commentFormID).show();
+      $("#image-upload-text-" + $E.commentFormID).show();
+      $("#choose-one-" + $E.commentFormID).hide();
+    },
+    done: function (e, data) {
+      $("#image-upload-progress-container-" + $E.commentFormID).hide();
+      $("#image-upload-text-" + $E.commentFormID).hide();
+      $("#choose-one-" + $E.commentFormID).show();
+      $("#image-upload-progress-bar-" + $E.commentFormID).css('width', 0);
+      var extension = data.result['filename'].split('.')[data.result['filename'].split('.').length - 1]; var file_url = data.result.url.split('?')[0]; var file_type;
+      if (['gif', 'GIF', 'jpeg', 'JPEG', 'jpg', 'JPG', 'png', 'PNG'].includes(extension))
+        file_type = 'image'
+      else if (['csv', 'CSV'].includes(extension))
+        file_type = 'csv'
+      switch (file_type) {
+        case 'image':
+          orig_image_url = file_url + '?s=o' // size = original
+          $E.wrap('[![', '](' + file_url + ')](' + orig_image_url + ')', true, data.result['filename']);
+          break;
+        case 'csv':
+          $E.wrap('[graph:' + file_url + ']', '', true);
+          break;
+        default:
+          $E.wrap('<a href="'+data.result.url.split('?')[0]+'"><i class="fa fa-file"></i> ', '</a>', true, data.result['filename'].replace(/[()]/g , "-")); // on its own line; see /app/assets/js/editor.js
+      }
+      // here append the image id to the wiki edit form:
+      if ($('#node_images').val() && $('#node_images').val().split(',').length > 1) $('#node_images').val([$('#node_images').val(),data.result.id].join(','))
+      else $('#node_images').val(data.result.id)
+    },
+    fileuploadfail: function(e, data) {
+      console.log(e);
+    },
+    progressall: function (e, data) {
+      const closestProgressBar = $("#image-upload-progress-bar-" + $E.commentFormID);
+      return progressAll(closestProgressBar, data);
+    }
+  };
+}
+
+function progressAll(elem, data) {
+  var progress = parseInt(data.loaded / data.total * 100, 10);
+  $(elem).css(
+    'width',
+    progress + '%'
+  );
+}
+
 // attach eventListeners on document.load: 
 //   1. rich-text buttons
 //   2. save & recover buttons
 //   3. textareas
 //   4. image upload .dropzones & buttons
 $(function() {
-  // for rich-text buttons (bold, italic, header, and link):
-  $('.rich-text-button').on('click', function(e) {
-    $E.setState(e.currentTarget.dataset.formId); // string that is: "main", "reply-123", "edit-123" etc.
-    const action = e.currentTarget.dataset.action // 'bold', 'italic', etc.
-    $E[action](); // call the appropriate editor function
-  });
-
-  // for save & recover buttons
-  $('.save-button').on('click', function(e) {
-    $E.setState(e.currentTarget.dataset.formId); // string that is: "main", "reply-123", "edit-123" etc.
-    $E.save($E);
-  });
-
-  $('.recover-button').on('click', function(e) {
-    $E.setState(e.currentTarget.dataset.formId); // string that is: "main", "reply-123", "edit-123" etc.
-    $E.recover();
-  });
-
-  // textAreas
-  $('.text-input').on('click', function(e) {
-    $E.setState(e.currentTarget.dataset.formId);
-  });
-
-  // image upload event listeners for both:
-  //   1. click-to-upload
-  //   2. drag & drop
-  // based on the basic plugin from jQuery file upload: 
-  // https://github.com/blueimp/jQuery-File-Upload/wiki/Basic-plugin
-  $('.dropzone').each(function() {
+  // attach all events to #legacy-editor-container
+  // this is so dynamically inserted forms (ie. freshly posted comments) will have events automatically attached too.
+  $("#legacy-editor-container")
+    // for rich-text buttons (bold, italic, header, and link):
+    .on("click", ".rich-text-button", function(e) {
+      $E.setState(e.currentTarget.dataset.formId); // string that is: "main", "reply-123", "edit-123" etc.
+      const action = e.currentTarget.dataset.action // 'bold', 'italic', etc.
+      $E[action](); // call the appropriate editor function
+    })
+    // for save & recover buttons
+    .on("click", ".save-button", function(e) {
+      $E.setState(e.currentTarget.dataset.formId); // string that is: "main", "reply-123", "edit-123" etc.
+      $E.save($E);
+    })
+    .on("click", ".recover-button", function(e) {
+      $E.setState(e.currentTarget.dataset.formId); // string that is: "main", "reply-123", "edit-123" etc.
+      $E.recover();
+    })
+    // textAreas
+    .on("click", ".text-input", function(e) {
+      $E.setState(e.currentTarget.dataset.formId);
+    })
     // style changes for dragging an image over a dropzone
-    $(this).on('dragenter',function(e) {
+    .on("dragenter", ".dropzone", function(e) {
       e.preventDefault();
       $(e.currentTarget).addClass('hover');
-    });
-
-    $(this).on('dragleave',function(e) {
+    })
+    .on("dragleave", ".dropzone", function(e) {
       $(e.currentTarget).removeClass('hover');
-    });
-
-    // runs on drag & drop
-    $(this).on('drop',function(e) {
+    })
+    // set the editor's comment form state on drop
+    .on("drop", ".dropzone", function(e) {
       e.preventDefault();
       $E.setState(e.currentTarget.dataset.formId); // string that is: "main", "reply-123", "edit-123" etc.
     });
 
+  // image upload functionality for both:
+  //   1. click-to-upload
+  //   2. drag & drop
+  // based on the basic plugin from jQuery file upload: 
+  // https://github.com/blueimp/jQuery-File-Upload/wiki/Basic-plugin
+
+  // this instantiates fileupload for every comment form present at document.load
+  // for freshly posted comments, see /app/views/comments/create.js.erb
+  $('.dropzone').each(function() {
     // disable drag-and-drop image upload on small dropzones
     // small dropzones are the image upload button in the comment form toolbar
-    let dropZone = $(this);
-    if ($(this).hasClass("dropzone-small")) {
-      dropZone = null;
-    }
-
-    $(this).fileupload({
-      url: "/images",
-        paramName: "image[photo]",
-        dropZone: dropZone,
-        dataType: 'json',
-        formData: {
-          'uid':$D.uid,
-          'nid':$D.nid
-        },
-        // 'start' function runs:
-          //   1. when user drag-and-drops image
-          //   2. when user clicks on upload button.
-        start: function(e) {
-          $E.setState(e.target.dataset.formId); // string that is: "main", "reply-123", "edit-123" etc.
-          $(e.target).removeClass('hover');
-          $("#image-upload-progress-container-" + $E.commentFormID).show();
-          $("#image-upload-text-" + $E.commentFormID).show();
-          $("#choose-one-" + $E.commentFormID).hide();
-        },
-        done: function (e, data) {
-          $("#image-upload-progress-container-" + $E.commentFormID).hide();
-          $("#image-upload-text-" + $E.commentFormID).hide();
-          $("#choose-one-" + $E.commentFormID).show();
-          $("#image-upload-progress-bar-" + $E.commentFormID).css('width', 0);
-          var extension = data.result['filename'].split('.')[data.result['filename'].split('.').length - 1]; var file_url = data.result.url.split('?')[0]; var file_type;
-          if (['gif', 'GIF', 'jpeg', 'JPEG', 'jpg', 'JPG', 'png', 'PNG'].includes(extension))
-            file_type = 'image'
-          else if (['csv', 'CSV'].includes(extension))
-            file_type = 'csv'
-          switch (file_type) {
-            case 'image':
-              orig_image_url = file_url + '?s=o' // size = original
-              $E.wrap('[![', '](' + file_url + ')](' + orig_image_url + ')', true, data.result['filename']);
-              break;
-            case 'csv':
-              $E.wrap('[graph:' + file_url + ']', '', true);
-              break;
-            default:
-              $E.wrap('<a href="'+data.result.url.split('?')[0]+'"><i class="fa fa-file"></i> ', '</a>', true, data.result['filename'].replace(/[()]/g , "-")); // on its own line; see /app/assets/js/editor.js
-          }
-          // here append the image id to the wiki edit form:
-          if ($('#node_images').val() && $('#node_images').val().split(',').length > 1) $('#node_images').val([$('#node_images').val(),data.result.id].join(','))
-          else $('#node_images').val(data.result.id)
-        },
-        fileuploadfail: function(e, data) {
-          console.log(e);
-        },
-        progressall: function (e, data) {
-          const closestProgressBar = $("#image-upload-progress-bar-" + $E.commentFormID);
-          return progressAll(closestProgressBar, data);
-        }
-    });
+    const isSmallDropzone = $(this).hasClass("dropzone-small");
+    const fileUploadOptions = getFileUploadOptions($(this), isSmallDropzone);
+    $(this).fileupload(fileUploadOptions);
   });
-
-  const progressAll = (elem, data) => {
-    var progress = parseInt(data.loaded / data.total * 100, 10);
-    $(elem).css(
-      'width',
-      progress + '%'
-    );
-  }
 
   // #side-dropzone, is for the main image of research notes, in /app/views/editor/post.html.erb
   $('#side-dropzone').on('dragover',function(e) {
--- test/system/comment_test.rb
+++ test/system/comment_test.rb
@@ -376,6 +376,42 @@ class CommentTest < ApplicationSystemTestCase
       assert_equal(reply_text, comment_text_reply)
       assert_equal(edit_text, comment_text_edit)
     end
+
+    test "#{page_type_string}: rich-text change and image upload work on editing FRESH comment" do
+      visit get_path(page_type, nodes(node_name).path)
+      # make a fresh comment in the main comment form
+      main_comment_form =  page.find('#comment-form-main')
+      # fill out the comment form
+      main_comment_form.find('#text-input-main')
+        .click
+        .fill_in with: comment_text
+      # publish
+      main_comment_form
+        .find('button', text: 'Publish')
+        .click
+      page.find(".noty_body", text: "Comment Added!")
+      # we need the ID of parent div that contains <p>comment_text</p>:
+      fresh_comment_id = page.find('p', text: comment_text).find(:xpath, '..')[:id]
+      # regex to strip the ID number out of string. ID format is comment-body-4231
+      fresh_comment_id_num = /comment-body-(\d+)/.match(fresh_comment_id)[1]
+      # open up the edit comment form
+      page.find(".edit-comment-btn").click
+      # click on the Bold rich-text button
+      page.find("#bold-button-edit-" + fresh_comment_id_num).click
+      # find what's in the textarea
+      edit_input_value = page.find('#text-input-edit-' + fresh_comment_id_num).value
+      # 1st assertion: check if bold (****) text is in the textarea!
+      assert_equal('****' + comment_text, edit_input_value)
+      # upload image by select
+      Capybara.ignore_hidden_elements = false
+      page.find('#fileinput-button-edit-' + fresh_comment_id_num).set("#{Rails.root.to_s}/public/images/pl.png")
+      Capybara.ignore_hidden_elements = true
+      wait_for_ajax
+      # click on comment preview button
+      page.find('#comment-form-edit-' + fresh_comment_id_num + ' a', text: 'Preview').click
+      # 2nd assertion: check if image uploaded
+      assert_selector('#comment-preview-edit-' + fresh_comment_id_num + ' img', count: 1)
+    end
   end
 
   # TESTS for ALL PAGE TYPES!
