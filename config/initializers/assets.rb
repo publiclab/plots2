4c1060522d8bcd1a7bd6ec5785b53e66139aee4f
Katie Bankhead <56419546+katiebankhead@users.noreply.github.com>
2021-02-14 02:15:23 -0700

    Rename async_tag_subscriptions.js to asyncTagSubscriptions.js (#9194)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -34,7 +34,7 @@ Rails.application.config.assets.precompile += [
   'wiki.css',
   'horsey.js',
   'atwho_autocomplete.js',
-  'async_tag_subscriptions.js',
+  'asyncTagSubscriptions.js',
   'cable.js',
   'spam2.css',
   'spam2.js',
