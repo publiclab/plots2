09cbda7e73dae3a2ad76e5394cf199fefac96185
Ruth Njeri <ruthnwaiganjo@gmail.com>
2021-02-18 19:25:26 +0300

    test if test will fail

--- test/system/dashboard_v2_test.rb
+++ test/system/dashboard_v2_test.rb
@@ -3,23 +3,6 @@ require "application_system_test_case"
 class DashboardV2Test < ApplicationSystemTestCase
   Capybara.default_max_wait_time = 60
 
-  test 'view trending topics' do
-    user = users(:bob)
-    subscribed_tags = TagSelection.where(user_id: user.id).pluck(:tid)
-    # Return the name of a subscribed tag at random
-    tag_name = Tag.where(tid: subscribed_tags).pluck(:name).sample
-
-    visit '/'
-    click_on 'Login'
-    fill_in("username-login", with: "Bob")
-    fill_in("password-signup", with: "secretive")
-    click_on 'Log in'
-    visit '/v1/dashboard'
-    # Ensure that a subscribed tag is not present on the trending and follow section
-    assert_selector("div > div.other-topics > span a[href='/tag/#{tag_name}']", count: 0)
-    assert_selector("div#moreTopics div > div > div a[href='/tag/#{tag_name}']", count: 0)
-  end
-
   test 'trending tags are returned when a user has not subscribed to any topics' do
     visit '/'
     click_on 'Login'
