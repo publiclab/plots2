53d7b58d703644da062d615321e02f1a54bfe94b
jywarren <jeff@unterbahn.com>
2021-02-19 15:02:30 +0000

    re-add test for owmloading error

--- test/system/dashboard_v2_test.rb
+++ test/system/dashboard_v2_test.rb
@@ -14,4 +14,22 @@ class DashboardV2Test < ApplicationSystemTestCase
     assert_selector("div > div.other-topics > span a")
     assert_selector("div#moreTopics div > div > div a")
   end
+
+  test 'view trending topics' do	
+    user = users(:bob)	
+    subscribed_tags = TagSelection.where(user_id: user.id).pluck(:tid)	
+    # Return the name of a subscribed tag at random	
+    tag_name = Tag.where(tid: subscribed_tags).pluck(:name).sample	
+
+    visit '/'	
+    click_on 'Login'	
+    fill_in("username-login", with: "Bob")	
+    fill_in("password-signup", with: "secretive")	
+    click_on 'Log in'	
+    visit '/v2/dashboard'	
+    # Ensure that a subscribed tag is not present on the trending and follow section	
+    assert_selector("div > div.other-topics > span a[href='/tag/#{tag_name}']", count: 0)	
+    assert_selector("div#moreTopics div > div > div a[href='/tag/#{tag_name}']", count: 0)	
+  end
+
 end
