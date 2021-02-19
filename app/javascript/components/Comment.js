436ac0daea91427a9dae47e202fa89e8f5e9a45c
Will Gutierrez <will.gutierrez@gmail.com>
2021-02-18 17:00:41 -0800

    add author pic, username, time posted, border, reply link #9175

--- app/javascript/components/Comment.js
+++ app/javascript/components/Comment.js
@@ -3,40 +3,70 @@ import PropTypes from "prop-types";
 
 class Comment extends React.Component {
   render() {
+    const {  
+      authorPicUrl,
+      authorPicFilename,
+      authorUsername,
+      cid,
+      commentName,
+      htmlCommentText,
+      rawCommentText,
+      replies,
+      replyTo,
+      timeCreatedString
+    } = this.props.comment;
+
     let authorSection = [];
-    if (this.props.comment.author) {
-      const authorProfilePic = (this.props.comment.author.photo_file_name) ?
-        <img
-          className="rounded-circle"
-          alt="Comment Author Profile Picture"
-          src={this.props.comment.author.photo_path.thumb}
+    const authorProfilePic = (authorPicFilename) ? 
+      <img
+        className="rounded-circle"
+        alt="Comment Author Profile Picture"
+        src={authorPicUrl}
+        style={{
+          marginRight: "6px",
+          width: "32px"
+        }}
+      /> :
+      <div 
+        className="rounded-circle" 
+        style={{
+          background: "#ccc", 
+          display: "inline-block", 
+          height: "32px", 
+          marginRight: "6px", 
+          verticalAlign: "middle", 
+          width: "32px"
+        }} 
+      ></div>;
+      
+    const authorName = (authorUsername) ?
+      <a href={"/profile/" + authorUsername}>
+        {" " + authorUsername}
+      </a> :
+      commentName;
+    authorSection = authorSection.concat([authorProfilePic, authorName]);
+    
+    let replySection = "";
+    if (!replyTo) {
+      const repliesList = replies.map((reply, index) => {
+        return <Comment key={index} comment={reply} userCommentedText={this.props.userCommentedText} />;
+      });
+      const replyToggleLink = <p
+          id={"comment-" + cid + "-reply-toggle"}
           style={{
-            marginRight: "6px",
-            width: "32px"
+            color: "#006dcc",
+            cursor: "pointer",
+            userSelect: "none"
           }}
-        /> :
-        <div
-          className="rounded-circle"
-          style={{
-            background: "#ccc",
-            display: "inline-block",
-            height: "32px",
-            marginRight: "6px",
-            verticalAlign: "middle",
-            width: "32px"
-          }}
-        />;
-      const authorName = (this.props.comment.author.name) ?
-        <a href={"/profile/" + this.props.comment.author.name}>
-          {this.props.comment.author.name}
-        </a> :
-        this.props.comment.name;
-      authorSection.concat([authorProfilePic, authorName]);
+        >
+          Reply to this comment...
+        </p>;
+      replySection = [repliesList, replyToggleLink];
     }
 
     return (
       <div
-        id={"c" + this.props.comment.cid}
+        id={"c" + cid}
         className="comment"
         style={{
           marginTop: "20px",
@@ -58,7 +88,10 @@ class Comment extends React.Component {
           <div className="navbar-text float-left d-md-none">&nbsp;&nbsp;</div>
           <div className="navbar-text float-left">
             {authorSection}
-            {/* placeholder: user commented text */}
+            <span className="d-none d-md-inline">{" " + this.props.userCommentedText}</span>
+            <a style={{ color: "#aaa" }} href={"#c" + cid}>
+              {" " + timeCreatedString}
+            </a>
           </div>
           <div
             className="navbar-text float-right"
@@ -67,17 +100,41 @@ class Comment extends React.Component {
               paddingRight: "10px"
             }}
           >
-
+            {/* placeholder: role icon for admins and moderators--but it's commented out in the original partial? */}
+            {/* placeholder: this comment was posted by email */}
+            {/* placeholder: top-right corner buttons (edit, delete, mark as spam, emoji reactions) */}
           </div>
-          {this.props.comment.commentText}
         </div>
+        <div
+          id={"c" + cid + "show"}
+          style={{
+            border: "1px solid #e7e7e7",
+            padding: "36px"
+          }}
+        >
+          <div 
+            id={"comment-body-" + cid} 
+            className="comment-body"
+          >
+            {htmlCommentText}
+            {/* partial has a contain_trimmed_body bit ? */}
+            {/* is this a question? post it to the questions page */}
+            {/* breakout questions */}
+            {/* have you attempted or completed this activity? */}
+          </div>
+          {replySection}
+          {/* reply form, or link to login if no currentUser */}
+        </div>
+        {/* emojis section */}
+        {/* edit comment form */}
       </div>
     );
   }
 }
 
 Comment.propTypes = {
-  comment: PropTypes.object
+  comment: PropTypes.object,
+  userCommentedText: PropTypes.string
 };
 
 export default Comment;
