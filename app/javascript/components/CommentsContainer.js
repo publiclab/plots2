d6fa11faf03658da0d77f0cf99add0e8f1ea5d79
Will Gutierrez <will.gutierrez@gmail.com>
2021-02-21 12:25:24 -0800

    create User Context #9175

--- app/javascript/components/CommentsContainer.js
+++ app/javascript/components/CommentsContainer.js
@@ -1,31 +1,55 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import CommentForm from "./CommentForm";
+import  { UserContext } from "./user-context";
 import Comment from "./Comment";
+import CommentForm from "./CommentForm";
 
 class CommentsContainer extends React.Component {
-  
   render() {
-    const comments = this.props.comments.map((comment, index) => {
-      
-      return <Comment key={index} comment={comment} userCommentedText={this.props.userCommentedText} />;
+    const {
+      comments,
+      commentsHeaderText,
+      currentUser,
+      nodeAuthorId,
+      nodeId,
+      userCommentedText
+    } = this.props;
+
+    const commentsList = comments.map((comment, index) => {
+      const replyCommentForm = comment.replyTo ?
+      null :
+      <CommentForm
+        commentId={comment.commentId}
+        commentFormType="reply"
+        nodeId={nodeId}
+      />;
+
+      return <Comment 
+        key={index} 
+        comment={comment} 
+        nodeAuthorId={nodeAuthorId}
+        replyCommentForm={replyCommentForm}
+        userCommentedText={userCommentedText} 
+      />;
     })
 
     return (
-      <div id="legacy-editor-container" className="row">
-        <div id="comments" className="col-lg-10 comments">
-          <h3>
-            <span id="comment-count">
-              {comments.length + " " + this.props.commentsHeaderText}
-            </span>
-          </h3>
-          <div id="comments-list" style={{ marginBottom: "50px" }}>
-            {comments}
+      <UserContext.Provider value={currentUser}>
+        <div id="legacy-editor-container" className="row">
+          <div id="comments" className="col-lg-10 comments">
+            <h3>
+              <span id="comment-count">
+                {comments.length + " " + commentsHeaderText}
+              </span>
+            </h3>
+            <div id="comments-list" style={{ marginBottom: "50px" }}>
+              {commentsList}
+            </div>
+            <CommentForm commentFormType="main" nodeId={nodeId} />
           </div>
         </div>
-        <CommentForm location="main" />
-      </div>
+      </UserContext.Provider>
     );
   }
 }
@@ -33,6 +57,8 @@ class CommentsContainer extends React.Component {
 CommentsContainer.propTypes = {
   comments: PropTypes.array,
   commentsHeaderText: PropTypes.string,
+  nodeAuthorId: PropTypes.number,
+  nodeId: PropTypes.number,
   userCommentedText: PropTypes.string
 };
 
