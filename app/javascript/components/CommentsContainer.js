f116fbee18f9cbd558ce9469e177b6260b1e9635
Will Gutierrez <will.gutierrez@gmail.com>
2021-02-22 23:13:29 -0800

    create handlers for form submission and textarea input #9175

--- app/javascript/components/CommentsContainer.js
+++ app/javascript/components/CommentsContainer.js
@@ -1,72 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import  { UserContext } from "./user-context";
 import Comment from "./Comment";
 import CommentForm from "./CommentForm";
 
-class CommentsContainer extends React.Component {
-  render() {
-    const {
-      comments,
-      currentUser,
-      elementText,
-      elementText: {
-        commentFormPlaceholder,
-        commentsHeaderText,
-        commentPreviewText,
-        commentPublishText,
-        userCommentedText
-      },
-      nodeAuthorId,
-      nodeId
-    } = this.props;
-
-    const commentsList = comments.map((comment, index) => {
-      const replyCommentForm = comment.replyTo ?
-      null :
-      <CommentForm
-        commentId={comment.commentId}
-        commentFormPlaceholder={elementText.commentFormPlaceholder}
-        commentFormType="reply"
-        commentPreviewText={elementText.commentPreviewText}
-        commentPublishText={elementText.commentPublishText}
-        nodeId={nodeId}
-      />;
-
-      return <Comment 
-        key={index} 
-        comment={comment} 
-        nodeAuthorId={nodeAuthorId}
-        replyCommentForm={replyCommentForm}
-        userCommentedText={elementText.userCommentedText} 
-      />;
-    })
-
-    return (
-      <UserContext.Provider value={currentUser}>
-        <div id="legacy-editor-container" className="row">
-          <div id="comments" className="col-lg-10 comments">
-            <h3>
-              <span id="comment-count">
-                {comments.length + " " + elementText.commentsHeaderText}
-              </span>
-            </h3>
-            <div id="comments-list" style={{ marginBottom: "50px" }}>
-              {commentsList}
-            </div>
-            <CommentForm 
-              commentFormPlaceholder={elementText.commentFormPlaceholder}
-              commentFormType="main" 
-              commentPreviewText={elementText.commentPreviewText}
-              commentPublishText={elementText.commentPublishText}
-              nodeId={nodeId} 
-            />
-          </div>
-        </div>
-      </UserContext.Provider>
-    );
+const CommentsContainer = ({
+  // ES6 destructure the props
+  // so we can simply refer to comments instead of this.props.comments
+  comments,
+    currentUser,
+    elementText,
+    elementText: {
+      commentFormPlaceholder,
+      commentsHeaderText,
+      commentPreviewText,
+      commentPublishText,
+      userCommentedText
+    },
+    nodeAuthorId,
+    nodeId
+}) => {
+  // React Hook managing textarea input state 
+  const [textAreaValues, setTextAreaValues] = useState({});
+  // function for handling user input into comment form <textarea>s
+  const handleTextAreaChange = (event) => {
+    const value = event.target.value;
+    const formId = event.target.dataset.formId // eg. "main", "reply-123", "edit-432"
+    // textAreaValues is an object that holds multiple text forms, eg:
+    //   { main: "foo", reply-123: "bar" }
+    // keep the old state values (as ...state) and insert the new one
+    setTextAreaValues(state => ({ ...state, [formId]: value }));
   }
+
+  // comment form submission
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(textAreaValues);
+  }
+
+  // iterate over comments prop containing all node comments.
+  // create a Comment component containing 1-3 CommentForms.
+  const commentsList = comments.map((comment, index) => {
+    // if the comment is a reply to another comment, DON'T render a reply form.
+    // otherwise, the comment can accept replies
+    const replyCommentForm = comment.replyTo ?
+    null :
+    <CommentForm
+      commentId={comment.commentId}
+      commentFormPlaceholder={elementText.commentFormPlaceholder}
+      commentFormType="reply"
+      commentPreviewText={elementText.commentPreviewText}
+      commentPublishText={elementText.commentPublishText}
+      handleFormSubmit={handleFormSubmit}
+      handleTextAreaChange={handleTextAreaChange}
+      nodeId={nodeId}
+    />;
+
+    return <Comment 
+      key={index} 
+      comment={comment} 
+      nodeAuthorId={nodeAuthorId}
+      replyCommentForm={replyCommentForm}
+      userCommentedText={elementText.userCommentedText} 
+    />;
+  })
+
+  return (
+    // React Context ensures that all components below this one can access the currentUser prop object.
+    <UserContext.Provider value={currentUser}>
+      <div id="legacy-editor-container" className="row">
+        <div id="comments" className="col-lg-10 comments">
+          <h3>
+            <span id="comment-count">
+              {comments.length + " " + elementText.commentsHeaderText}
+            </span>
+          </h3>
+          <div id="comments-list" style={{ marginBottom: "50px" }}>
+            {commentsList}
+          </div>
+          <CommentForm 
+            commentFormPlaceholder={elementText.commentFormPlaceholder}
+            commentFormType="main" 
+            commentPreviewText={elementText.commentPreviewText}
+            commentPublishText={elementText.commentPublishText}
+            handleFormSubmit={handleFormSubmit}
+            handleTextAreaChange={handleTextAreaChange}
+            nodeId={nodeId} 
+          />
+        </div>
+      </div>
+    </UserContext.Provider>
+  );
 }
 
 CommentsContainer.propTypes = {
