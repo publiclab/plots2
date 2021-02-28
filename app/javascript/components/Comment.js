77961a2761d5c2f9a60ab5636e95f576206a1a10
Will Gutierrez <will.gutierrez@gmail.com>
2021-02-28 09:49:53 -0800

    extract CommentDisplay and CommentHeader into separate components #9175

--- app/javascript/components/Comment.js
+++ app/javascript/components/Comment.js
@@ -3,8 +3,8 @@ import PropTypes from "prop-types";
 
 import { UserContext } from "./user-context";
 
-import CommentAuthorSlug from "./CommentAuthorSlug.js";
-import CommentReplies from "./CommentReplies.js";
+import CommentDisplay from "./CommentDisplay";
+import CommentHeader from "./CommentHeader.js";
 import CommentToolbar from "./CommentToolbar.js";
 
 const Comment = ({
@@ -35,45 +35,6 @@ const Comment = ({
     setIsEditFormVisible(!isEditFormVisible);
   }
 
-  // this toggles visibility depending on isEditFormVisible
-  const commentDisplay = (user) => {
-    return (
-      <div
-        id={"c" + commentId + "show"}
-        style={{
-          border: "1px solid #e7e7e7",
-          padding: "36px"
-        }}
-      >
-        <div 
-          id={"comment-body-" + commentId} 
-          className="comment-body"
-        >
-          {htmlCommentText}
-          {/* partial has a contain_trimmed_body bit ? */}
-          {/* is this a question? post it to the questions page */}
-          {/* breakout questions */}
-          {/* have you attempted or completed this activity? */}
-        </div>
-        {/* only comments that DO NOT have a replyTo will have a reply section */}
-        {replyTo ?
-          "" :
-          <CommentReplies 
-            commentId={commentId}
-            currentUser={user}
-            isReplyFormVisible={isReplyFormVisible}
-            handleReplyFormToggle={() => setIsReplyFormVisible(!isReplyFormVisible)}
-            nodeAuthorId={nodeAuthorId}
-            replies={replies}
-            replyCommentForm={replyCommentForm}
-            userCommentedText={userCommentedText}
-          />
-        }
-        {/* emojis section - can decide which <div> this should nest into, in non-React version, this is right after the closing </div> tag below */}
-      </div>
-    );
-  }
-
   return (
     // hooks this component up to React Context, so it can access currentUser object.
     // see CommentsContainer.js
@@ -99,18 +60,15 @@ const Comment = ({
             }}
           >
             {/* placeholder: moderator controls for approving comments from first-time posters */}
-            <div className="navbar-text float-left">
-              <CommentAuthorSlug 
-                authorPicFilename={authorPicFilename}
-                authorUsername={authorUsername}
-                authorPicUrl={authorPicUrl}
-                commentName={commentName}
-              />
-              <span className="d-none d-md-inline">{" " + userCommentedText}</span>
-              <a style={{ color: "#aaa" }} href={"#c" + commentId}>
-                {" " + timeCreatedString}
-              </a>
-            </div>
+            <CommentHeader 
+              authorPicFilename={authorPicFilename}
+              authorUsername={authorUsername}
+              authorPicUrl={authorPicUrl}
+              commentId={commentId}
+              commentName={commentName}
+              timeCreatedString={timeCreatedString}
+              userCommentedText={userCommentedText}
+            />
             <CommentToolbar 
               authorId={authorId} 
               currentUser={currentUser} 
@@ -120,7 +78,18 @@ const Comment = ({
           </div>
           {isEditFormVisible ?
             editCommentForm :
-            commentDisplay(currentUser)
+            <CommentDisplay
+              commentId={commentId}
+              htmlCommentText={htmlCommentText}
+              isReplyFormVisible={isReplyFormVisible}
+              nodeAuthorId={nodeAuthorId}
+              replies={replies}
+              replyCommentForm={replyCommentForm}
+              replyTo={replyTo}
+              setIsReplyFormVisible={setIsReplyFormVisible}
+              user={currentUser}
+              userCommentedText={userCommentedText}
+            />
           }
         </div>
       )}
