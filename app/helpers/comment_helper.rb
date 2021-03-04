eadafb4c2602c9ec8c13c40f283a8bc0d9b31d0a
Will Gutierrez <will.gutierrez@gmail.com>
2021-03-04 14:19:42 -0800

    syntax tweaks to pass codeclimate #9175

--- app/helpers/comment_helper.rb
+++ app/helpers/comment_helper.rb
@@ -31,7 +31,7 @@ module CommentHelper
       # nest the comment's replies in an array within the comment
       comment_json[:replies] = get_react_comments(comment.replied_comments, true)
       comment_json[:replyTo] = comment.reply_to
-      time_created_string = distance_of_time_in_words(comment.created_at, Time.current, include_seconds: false, scope: 'datetime.time_ago_in_words' )
+      time_created_string = distance_of_time_in_words(comment.created_at, Time.current, include_seconds: false, scope: 'datetime.time_ago_in_words')
       comment_json[:timeCreatedString] = time_created_string
       comments << comment_json
     end
