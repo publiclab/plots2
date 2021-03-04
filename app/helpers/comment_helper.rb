e26cc2d5e6685c746e096073cf2a6939e5391356
Will Gutierrez <will.gutierrez@gmail.com>
2021-03-04 14:13:01 -0800

    added snake_case, changed hash syntax to pass codeclimate #9175

--- app/helpers/comment_helper.rb
+++ app/helpers/comment_helper.rb
@@ -16,25 +16,24 @@ module CommentHelper
   # used in notes_controller.rb and comment_controller.rb
   def get_react_comments(comments_record, getting_replies = false)
     comments = []
-    comments_record.each_with_index do |comment, index|
-      if comment.reply_to.nil? || getting_replies
-        commentJSON = {}
-        commentJSON[:authorId] = comment.uid
-        commentJSON[:authorPicFilename] = comment.author.photo_file_name
-        commentJSON[:authorPicUrl] = comment.author.photo_path(:thumb)
-        commentJSON[:authorUsername] = comment.author.username
-        commentJSON[:commentId] = comment.cid
-        commentJSON[:commentName] = comment.name
-        commentJSON[:createdAt] = comment.created_at
-        commentJSON[:htmlCommentText] = raw insert_extras(filtered_comment_body(comment.render_body))
-        commentJSON[:rawCommentText] = comment.comment
-        # nest the comment's replies in an array within the comment
-        commentJSON[:replies] = get_react_comments(comment.replied_comments, true)
-        commentJSON[:replyTo] = comment.reply_to
-        time_created_string = distance_of_time_in_words(comment.created_at, Time.current, { include_seconds: false, scope: 'datetime.time_ago_in_words' })
-        commentJSON[:timeCreatedString] = time_created_string
-        comments << commentJSON
-      end
+    comments_record.each do |comment|
+      next unless comment.reply_to.nil? || getting_replies
+      comment_json = {}
+      comment_json[:authorId] = comment.uid
+      comment_json[:authorPicFilename] = comment.author.photo_file_name
+      comment_json[:authorPicUrl] = comment.author.photo_path(:thumb)
+      comment_json[:authorUsername] = comment.author.username
+      comment_json[:commentId] = comment.cid
+      comment_json[:commentName] = comment.name
+      comment_json[:createdAt] = comment.created_at
+      comment_json[:htmlCommentText] = raw insert_extras(filtered_comment_body(comment.render_body))
+      comment_json[:rawCommentText] = comment.comment
+      # nest the comment's replies in an array within the comment
+      comment_json[:replies] = get_react_comments(comment.replied_comments, true)
+      comment_json[:replyTo] = comment.reply_to
+      time_created_string = distance_of_time_in_words(comment.created_at, Time.current, include_seconds: false, scope: 'datetime.time_ago_in_words' )
+      comment_json[:timeCreatedString] = time_created_string
+      comments << comment_json
     end
     comments
   end
