766c8bcc589be1ab497a733585286b79e2f9c1f3
Will Gutierrez <will.gutierrez@gmail.com>
2021-03-14 18:47:01 -0700

    undo reply nesting for comment state #9175

--- app/helpers/comment_helper.rb
+++ app/helpers/comment_helper.rb
@@ -14,10 +14,9 @@ module CommentHelper
 
   # takes an activerecord query, returns a plain array
   # used in notes_controller.rb and comment_controller.rb
-  def get_react_comments(comments_record, getting_replies = false)
+  def get_react_comments(comments_record)
     comments = []
     comments_record.each do |comment|
-      next unless comment.reply_to.nil? || getting_replies
       comment_json = {}
       comment_json[:authorId] = comment.uid
       comment_json[:authorPicFilename] = comment.author.photo_file_name
@@ -28,8 +27,6 @@ module CommentHelper
       comment_json[:createdAt] = comment.created_at
       comment_json[:htmlCommentText] = raw insert_extras(filtered_comment_body(comment.render_body))
       comment_json[:rawCommentText] = comment.comment
-      # nest the comment's replies in an array within the comment
-      comment_json[:replies] = get_react_comments(comment.replied_comments, true)
       comment_json[:replyTo] = comment.reply_to
       time_created_string = distance_of_time_in_words(comment.created_at, Time.current, include_seconds: false, scope: 'datetime.time_ago_in_words')
       comment_json[:timeCreatedString] = time_created_string
