aa928aec9cb5d1753326c33702eb05bb6bf0ffba
Cess <cessmbuguar@gmail.com>
2021-03-30 19:38:34 +0300

    define specific time on setup
    Use unchanged time to test to avoid disperancies

--- test/unit/tag_selection_test.rb
+++ test/unit/tag_selection_test.rb
@@ -1,19 +1,19 @@
 require 'test_helper'
 
 class TagSelectionTest < ActiveSupport::TestCase
+  def setup
+    @start_time = (Date.today - 1.month).to_time
+    @end_time = Date.today.to_time
+  end
 
   test 'graph' do
-    Timecop.freeze # account for timestamp change
-    end_time = Time.now
-    start_time = end_time - 4.weeks
     ts_count = TagSelection.select(:following, :created_at)
-      .where(following: true, created_at: (start_time..end_time))
+      .where(following: true, created_at: (@start_time..@end_time))
       .count
 
-    graph = TagSelection.graph(start_time, end_time)
+    graph = TagSelection.graph(@start_time, @end_time)
 
     assert_equal ts_count, graph.values.sum
     assert_equal Hash, graph.class
-    Timecop.return # unfreeze
   end
 end
