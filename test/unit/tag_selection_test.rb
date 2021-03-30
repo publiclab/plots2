5a053acb7bb32b19747430ae2d46c55e226299ba
Cess <cessmbuguar@gmail.com>
2021-03-30 19:51:16 +0300

    use local vars

--- test/unit/tag_selection_test.rb
+++ test/unit/tag_selection_test.rb
@@ -1,17 +1,15 @@
 require 'test_helper'
 
 class TagSelectionTest < ActiveSupport::TestCase
-  def setup
-    @start_time = (Date.today - 1.month).to_time
-    @end_time = Date.today.to_time
-  end
 
   test 'graph' do
+    start_time = (Date.today - 1.month).to_time
+    end_time = Date.today.to_time
     ts_count = TagSelection.select(:following, :created_at)
-      .where(following: true, created_at: (@start_time..@end_time))
+      .where(following: true, created_at: (start_time..end_time))
       .count
 
-    graph = TagSelection.graph(@start_time, @end_time)
+    graph = TagSelection.graph(start_time, end_time)
 
     assert_equal ts_count, graph.values.sum
     assert_equal Hash, graph.class
