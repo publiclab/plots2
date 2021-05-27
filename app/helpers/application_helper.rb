fe8373c533e8c57d7a7628685843f6cb53271ec0
imajit <ajit.171it233@nitk.edu.in>
2021-05-28 01:08:19 +0530

    Changed UI on Dashboard and added translation function overwrite

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -167,4 +167,18 @@ module ApplicationHelper
       raw(translated_string)
     end
   end
+
+  def translation(key, html = true, options = {})
+    translated_string = t(key, options)
+    options[:fallback] = false
+    translated_string2 = t(key, options)
+
+    if html && current_user&.has_tag('translation-helper') && translated_string2.include?("translation missing") && !translated_string.include?("<")
+      raw(%(<span>#{translated_string} <a href="https://www.transifex.com/publiclab/publiclaborg/translate/#de/$?q=text%3A#{translated_string}">
+          <i data-toggle='tooltip' data-placement='top' title='Needs translation? Click to help translate this text.' style='position:relative; right:2px; color:#bbb; font-size: 15px;' class='fa fa-globe'></i></a>
+       </span>))
+    else
+      raw(translated_string)
+    end
+  end
 end
