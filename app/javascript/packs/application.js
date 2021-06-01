a99eb5ee9a341fcbd422f336e20446f6f1ac6258
Tilda Udufo <mathildaudufo@gmail.com>
2021-06-01 17:10:15 +0100

    menu for wikis (#9711)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,3 +20,4 @@ console.log('Hello World from Webpacker')
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
+const tocbot = require('tocbot');
