ca6b7cc7e0a2c946966956018bdd080da1cc3d2f
Manasa2850 <manasakashyap28@gmail.com>
2021-06-24 10:43:11 +0530

    added onClick check

--- test/system/post_test.rb
+++ test/system/post_test.rb
@@ -51,6 +51,8 @@ class PostTest < ApplicationSystemTestCase
     # There should be 3 tags that show up as a card
     page.assert_selector('.tags-list .card-body', :count => 3)
 
+    find('.tags-list .card-body .ellipsis').click()
+
     accept_alert do
       find('.tags-list .card-body .tag-delete').click()
     end
