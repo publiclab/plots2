0ed84781562b40c9573197b273d473f526691683
Jeffrey Warren <jeff@unterbahn.com>
2021-06-29 15:17:09 -0400

    Update paperclip.rb

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -8,3 +8,10 @@ Paperclip.options[:content_type_mappings] = {
   csv: ['application/octet-stream', 'text/plain']
 }
 Paperclip::DataUriAdapter.register
+Paperclip::Attachment.default_options[:storage] = :fog
+Paperclip::Attachment.default_options[:fog_directory] = ENV["GOOGLE_STORAGE_BUCKET_NAME"]
+#Paperclip::Attachment.default_options[:path] = "public/system" # this is set by paperclip already
+# the following we do in the image model at /app/models/image.rb
+#Paperclip::Attachment.default_options[:fog_credentials] = { provider: "Local", local_root: "#{Rails.root}/public"}
+# the following are for local storage:
+#Paperclip::Attachment.default_options[:fog_host] = "http://localhost:3000"
