ff931ab954068254548250670dd1e6f2f9cd21ac
jywarren <jeff@unterbahn.com>
2021-07-09 22:00:55 +0000

    fixed display config with style, path

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -10,7 +10,7 @@ Paperclip.options[:content_type_mappings] = {
 Paperclip::DataUriAdapter.register
 Paperclip::Attachment.default_options[:storage] = :fog
 Paperclip::Attachment.default_options[:fog_directory] = ENV["GOOGLE_STORAGE_BUCKET_NAME"] || ''
-#Paperclip::Attachment.default_options[:path] = "public/system" # this is set by paperclip already
+Paperclip::Attachment.default_options[:path] = ":rails_root/public/system/system/:class/:attachment/:id_partition/:style/:filename"
 Paperclip::Attachment.default_options[:fog_credentials] = {
     provider: ENV["FOG_PROVIDER"] || "Local",
     local_root: "#{Rails.root}/public",
