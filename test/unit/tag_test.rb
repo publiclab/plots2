1f26fa0308407226bf06d4bd6be84c5b7ba17706
Jeffrey Warren <jeff@unterbahn.com>
2021-10-12 16:11:17 -0400

    tweak counts

--- test/unit/tag_test.rb
+++ test/unit/tag_test.rb
@@ -183,14 +183,14 @@ class TagTest < ActiveSupport::TestCase
   test 'contributors with specific tag name' do
     tag = tags(:test)
     contributors = Tag.contributors(tag.name)
-    assert_equal [1, 2], contributors.pluck(:id)
+    assert_equal [1, 2, 5, 6], contributors.pluck(:id)
   end
 
   test 'contributors with timeframe' do
     tag = tags(:test)
     # including current date
     contributors = Tag.contributors(tag.name, start: Time.now-1.month, finish: Time.now)
-    assert_equal [1, 2], contributors.pluck(:id)
+    assert_equal [1, 2, 5, 6], contributors.pluck(:id)
     # during presumably empty time period
     contributors2 = Tag.contributors(tag.name, start: Time.now-10.years, finish: Time.now-9.years)
     assert_equal 0, contributors2.length
@@ -202,7 +202,7 @@ class TagTest < ActiveSupport::TestCase
   test 'contributor_count with specific tag name' do
     tag = tags(:test)
     contributor_count = Tag.contributor_count(tag.name)
-    assert_equal 2,contributor_count
+    assert_equal 4,contributor_count
   end
 
   test 'check sort according to followers ascending' do
