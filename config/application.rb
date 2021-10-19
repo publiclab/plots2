58c59338cbfda73b6a4457ba3bca0ede97dc9e00
Jeffrey Warren <jeff@unterbahn.com>
2021-10-19 18:32:51 -0400

    update to sentry-ruby (#10364)
    * update to sentry-ruby
    
    * enabled_environments

--- config/application.rb
+++ config/application.rb
@@ -96,11 +96,34 @@ module Plots2
     config.paths.add File.join('app','api'), glob: File.join('**', '*.rb')
     config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
 
-    Raven.configure do |config|
-      # DSN should be an ENV variable!
-      config.dsn = ENV["SENTRY_DSN"] || 'https://0490297edae647b3bd935bdb4658da54@o239675.ingest.sentry.io/1410626'
-      config.current_environment = ENV["COMPOSE_PROJECT_NAME"] || ENV["RAILS_ENV"] || %w(production)
-    end
+    Sentry.init do |config|
+       # DSN should be an ENV variable!
+       config.dsn = ENV["SENTRY_DSN"] || 'https://0490297edae647b3bd935bdb4658da54@o239675.ingest.sentry.io/1410626'
+      config.breadcrumbs_logger = [:sentry_logger, :http_logger]
+    
+      # To activate performance monitoring, set one of these options.
+      # We recommend adjusting the value in production:
+      config.traces_sample_rate = 0.5
+
+      config.environment = case ENV["COMPOSE_PROJECT_NAME"] 
+        when 'plots_stable'
+          'stable'
+        when 'plots_unstable'
+          'unstable'
+      else
+        ENV["RAILS_ENV"]
+      end
+      config.enabled_environments = %w[production, stable, unstable]
+
+      # use Rails' parameter filter to sanitize the event payload:
+      # for Rails 6+:
+      # filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
+      # for Rails 5:
+      filter = ActionDispatch::Http::ParameterFilter.new(Rails.application.config.filter_parameters)
+      config.before_send = lambda do |event, hint|
+        filter.filter(event.to_hash)
+      end
+    end 
 
   end
 end
