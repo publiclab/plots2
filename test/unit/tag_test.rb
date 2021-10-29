023dab002d49b83dd5d230cfb1e7658f497cd9dc
jywarren <jeff@unterbahn.com>
2021-10-29 21:28:34 +0000

    change expected

--- test/unit/tag_test.rb
+++ test/unit/tag_test.rb
@@ -192,7 +192,7 @@ class TagTest < ActiveSupport::TestCase
     tag = tags(:test)
     # including current date
     contributors = Tag.contributors(tag.name, start: Time.now-1.month, finish: Time.now+1.day)
-    assert_equal [1, 2], contributors.pluck(:id)
+    assert_equal [1, 2, 5, 6, 12], contributors.pluck(:id)
     # during presumably mostly empty time period
     contributors2 = Tag.contributors(tag.name, start: Time.now-10.years, finish: Time.now-9.years)
     assert_equal [2], contributors2.pluck(:id)
