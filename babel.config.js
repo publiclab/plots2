f0abc9b881d510baaded6ad59e10dec538adccd7
KarishmaVanwari <78212650+KarishmaVanwari@users.noreply.github.com>
2021-11-25 21:01:06 +0530

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -62,18 +62,7 @@ module.exports = function(api) {
           useBuiltIns: true
         }
       ],
-      [
-        '@babel/plugin-proposal-private-methods',
-        {
-          loose: true
-        }
-      ],
-      [
-        '@babel/plugin-proposal-private-property-in-object',
-        {
-          loose: true
-        }
-      ],
+ 
       [
         '@babel/plugin-transform-runtime',
         {
