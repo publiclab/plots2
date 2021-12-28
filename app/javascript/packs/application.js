7dee01087310ff08aee7f6921435e7704b1e4566
Billy <wjmoroney57@gmail.com>
2021-12-28 12:27:57 -0600

    add fetch_counts, @node_type, and change render method in def comments

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,3 +20,7 @@ console.log('Hello World from Webpacker')
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
