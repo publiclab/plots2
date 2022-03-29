4d64ce3dfcbf438d6eef3153baf5e646d7cdcc5e
Jeffrey Warren <jeff@unterbahn.com>
2022-03-29 17:06:02 -0400

    Update translation.js

--- app/assets/javascripts/translation.js
+++ app/assets/javascripts/translation.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   // display only every 3rd translation prompt, and stop when we reach 10
   $('.translationIcon').filter(function(i) {
-    var maxTranslationPrompts = 5
+    var maxTranslationPrompts = 20
     return (i + 1) % 3 === 0 && i < maxTranslationPrompts / 3
   }).show();
 });
