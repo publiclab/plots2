caa94f41bf0205bdd0e32cab4cc54e634aaede5a
Victoria Ajala <victoria.o.ajala@gmail.com>
2022-04-05 18:07:50 +0100

    display confirm modal before deleting spam posts (#10904)
    * Update spam2.js
    
    * Update spam2_test.rb
    
    * Update spam2.js
    
    * indent
    
    Co-authored-by: Tilda Udufo <mathildaudufo@gmail.com>

--- app/assets/javascripts/spam2.js
+++ app/assets/javascripts/spam2.js
@@ -57,6 +57,10 @@ function search_table(filter, url) {
 }
 
 function batch_nav(bulk) {
+	if(bulk == "batch_delete" || bulk == "batch_comment/delete") {
+        	let result = confirm("Are you sure you want to delete the selected nodes?");
+        	if(!result) return false
+	}
 	vals = []
 	$('.selectedId').each(function (i, a) { // batch nav
 		if (a.checked) vals.push(a.value);
