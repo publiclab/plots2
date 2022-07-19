dae8bc20d1b21d54683386d44ebec9f6d5f7bcce
Jeffrey Warren <jeff@unterbahn.com>
2022-07-19 18:30:14 -0400

    Update config/environments/test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
 
   config.active_job.queue_adapter = :inline
 
-  config.active_record.yaml_column_permitted_classes = [OAuth::AccessToken, OAuth::Consumer]
+  config.active_record.yaml_column_permitted_classes = [OAuth::AccessToken, OAuth::Consumer, Symbol]
   
   OmniAuth.config.test_mode = true
   #OAuth hash for different providers for testing purpose
