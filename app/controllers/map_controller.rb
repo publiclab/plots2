0aec50638c97827889d0beed70eea1360b4bf5a0
Jeffrey Warren <jeff@unterbahn.com>
2022-08-28 11:55:17 -0400

    Update app/controllers/map_controller.rb
    Co-authored-by: Anirudh Prabhakaran <76864239+anirudhprabhakaran3@users.noreply.github.com>

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,7 +2,7 @@ class MapController < ApplicationController
   def index
     @title = 'Maps'
     nodes = Node.find_by_tag('map-archive')
-      .order('nodes.nid DESC')
+      .order('node.nid DESC')
       .where(status: 1)
     @pagy, @nodes = pagy(nodes, items: 32)
 
