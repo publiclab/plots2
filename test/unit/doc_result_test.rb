3778cb5a990cf2d9505416a156a77274f0733e12
Anirudh Prabhakaran <anirudhprabhakaran3@gmail.com>
2022-09-03 15:11:33 +0530

    Fixed the activity function in home controller

--- test/unit/doc_result_test.rb
+++ test/unit/doc_result_test.rb
@@ -9,7 +9,7 @@ class DocResultTest < ActiveSupport::TestCase
       doc_type: 'QUESTIONS',
       doc_url: question.path(:question),
       doc_title: question.title,
-      # score: question.answers.length    What should I do about this particular field?
+      score: question.comments.length
     )
 
     assert_equal question.nid,    result.doc_id
