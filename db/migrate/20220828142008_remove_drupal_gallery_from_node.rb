4e80ae2cf6f30aad9515f324ca5ceecce3697c17
Jeffrey Warren <jeff@unterbahn.com>
2022-09-06 11:37:19 -0400

    Adjust gallery references in drupal gallery migration
    Fix for #11371

--- db/migrate/20220828142008_remove_drupal_gallery_from_node.rb
+++ db/migrate/20220828142008_remove_drupal_gallery_from_node.rb
@@ -2,10 +2,10 @@ class RemoveDrupalGalleryFromNode < ActiveRecord::Migration[5.2]
   def change
     Node.all.each do |node|
       new_revision = node.revisions.first.dup
-      gallery_images = ""
-      node&.gallery.each do |image|
+      gallery_images = ""      
+      node&.drupal_content_field_image_gallery.each do |image|
         html = "<a target='_blank' href='#{image&.image&.path(:original)}'><img rel='tooltip' data-title='#{image&.description}' style='margin-bottom:4px;' class='rounded' src='#{image&.image&.path(:thumb)}' /></a>"
-        gallery_images << html
+        gallery_images << html unless image.nil? || image.image.nil? || image.image.path.nil?
       end
       new_revision.body = gallery_images + new_revision.body
       new_revision.save!
