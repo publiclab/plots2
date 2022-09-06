ae016fe0abb90bbbdd7710c3adca8f34dcfc9744
Jeffrey Warren <jeff@unterbahn.com>
2022-09-06 12:38:27 -0400

    additional migration fix to image galleries
    https://github.com/publiclab/plots2/pull/11371

--- db/migrate/20220828142008_remove_drupal_gallery_from_node.rb
+++ db/migrate/20220828142008_remove_drupal_gallery_from_node.rb
@@ -6,8 +6,10 @@ class RemoveDrupalGalleryFromNode < ActiveRecord::Migration[5.2]
       gallery = DrupalContentFieldImageGallery.where(nid: node.nid)
         .order('field_image_gallery_fid')
       gallery.each do |image|
-        html = "<a target='_blank' href='#{image&.image&.path(:original)}'><img rel='tooltip' data-title='#{image&.description}' style='margin-bottom:4px;' class='rounded' src='#{image&.image&.path(:thumb)}' /></a>"
-        gallery_images << html unless image.nil? || image.image.nil? || image.image.path.nil?
+        unless image.nil? || image.field_image_gallery_fid.nil? || image.image.nil? || image.image.path.nil?
+          html = "<a target='_blank' href='#{image&.image&.path(:original)}'><img rel='tooltip' data-title='#{image&.description}' style='margin-bottom:4px;' class='rounded' src='#{image&.image&.path(:thumb)}' /></a>"
+          gallery_images << html
+        end
       end
       new_revision.body = gallery_images + new_revision.body
       new_revision.save!
