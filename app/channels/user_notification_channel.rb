f083e148a41e5a1bb1fb6640b2fd08e89157ada1
Gift Segun Adesigbin <giftsegunade@gmail.com>
2022-09-15 10:59:04 +0100

    Update user_notification_channel.rb

--- app/channels/user_notification_channel.rb
+++ app/channels/user_notification_channel.rb
@@ -1,11 +1,12 @@
 class UserNotificationChannel < ApplicationCable::Channel
-  def subscribed
-    if !current_user.nil?
-      stream_from "users:notification:#{current_user.id}"
-    else
+  def subscribed 
+    if current_user.nil?
       reject
+    else
+      stream_from "users:notification:#{current_user.id}"
     end
   end
+ 
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
