50d7a8cc4794d649079d609ff92e40eb18a2ac33
melwhite1 <107372823+melwhite1@users.noreply.github.com>
2022-09-15 10:11:08 -0400

    Update user_channel.rb

--- app/channels/user_channel.rb
+++ app/channels/user_channel.rb
@@ -1,11 +1,11 @@
 class UserChannel < ApplicationCable::Channel
   def subscribed
-    if !current_user.nil?
-      stream_from "users:#{current_user.id}"
-    else
-      reject
-    end
-  end
++    if current_user.nil?
+       reject
++    else
++      stream_from "users:#{current_user.id}"
+     end
+   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
