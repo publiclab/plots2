3da00175472bcacbf492b46393f3e887055b9fb0
theorem23 <macginyani@gmail.com>
2022-10-16 13:23:15 +0200

    fix: replace var keyword

--- public/research-notify.js
+++ public/research-notify.js
@@ -3,7 +3,7 @@
 //Nice tips on jquery inclusion etc here: http://www.smashingmagazine.com/2010/05/23/make-your-own-bookmarklets-with-jquery/
 
 function getSelText() {
-  var SelText = '';
+  let SelText = "";
   if (window.getSelection) {
     SelText = window.getSelection();
   } else if (document.getSelection) {
@@ -14,18 +14,23 @@ function getSelText() {
   return SelText;
 }
 
-(function(){
-
+(function () {
   // the minimum version of jQuery we want
-  var v = "1.3.2";
+  const v = "1.3.2";
 
   // check prior inclusion and version
   if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
-    var done = false;
-    var script = document.createElement("script");
-    script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
-    script.onload = script.onreadystatechange = function(){
-      if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+    let done = false;
+    const script = document.createElement("script");
+    script.src =
+      "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+    script.onload = script.onreadystatechange = function () {
+      if (
+        !done &&
+        (!this.readyState ||
+          this.readyState == "loaded" ||
+          this.readyState == "complete")
+      ) {
         done = true;
         initMyBookmarklet();
       }
@@ -34,19 +39,22 @@ function getSelText() {
   } else {
     initMyBookmarklet();
   }
-  
+
   function initMyBookmarklet() {
-    (window.myBookmarklet = function() {
+    (window.myBookmarklet = function () {
       // your JavaScript code goes here!
-      
-      
-      $('body')[0].onmouseup = function(e) {
-        window.location = "http://publiclab.org/post?title="+$('#title').text()+"&body="+getSelText()
-      }
 
-      alert("Drag to select the content you want to open source at PublicLab.org")
+      $("body")[0].onmouseup = function (e) {
+        window.location =
+          "http://publiclab.org/post?title=" +
+          $("#title").text() +
+          "&body=" +
+          getSelText();
+      };
 
+      alert(
+        "Drag to select the content you want to open source at PublicLab.org"
+      );
     })();
   }
-
 })();
