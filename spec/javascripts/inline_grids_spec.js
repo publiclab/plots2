e886b2ad4f1a7d12ccd8a552fd7511c31dc3ceac
Divine-peace <divinepeaceokorhi@gmail.com>
2022-10-18 15:51:58 +0100

    update var to let

--- spec/javascripts/inline_grids_spec.js
+++ spec/javascripts/inline_grids_spec.js
@@ -1,38 +1,32 @@
 /* eslint-disable no-empty-label */
 
-var editor;
+let editor;
 
 fixture.preload("inline_grid.html");
 
-describe("Inline grids", function() {
-
-  beforeEach(function() {
-
-      this.fixtures = fixture.load("inline_grid.html");
-
+describe("Inline grids", function () {
+  beforeEach(function () {
+    this.fixtures = fixture.load("inline_grid.html");
   });
 
+  it("sends a like request when like button is clicked", function () {
+    expect($($(".notes-grid tr")[1]).find("td:first a").html()).to.eql(
+      "First post"
+    );
 
-  it("sends a like request when like button is clicked", function() {
+    $("table:first th:first a").trigger("click");
 
+    setTimeout(0, function () {
+      expect($($(".notes-grid tr")[1]).find("td:first a").html()).to.eql(
+        "Third post"
+      );
 
-    expect($($('.notes-grid tr')[1]).find('td:first a').html()).to.eql('First post');
-
-    $('table:first th:first a').trigger('click');
-
-    setTimeout(0, function() {
-
-      expect($($('.notes-grid tr')[1]).find('td:first a').html()).to.eql('Third post');
- 
-      setTimeout(0, function() {
-
-        $('table:first th:first a').trigger('click');
-        expect($($('.notes-grid tr')[1]).find('td:first a').html()).to.eql('First post');
-
+      setTimeout(0, function () {
+        $("table:first th:first a").trigger("click");
+        expect($($(".notes-grid tr")[1]).find("td:first a").html()).to.eql(
+          "First post"
+        );
       });
-
     });
-
   });
-
 });
