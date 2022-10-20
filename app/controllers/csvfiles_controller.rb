83e0439ae8449599ec11f813563dbef5393b43f3
victory-lydia <81695564+victory-lydia@users.noreply.github.com>
2022-10-20 18:14:39 +0100

    rubocop fixes - csvfiles controller (#11516)
    * I changed lines 10,31 and 41
    
    * Changed lines 10 and 31
    
    * Update app/controllers/csvfiles_controller.rb
    
    Co-authored-by: Tilda Udufo <mathildaudufo@gmail.com>
    
    * Update app/controllers/csvfiles_controller.rb
    
    Co-authored-by: Tilda Udufo <mathildaudufo@gmail.com>
    
    * Update README.md
    
    Co-authored-by: Tilda Udufo <mathildaudufo@gmail.com>

--- app/controllers/csvfiles_controller.rb
+++ app/controllers/csvfiles_controller.rb
@@ -7,7 +7,7 @@ class CsvfilesController < ApplicationController
       filetitle: params[:filetitle],
       filedescription: params[:filedescription],
       filepath: params[:object],
-      filename: "file" + Time.now.to_i.to_s,
+      filename: "file#{Time.now.to_i}",
       filestring: params[:filestring]
     )
     render json: @csvfile if @csvfile.save
@@ -28,7 +28,7 @@ class CsvfilesController < ApplicationController
       filetitle: params[:filetitle],
       filedescription: params[:filedescription],
       filepath: params[:object],
-      filename: "file" + Time.now.to_i.to_s,
+      filename: "file#{Time.now.to_i}",
       filestring: params[:filestring],
       graphobject: params[:graphobject]
     )
