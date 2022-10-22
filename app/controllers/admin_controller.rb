40c1c3365efabad8fca5451cb11b4822fc12dc16
Gerald Nwanwko <geraldcjnwankwo@outlook.com>
2022-10-22 13:39:06 +0100

    Update app/controllers/admin_controller.rb
    Co-authored-by: Tilda Udufo <mathildaudufo@gmail.com>

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -280,7 +280,7 @@ class AdminController < ApplicationController
     else
       flash[:error] = 'Only moderators can moderate other users.'
     end
-   redirect_to "/profile/#{user.name}?_=#{Time.now.to_i}"
+    redirect_to "/profile/#{user.name}?_=#{Time.now.to_i}"
   end
 
   def unmoderate
