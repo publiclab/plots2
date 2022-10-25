affe0fc0af1aa76a09003f75b8994fdad20ee112
Cent <66119709+ijayhub@users.noreply.github.com>
2022-10-25 18:28:26 +0100

    update like_spec.js (#11524)
    * update like_spec.js
    
    The code was in var an es5 way of declaring a variable changed it to const
    
    * Update like_spec.js
    
    The code was in var an es5 way of declaring a variable changed it to const
    
    * Update like_spec.js
    
    The code was in var an es5 way of declaring a variable changed it to let
    
    * Update like_spec.js
    
    fix: modified
    
    * Update like_spec.js
    
    * Update like_spec.js

--- spec/javascripts/like_spec.js
+++ spec/javascripts/like_spec.js
@@ -19,12 +19,12 @@ describe("Like Button", function () {
     ajaxStub = sinon.stub($, 'ajax', function (object) {
       response = object.url === '/likes/node/1/create' ? '4' : 'none'
 
-      var d = $.Deferred();
+      const d = $.Deferred();
       response === '4' ? d.resolve(response) : d.reject(response);
       return d.promise();
       
     });
- 
+
     $("#like-button-1").trigger("click");
 
     expect(ajaxStub).to.have.been.called;
@@ -44,7 +44,7 @@ describe("Like Button", function () {
       //   console.log('Failed to fake response to:', object.url);
       // }
 
-      var d = $.Deferred();
+      const d = $.Deferred();
       response === '4' ? d.resolve(response) : d.reject(response);
       return d.promise();
 
@@ -65,7 +65,7 @@ describe("Like Button", function () {
     ajaxStub = sinon.stub($, 'ajax', function (object) {
       response = object.url === '/likes/node/1/delete' ? '4' : 'none'
 
-      var d = $.Deferred();
+      const d = $.Deferred();
       response === '4' ? d.resolve(response) : d.reject(response);
       return d.promise();
 
